5ad6900da3146c519bcba2e6cae8f13e8f44f24d
Praveen K. Gostu <praveek9@in.ibm.com>
2017-07-13 22:39:13 +0530

    Added more testcases for IAM Policy

--- ibm/data_source_ibm_iam_user_policy_test.go
+++ ibm/data_source_ibm_iam_user_policy_test.go
@@ -2,9 +2,11 @@ package ibm
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 
 	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
 )
 
 func TestAccIBMIAMUserPolicyDataSource_basic(t *testing.T) {
@@ -16,14 +18,32 @@ func TestAccIBMIAMUserPolicyDataSource_basic(t *testing.T) {
 			resource.TestStep{
 				Config: testAccCheckIBMIAMUserPolicyDataSourceConfig(),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.ibm_iam_user_policy.testacc_iam_policies", "policies.#", "1"),
-					resource.TestCheckResourceAttr("data.ibm_iam_user_policy.testacc_iam_policies", "policies.0.roles.#", "1"),
-					resource.TestCheckResourceAttr("data.ibm_iam_user_policy.testacc_iam_policies", "policies.0.resources.#", "1"),
-					resource.TestCheckResourceAttr("data.ibm_iam_user_policy.testacc_iam_policies", "policies.0.roles.0.name", "viewer"),
+					testAccIBMIAMUserPolicyCheck("data.ibm_iam_user_policy.testacc_iam_policies"),
 				),
 			},
 		},
 	})
+}
+
+func testAccIBMIAMUserPolicyCheck(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+
+		r := s.RootModule().Resources[n]
+		a := r.Primary.Attributes
+
+		var (
+			policySize int
+			err        error
+		)
+
+		if policySize, err = strconv.Atoi(a["policies.#"]); err != nil {
+			return err
+		}
+		if policySize < 1 {
+			return fmt.Errorf("No policies found")
+		}
+		return nil
+	}
 }
 
 func testAccCheckIBMIAMUserPolicyDataSourceConfig() string {
