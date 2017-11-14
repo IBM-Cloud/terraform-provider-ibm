d71cefede91eecfea6c8188ef9ca9df8051dbb36
favrecr <eariasn@us.ibm.com>
2017-11-14 14:59:22 -0500

    updating based on PR #74 feedback

---
+++ ibm/data_source_ibm_account_test.go
@@ -0,0 +1,39 @@
+package ibm
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccIBMAccountDataSource_basic(t *testing.T) {
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccCheckIBMAccountDataSourceConfig(),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.ibm_org.testacc_ds_org", "org", cfOrganization),
+					resource.TestCheckResourceAttrSet(
+						"data.ibm_account.testacc_acc", "id"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckIBMAccountDataSourceConfig() string {
+	return fmt.Sprintf(`
+	
+data "ibm_org" "testacc_ds_org" {
+    org = "%s"
+}
+
+data "ibm_account" "testacc_acc" {
+    org_guid = "${data.ibm_org.testacc_ds_org.id}"
+}`, cfOrganization)
+
+}
