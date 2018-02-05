d8385c70d9464487344e084828d4c8fdea35f7b3
Praveen K. Gostu <praveek9@in.ibm.com>
2018-02-05 12:02:00 +0530

    Datasource to list supported kube versions in IBM Container service
    Closes #124

---
+++ ibm/data_source_ibm_container_cluster_versions.go
@@ -0,0 +1,55 @@
+package ibm
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform/helper/schema"
+	"time"
+)
+
+func dataSourceIBMContainerClusterVersions() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceIBMContainerClusterVersionsRead,
+
+		Schema: map[string]*schema.Schema{
+			"org_guid": {
+				Description: "The bluemix organization guid this cluster belongs to",
+				Type:        schema.TypeString,
+				Required:    true,
+			},
+			"space_guid": {
+				Description: "The bluemix space guid this cluster belongs to",
+				Type:        schema.TypeString,
+				Required:    true,
+			},
+			"account_guid": {
+				Description: "The bluemix account guid this cluster belongs to",
+				Type:        schema.TypeString,
+				Required:    true,
+			},
+			"valid_kube_versions": {
+				Description: "List supported kube-versions",
+				Type:        schema.TypeList,
+				Computed:    true,
+				Elem:        &schema.Schema{Type: schema.TypeString},
+			},
+		},
+	}
+}
+
+func dataSourceIBMContainerClusterVersionsRead(d *schema.ResourceData, meta interface{}) error {
+	csClient, err := meta.(ClientSession).ContainerAPI()
+	if err != nil {
+		return err
+	}
+	verAPI := csClient.KubeVersions()
+	targetEnv := getClusterTargetHeader(d)
+
+	availableVersions, _ := verAPI.List(targetEnv)
+	versions := make([]string, len(availableVersions))
+	for i, version := range availableVersions {
+		versions[i] = fmt.Sprintf("%d%s%d%s%d", version.Major, ".", version.Minor, ".", version.Patch)
+	}
+	d.SetId(time.Now().UTC().String())
+	d.Set("valid_kube_versions", versions)
+	return nil
+}
