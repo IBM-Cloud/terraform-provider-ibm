790f777362e13fec5c17669507ca1214c401b4b0
JunaidKhan001 <jkhan001@in.ibm.com>
2018-03-06 14:31:36 +0530

    On branch cdn  Changes to be committed: 	modified:   ibm/resource_ibm_cdn.go

--- ibm/resource_ibm_cdn.go
+++ ibm/resource_ibm_cdn.go
@@ -19,7 +19,6 @@ func resourceIBMCDN() *schema.Resource {
 		Update: resourceIBMCDNUpdate,
 		Delete: resourceIBMCDNDelete,
 		Exists: resourceIBMCDNExists,
-		//Importer: &schema.ResourceImporter{},
 
 		Schema: map[string]*schema.Schema{
 			"hostname": &schema.Schema{
@@ -85,12 +84,17 @@ func resourceIBMCDNCreate(d *schema.ResourceData, meta interface{}) error {
 			VendorName: sl.String(vendorname),
 			Domain:     sl.String(domain),
 			Protocol:   sl.String(protocol),
-			HttpPort:   sl.Int(httpsport),
+			HttpPort:   sl.Int(httpport),
 			OriginType: sl.String(origintype),
 			BucketName: sl.String(bucketname),
 		})
+		///Print the response of the requested the service.
+		log.Print("Response for cdn order")
 		log.Println(receipt1)
-		log.Println(err)
+		if err != nil {
+			log.Print("Error creating : ")
+			log.Println(err)
+		}
 		d.SetId(*receipt1[0].UniqueId)
 	}
 	if origintype == "OBJECT_STORAGE" && protocol == "HTTPS" {
@@ -103,8 +107,13 @@ func resourceIBMCDNCreate(d *schema.ResourceData, meta interface{}) error {
 			OriginType: sl.String(origintype),
 			BucketName: sl.String(bucketname),
 		})
+		///Print the response of the requested the service.
+		log.Print("Response for cdn order")
 		log.Println(receipt2)
-		log.Println(err)
+		if err != nil {
+			log.Println("error creating")
+			log.Println(err)
+		}
 		d.SetId(*receipt2[0].UniqueId)
 	}
 	if origintype == "OBJECT_STORAGE" && protocol == "HTTP_AND_HTTPS" {
@@ -113,13 +122,18 @@ func resourceIBMCDNCreate(d *schema.ResourceData, meta interface{}) error {
 			VendorName: sl.String(vendorname),
 			Domain:     sl.String(domain),
 			Protocol:   sl.String(protocol),
-			HttpPort:   sl.Int(httpsport),
+			HttpPort:   sl.Int(httpport),
 			HttpsPort:  sl.Int(httpsport),
 			OriginType: sl.String(origintype),
 			BucketName: sl.String(bucketname),
 		})
+		///Print the response of the requested the service.
+		log.Print("Response for cdn order")
 		log.Println(receipt3)
-		log.Println(err)
+		if err != nil {
+			log.Println("error creating")
+			log.Println(err)
+		}
 		d.SetId(*receipt3[0].UniqueId)
 	}
 	if origintype == "HOST_SERVER" && protocol == "HTTP" {
@@ -131,8 +145,13 @@ func resourceIBMCDNCreate(d *schema.ResourceData, meta interface{}) error {
 			HttpPort:   sl.Int(httpport),
 			OriginType: sl.String(origintype),
 		})
+		///Print the response of the requested the service.
+		log.Print("Response for cdn order")
 		log.Println(receipt4)
-		log.Println(err)
+		if err != nil {
+			log.Println("error creating")
+			log.Println(err)
+		}
 		d.SetId(*receipt4[0].UniqueId)
 	}
 	if origintype == "HOST_SERVER" && protocol == "HTTPS" {
@@ -144,8 +163,13 @@ func resourceIBMCDNCreate(d *schema.ResourceData, meta interface{}) error {
 			HttpsPort:  sl.Int(httpsport),
 			OriginType: sl.String(origintype),
 		})
+		///Print the response of the requested the service.
+		log.Print("Response for cdn order")
 		log.Println(receipt5)
-		log.Println(err)
+		if err != nil {
+			log.Println("error creating")
+			log.Println(err)
+		}
 		d.SetId(*receipt5[0].UniqueId)
 	}
 	if origintype == "HOST_SERVER" && protocol == "HTTP_AND_HTTPS" {
@@ -158,11 +182,15 @@ func resourceIBMCDNCreate(d *schema.ResourceData, meta interface{}) error {
 			HttpsPort:  sl.Int(httpsport),
 			OriginType: sl.String(origintype),
 		})
+		///Print the response of the requested the service.
+		log.Print("Response for cdn order")
 		log.Println(receipt6)
-		log.Println(err)
+		if err != nil {
+			log.Println("error creating")
+			log.Println(err)
+		}
 		d.SetId(*receipt6[0].UniqueId)
 	}
-
 	return nil
 }
 
@@ -171,12 +199,15 @@ func resourceIBMCDNRead(d *schema.ResourceData, meta interface{}) error {
 	log.Println("reading cdn service...")
 	service := services.GetNetworkCdnMarketplaceConfigurationMappingService(sess)
 	cdnId, err := strconv.Atoi(d.Id())
-	log.Println(cdnId)
 	///read the changes in the remote resource and update in the local resource.
 	read, err := service.VerifyDomainMapping(sl.Int(cdnId))
+	///Print the response of the requested the service.
+	log.Print("Response for cdn verification: ")
 	log.Println(read)
-	log.Println(err)
-
+	if err != nil {
+		log.Println("error Reading")
+		log.Println(err)
+	}
 	return nil
 }
 
@@ -190,7 +221,7 @@ func resourceIBMCDNUpdate(d *schema.ResourceData, meta interface{}) error {
 	originaddress := d.Get("origin_address").(string)
 	protocol := d.Get("protocol").(string)
 	httpport := d.Get("httpport").(int)
-	e := d.Id()
+	uniqueId := d.Id()
 	service := services.GetNetworkCdnMarketplaceConfigurationMappingService(sess)
 	///pass the changed as well as unchanged parameters to update the resource.
 	update, err := service.UpdateDomainMapping(&datatypes.Container_Network_CdnMarketplace_Configuration_Input{
@@ -200,10 +231,15 @@ func resourceIBMCDNUpdate(d *schema.ResourceData, meta interface{}) error {
 		Protocol:   sl.String(protocol),
 		HttpPort:   sl.Int(httpport),
 		OriginType: sl.String(origintype),
-		UniqueId:   sl.String(e),
+		UniqueId:   sl.String(uniqueId),
 	})
+	///Print the response of the requested service.
+	log.Print("Response for cdn update: ")
 	log.Println(update)
-	log.Println(err)
+	if err != nil {
+		log.Println("error updating")
+		log.Println(err)
+	}
 	return nil
 }
 
@@ -214,16 +250,16 @@ func resourceIBMCDNDelete(d *schema.ResourceData, meta interface{}) error {
 
 	log.Printf("[INFO] Deleting Domain Mapping:")
 	cdnId := sl.String(d.Id())
-	log.Println(cdnId)
 	///pass the id to delete the resource.
 	delete, err := service.DeleteDomainMapping(cdnId)
 	if err != nil {
 		log.Println("error destroying")
 		log.Println(err)
 	}
-	d.SetId("")
 	///print the delete response
+	log.Print("Delete response is : ")
 	log.Println(delete)
+	d.SetId("")
 	return nil
 }
 
@@ -232,10 +268,10 @@ func resourceIBMCDNExists(d *schema.ResourceData, meta interface{}) (bool, error
 	log.Println("Exists cdn service...")
 	service := services.GetNetworkCdnMarketplaceConfigurationMappingService(sess)
 	cdnId := sl.String(d.Id())
-
-	log.Println(cdnId)
 	///check if the resource exists with the given id.
 	exists, err := service.ListDomainMappingByUniqueId(cdnId)
+	///Print the response for exist request.
+	log.Print("Exists response is : ")
 	log.Println(exists)
 	if err != nil {
 		if apiErr, ok := err.(sl.Error); ok {
@@ -247,3 +283,4 @@ func resourceIBMCDNExists(d *schema.ResourceData, meta interface{}) (bool, error
 	}
 	return true, nil
 }
+
