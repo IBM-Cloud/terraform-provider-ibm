3b955cc125d2725cb940235e3ef588a57300b1c6
rushabh mhaisane <rmhaisan@in.ibm.com>
2018-03-21 12:17:24 +0530

    Specs for validation

--- ibm/resource_ibm_multi_vlan_firewall_test.go
+++ ibm/resource_ibm_multi_vlan_firewall_test.go
@@ -57,6 +57,19 @@ func TestAccIBMMultiVlanFirewallHA_Basic(t *testing.T) {
 		},
 	})
 }
+func TestAccIBMMultiVlanFirewall_InvalidFirewallType(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config:      testAccCheckIBMMultiVlanFirewallFirewallTypeConfig_InvalidFirewallType,
+				ExpectError: regexp.MustCompile("must contain a value from"),
+			},
+		},
+	})
+}
+
 
 const testAccCheckIBMMultiVlanFirewallConfig_basic = `
 resource "ibm_multi_vlan_firewall" "firewall_first" {
@@ -77,3 +90,15 @@ resource "ibm_multi_vlan_firewall" "firewall_first" {
 	firewall_type = "FortiGate Firewall Appliance HA Option"
 	addon_configuration = ["FortiGate Security Appliance - Web Filtering Add-on (High Availability)","FortiGate Security Appliance - NGFW Add-on (High Availability)","FortiGate Security Appliance - AV Add-on (High Availability)"]
 	}`
+const testAccCheckIBMMultiVlanFirewallFirewallTypeConfig_InvalidFirewallType = `
+	resource "ibm_multi_vlan_firewall" "firewall_first" {
+		datacenter = "dal13"
+		pod = "pod01"
+		name = "Checkdelete1"
+		public_vlan_id = 2213543
+		firewall_type = "FortiGate Firewall Appliance ABC"
+		addon_configuration = ["FortiGate Security Appliance - Web Filtering Add-on (High Availability)","FortiGate Security Appliance - NGFW Add-on (High Availability)","FortiGate Security Appliance - AV Add-on (High Availability)"]
+		}`
+
+
+
