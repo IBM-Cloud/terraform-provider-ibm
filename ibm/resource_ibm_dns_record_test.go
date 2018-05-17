0ac7e6f40b6632c072ef173df792b56ce5c76334
sakshi agarwal <sakshiag@in.ibm.com>
2018-05-18 00:45:06 +0530

    Fix Issue #222

--- ibm/resource_ibm_dns_record_test.go
+++ ibm/resource_ibm_dns_record_test.go
@@ -166,6 +166,38 @@ func TestAccIBMDNSRecord_MX_PRIORITY(t *testing.T) {
 	})
 }
 
+func TestAccIBMDNSRecord_SRV_PRIORITY_ZERO(t *testing.T) {
+	var dns_domain datatypes.Dns_Domain
+	var dns_domain_record datatypes.Dns_Domain_ResourceRecord
+
+	domainName := fmt.Sprintf("tfuatdomainr%s.ibm.com", acctest.RandString(10))
+	protocol := "_udp"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckIBMDNSDomainDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckIBMDNSRecordSRVWithPriorityZero(domainName, protocol),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckIBMDNSDomainExists("ibm_dns_domain.test_dns_domain_records", &dns_domain),
+					resource.TestCheckResourceAttr("ibm_dns_record.recordSRV", "protocol", "_udp"),
+					testAccCheckIBMDNSRecordExists("ibm_dns_record.recordSRV", &dns_domain_record),
+					resource.TestCheckResourceAttr("ibm_dns_record.recordSRV", "priority", "0"),
+					resource.TestCheckResourceAttr("ibm_dns_record.recordSRV", "weight", "0"),
+					resource.TestCheckResourceAttr("ibm_dns_record.recordSRV", "host", "hosta-srv.com"),
+					resource.TestCheckResourceAttr("ibm_dns_record.recordSRV", "responsible_person", "user@softlayer.com"),
+					resource.TestCheckResourceAttr("ibm_dns_record.recordSRV", "ttl", "900"),
+					resource.TestCheckResourceAttr("ibm_dns_record.recordSRV", "type", "srv"),
+					resource.TestCheckResourceAttr("ibm_dns_record.recordSRV", "port", "8080"),
+					resource.TestCheckResourceAttr("ibm_dns_record.recordSRV", "service", "_mail"),
+				),
+			},
+		},
+	})
+}
+
 func testAccCheckIBMDNSRecordExists(n string, dns_domain_record *datatypes.Dns_Domain_ResourceRecord) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		rs, ok := s.RootModule().Resources[n]
@@ -356,4 +388,26 @@ resource "ibm_dns_record" "recordMX" {
 	type = "mx"
 	mx_priority = 0
 }`, domainName, hostname)
+}
+
+func testAccCheckIBMDNSRecordSRVWithPriorityZero(domainName, protocol string) string {
+	return fmt.Sprintf(`
+resource "ibm_dns_domain" "test_dns_domain_records" {
+	name = "%s"
+	target = "172.16.0.100"
+}
+
+resource "ibm_dns_record" "recordSRV" {
+    data = "ns1.example.org"
+    domain_id = "${ibm_dns_domain.test_dns_domain_records.id}"
+    host = "hosta-srv.com"
+    responsible_person = "user@softlayer.com"
+    ttl = 900
+    type = "srv"
+	port = 8080
+	priority = 0
+	protocol = "%s"
+	weight = 0
+	service = "_mail"
+}`, domainName, protocol)
 }
