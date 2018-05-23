fb0bc28510b9115d0ac6860357fcefb142b18586
sumit-tembe-ibm <stembe02@in.ibm.com>
2018-05-23 11:49:52 +0530

    Multi Vlan Firewall support added.

--- vendor/github.com/softlayer/softlayer-go/helpers/product/product.go
+++ vendor/github.com/softlayer/softlayer-go/helpers/product/product.go
@@ -287,3 +287,33 @@ func GetPackageByKeyName(
 
 	return packages[0], nil
 }
+
+// GetPriceIdByPackageId return the priceids of the items according to the location group ids of the datacenter u specify
+func GetPriceIdByPackageId(addon string, listofpriceids []int, sess *session.Session, packageid int) (int, error) {
+	productpackageservice := services.GetProductPackageService(sess)
+	productpackageservicefilter := strings.Replace(`{"items":{"description":{"operation":"appliance"}}}`, "appliance", addon, -1)
+	productpackageservicemask := "description,prices.locationGroupId,prices.id"
+	resp, err := productpackageservice.Mask(productpackageservicemask).Filter(productpackageservicefilter).Id(packageid).GetItems()
+	if err != nil {
+		return 0, err
+	}
+	m := make(map[int]int)
+	if len(listofpriceids) == 0 {
+		listofpriceids = append(listofpriceids, 1)
+	}
+	for _, item := range listofpriceids {
+		for _, items := range resp {
+			for _, temp := range items.Prices {
+				if temp.LocationGroupId == nil {
+					m[item] = *temp.Id
+				} else if item == *temp.LocationGroupId {
+					m[*temp.LocationGroupId] = *temp.Id
+				}
+			}
+		}
+		if val, ok := m[item]; ok {
+			return val, nil
+		}
+	}
+	return 0, nil
+}
