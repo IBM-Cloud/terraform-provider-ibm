d92bfb91da268974704fc115c070b3b48ec8183c
Praveen K. Gostu <praveek9@in.ibm.com>
2018-07-23 14:30:50 +0530

    update the vlan and subnet id's

--- ibm/provider_test.go
+++ ibm/provider_test.go
@@ -62,115 +62,115 @@ func init() {
 	datacenter = os.Getenv("IBM_DATACENTER")
 	if datacenter == "" {
 		datacenter = "par01"
-		fmt.Println("[INFO] Set the environment variable IBM_DATACENTER for testing ibm_container_cluster resource else it is set to default value 'par01'")
+		fmt.Println("[WARN] Set the environment variable IBM_DATACENTER for testing ibm_container_cluster resource else it is set to default value 'par01'")
 	}
 
 	machineType = os.Getenv("IBM_MACHINE_TYPE")
 	if machineType == "" {
 		machineType = "u1c.2x4"
-		fmt.Println("[INFO] Set the environment variable IBM_MACHINE_TYPE for testing ibm_container_cluster resource else it is set to default value 'u1c.2x4'")
+		fmt.Println("[WARN] Set the environment variable IBM_MACHINE_TYPE for testing ibm_container_cluster resource else it is set to default value 'u1c.2x4'")
 	}
 
 	trustedMachineType = os.Getenv("IBM_TRUSTED_MACHINE_TYPE")
 	if trustedMachineType == "" {
 		trustedMachineType = "mb1c.16x64"
-		fmt.Println("[INFO] Set the environment variable IBM_TRUSTED_MACHINE_TYPE for testing ibm_container_cluster resource else it is set to default value 'mb1c.16x64'")
+		fmt.Println("[WARN] Set the environment variable IBM_TRUSTED_MACHINE_TYPE for testing ibm_container_cluster resource else it is set to default value 'mb1c.16x64'")
 	}
 
 	publicVlanID = os.Getenv("IBM_PUBLIC_VLAN_ID")
 	if publicVlanID == "" {
-		publicVlanID = "2350987"
-		fmt.Println("[INFO] Set the environment variable IBM_PUBLIC_VLAN_ID for testing ibm_container_cluster resource else it is set to default value '2350987'")
+		publicVlanID = "2393319"
+		fmt.Println("[WARN] Set the environment variable IBM_PUBLIC_VLAN_ID for testing ibm_container_cluster resource else it is set to default value '2393319'")
 	}
 
 	privateVlanID = os.Getenv("IBM_PRIVATE_VLAN_ID")
 	if privateVlanID == "" {
-		privateVlanID = "2350989"
-		fmt.Println("[INFO] Set the environment variable IBM_PRIVATE_VLAN_ID for testing ibm_container_cluster resource else it is set to default value '2350989'")
+		privateVlanID = "2393321"
+		fmt.Println("[WARN] Set the environment variable IBM_PRIVATE_VLAN_ID for testing ibm_container_cluster resource else it is set to default value '2393321'")
 	}
 
 	kubeVersion = os.Getenv("IBM_KUBE_VERSION")
 	if kubeVersion == "" {
 		kubeVersion = "1.9.8"
-		fmt.Println("[INFO] Set the environment variable IBM_KUBE_VERSION for testing ibm_container_cluster resource else it is set to default value '1.9.8'")
+		fmt.Println("[WARN] Set the environment variable IBM_KUBE_VERSION for testing ibm_container_cluster resource else it is set to default value '1.9.8'")
 	}
 
 	kubeUpdateVersion = os.Getenv("IBM_KUBE_UPDATE_VERSION")
 	if kubeUpdateVersion == "" {
 		kubeUpdateVersion = "1.10.3"
-		fmt.Println("[INFO] Set the environment variable IBM_KUBE_UPDATE_VERSION for testing ibm_container_cluster resource else it is set to default value '1.10.3'")
+		fmt.Println("[WARN] Set the environment variable IBM_KUBE_UPDATE_VERSION for testing ibm_container_cluster resource else it is set to default value '1.10.3'")
 	}
 
 	privateSubnetID = os.Getenv("IBM_PRIVATE_SUBNET_ID")
 	if privateSubnetID == "" {
-		privateSubnetID = "1571663"
-		fmt.Println("[INFO] Set the environment variable IBM_PRIVATE_SUBNET_ID for testing ibm_container_cluster resource else it is set to default value '1574951'")
+		privateSubnetID = "1636107"
+		fmt.Println("[WARN] Set the environment variable IBM_PRIVATE_SUBNET_ID for testing ibm_container_cluster resource else it is set to default value '1636107'")
 	}
 
 	publicSubnetID = os.Getenv("IBM_PUBLIC_SUBNET_ID")
 	if publicSubnetID == "" {
-		publicSubnetID = "1415689"
-		fmt.Println("[INFO] Set the environment variable IBM_PUBLIC_SUBNET_ID for testing ibm_container_cluster resource else it is set to default value '1415689'")
+		publicSubnetID = "1165645"
+		fmt.Println("[WARN] Set the environment variable IBM_PUBLIC_SUBNET_ID for testing ibm_container_cluster resource else it is set to default value '1165645'")
 	}
 
 	subnetID = os.Getenv("IBM_SUBNET_ID")
 	if subnetID == "" {
-		subnetID = "1415689"
-		fmt.Println("[INFO] Set the environment variable IBM_SUBNET_ID for testing ibm_container_cluster resource else it is set to default value '1415689'")
+		subnetID = "1165645"
+		fmt.Println("[WARN] Set the environment variable IBM_SUBNET_ID for testing ibm_container_cluster resource else it is set to default value '1165645'")
 	}
 
 	lbaasDatacenter = os.Getenv("IBM_LBAAS_DATACENTER")
 	if lbaasDatacenter == "" {
 		lbaasDatacenter = "wdc04"
-		fmt.Println("[INFO] Set the environment variable IBM_LBAAS_DATACENTER for testing ibm_lbaas resource else it is set to default value 'wdc04'")
+		fmt.Println("[WARN] Set the environment variable IBM_LBAAS_DATACENTER for testing ibm_lbaas resource else it is set to default value 'wdc04'")
 	}
 
 	lbaasSubnetId = os.Getenv("IBM_LBAAS_SUBNETID")
 	if lbaasSubnetId == "" {
 		lbaasSubnetId = "1511875"
-		fmt.Println("[INFO] Set the environment variable IBM_LBAAS_SUBNETID for testing ibm_lbaas resource else it is set to default value '1511875'")
+		fmt.Println("[WARN] Set the environment variable IBM_LBAAS_SUBNETID for testing ibm_lbaas resource else it is set to default value '1511875'")
 	}
 
 	dedicatedHostName = os.Getenv("IBM_DEDICATED_HOSTNAME")
 	if dedicatedHostName == "" {
 		dedicatedHostName = "terraform-dedicatedhost"
-		fmt.Println("[INFO] Set the environment variable IBM_DEDICATED_HOSTNAME for testing ibm_compute_vm_instance resource else it is set to default value 'terraform-dedicatedhost'")
+		fmt.Println("[WARN] Set the environment variable IBM_DEDICATED_HOSTNAME for testing ibm_compute_vm_instance resource else it is set to default value 'terraform-dedicatedhost'")
 	}
 
 	dedicatedHostID = os.Getenv("IBM_DEDICATED_HOST_ID")
 	if dedicatedHostID == "" {
 		dedicatedHostID = "30301"
-		fmt.Println("[INFO] Set the environment variable IBM_DEDICATED_HOST_ID for testing ibm_compute_vm_instance resource else it is set to default value '30301'")
+		fmt.Println("[WARN] Set the environment variable IBM_DEDICATED_HOST_ID for testing ibm_compute_vm_instance resource else it is set to default value '30301'")
 	}
 
 	zone = os.Getenv("IBM_WORKER_POOL_ZONE")
 	if zone == "" {
 		zone = "ams03"
-		fmt.Println("[INFO] Set the environment variable IBM_WORKER_POOL_ZONE for testing ibm_container_worker_pool_zone_attachment resource else it is set to default value 'ams03'")
+		fmt.Println("[WARN] Set the environment variable IBM_WORKER_POOL_ZONE for testing ibm_container_worker_pool_zone_attachment resource else it is set to default value 'ams03'")
 	}
 
 	zonePrivateVlan = os.Getenv("IBM_WORKER_POOL_ZONE_PRIVATE_VLAN")
 	if zonePrivateVlan == "" {
 		zonePrivateVlan = "1764491"
-		fmt.Println("[INFO] Set the environment variable IBM_WORKER_POOL_ZONE_PRIVATE_VLAN for testing ibm_container_worker_pool_zone_attachment resource else it is set to default value '1764491'")
+		fmt.Println("[WARN] Set the environment variable IBM_WORKER_POOL_ZONE_PRIVATE_VLAN for testing ibm_container_worker_pool_zone_attachment resource else it is set to default value '1764491'")
 	}
 
 	zonePublicVlan = os.Getenv("IBM_WORKER_POOL_ZONE_PUBLIC_VLAN")
 	if zonePublicVlan == "" {
 		zonePublicVlan = "1764435"
-		fmt.Println("[INFO] Set the environment variable IBM_WORKER_POOL_ZONE_PUBLIC_VLAN for testing ibm_container_worker_pool_zone_attachment resource else it is set to default value '1764435'")
+		fmt.Println("[WARN] Set the environment variable IBM_WORKER_POOL_ZONE_PUBLIC_VLAN for testing ibm_container_worker_pool_zone_attachment resource else it is set to default value '1764435'")
 	}
 
 	zoneUpdatePrivateVlan = os.Getenv("IBM_WORKER_POOL_ZONE_UPDATE_PRIVATE_VLAN")
 	if zoneUpdatePrivateVlan == "" {
-		zoneUpdatePrivateVlan = "2218971"
-		fmt.Println("[INFO] Set the environment variable IBM_WORKER_POOL_ZONE_UPDATE_PRIVATE_VLAN for testing ibm_container_worker_pool_zone_attachment resource else it is set to default value '2218971'")
+		zoneUpdatePrivateVlan = "2388377"
+		fmt.Println("[WARN] Set the environment variable IBM_WORKER_POOL_ZONE_UPDATE_PRIVATE_VLAN for testing ibm_container_worker_pool_zone_attachment resource else it is set to default value '2388377'")
 	}
 
 	zoneUpdatePublicVlan = os.Getenv("IBM_WORKER_POOL_ZONE_UPDATE_PUBLIC_VLAN")
 	if zoneUpdatePublicVlan == "" {
-		zoneUpdatePublicVlan = "2218969"
-		fmt.Println("[INFO] Set the environment variable IBM_WORKER_POOL_ZONE_UPDATE_PUBLIC_VLAN for testing ibm_container_worker_pool_zone_attachment resource else it is set to default value '2218969'")
+		zoneUpdatePublicVlan = "2388375"
+		fmt.Println("[WARN] Set the environment variable IBM_WORKER_POOL_ZONE_UPDATE_PUBLIC_VLAN for testing ibm_container_worker_pool_zone_attachment resource else it is set to default value '2388375'")
 	}
 
 }
