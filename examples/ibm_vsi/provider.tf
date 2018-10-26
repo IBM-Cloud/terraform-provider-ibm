f94f00e21afe6a037cb264ae2f50717e2d303fe4
stevestrutt <steve_strutt@uk.ibm.com>
2018-10-26 22:30:40 +0100

    Update provider.tf

--- examples/ibm_vsi/provider.tf
+++ examples/ibm_vsi/provider.tf
@@ -6,12 +6,12 @@ variable "softlayer_api_key" {
   description = "Enter your IBM Infrastructure (SoftLayer) API key, you can get this using: https://control.bluemix.net/account/user/profile"
 }
 
-variable "ibmcloud_api_key" {
+variable "bluemix_api_key" {
   description = "Enter your IBM Cloud API Key, you can get your IBM Cloud API key using: https://console.bluemix.net/iam#/apikeys"
 }
 
 provider "ibm" {
   softlayer_username = "${var.softlayer_username}"
   softlayer_api_key  = "${var.softlayer_api_key}"
-  bluemix_api_key    = "${var.ibmcloud_api_key}"
+  bluemix_api_key    = "${var.bluemix_api_key}"
 }
