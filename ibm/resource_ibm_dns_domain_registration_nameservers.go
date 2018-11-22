281ad5a6e33c5d8cffb56fd5ff8f3d8720930796
STEPHEN STRUTT <steve_strutt@uk.ibm.com>
2018-11-22 10:47:03 +0000

    dns reg with delete

--- ibm/resource_ibm_dns_domain_registration_nameservers.go
+++ ibm/resource_ibm_dns_domain_registration_nameservers.go
@@ -29,6 +29,14 @@ func resourceIBMDNSDomainRegistrationNameservers() *schema.Resource {
 					Type: schema.TypeString,
 				},
 			},
+			"original_name_servers": &schema.Schema{
+				Description: "Save of name servers prior to update",
+				Type:        schema.TypeSet,
+				Computed:    true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
 		},
 	}
 }
@@ -103,7 +111,7 @@ func resourceIBMDNSDomainRegistrationNSCreate(d *schema.ResourceData, meta inter
 	for _, oldNs := range oldNameServers {
 		found := false
 		for _, newNs := range newNameServers {
-			log.Printf("old %s, new %s", oldNs, newNs)
+			log.Printf("old %s, new %s", oldNs, newNs.(string))
 			if oldNs == newNs {
 				found = true
 				break
@@ -127,7 +135,9 @@ func resourceIBMDNSDomainRegistrationNSCreate(d *schema.ResourceData, meta inter
 	_, _ = nService.Id(dnsId).LockDomain()
 	// Ignore lock errors as does not impact operation
 
+	// save the original name servers now as not available on read
 	d.SetId(fmt.Sprintf("%d", dnsId))
+	d.Set("original_name_servers", oldNameServers)
 	return resourceIBMDNSDomainRegistrationNSRead(d, meta)
 }
 
@@ -173,6 +183,82 @@ func resourceIBMDNSDomainRegistrationNSUpdate(d *schema.ResourceData, meta inter
 
 // No delete on IBM Cloud
 func resourceIBMDNSDomainRegistrationNSDelete(d *schema.ResourceData, meta interface{}) error {
+	// Exact reverse of create to restore name servers back to original values
+	sess := meta.(ClientSession).SoftLayerSession()
+	nService := services.GetDnsDomainRegistrationService(sess)
+	dnsId, _ := strconv.Atoi(d.Get("dns_registration_id").(string))
+	currentNameServers := d.Get("name_servers").(*schema.Set).List()
+	originalNameServers := d.Get("original_name_servers").(*schema.Set).List()
+
+	// original NS to restore, if not found in current list
+	var addNs []string
+	for _, originalNs := range originalNameServers {
+		found := false
+		for _, currentNs := range currentNameServers {
+			log.Printf("current %s, original %s", currentNs, originalNs)
+
+			if currentNs == originalNs {
+
+				found = true
+				break
+			}
+		}
+		if found == false {
+			addNs = append(addNs, originalNs.(string))
+		}
+	}
+	log.Printf("Name servers to restore %v\n", addNs)
+
+	// if no name servers to add then, we already have the correct name servers.
+	// So return at this point.
+	if len(addNs) == 0 {
+		d.SetId(fmt.Sprintf("%d", dnsId))
+		return resourceIBMDNSDomainRegistrationNSRead(d, meta)
+	}
+
+	nsUnlock_res, err := nService.Id(dnsId).
+		UnlockDomain()
+	if err != nil || nsUnlock_res != true {
+		return fmt.Errorf("Error unlocking domain registration record: %s", err)
+	}
+
+	nsAdd_res := false
+	nsAdd_res, err = nService.Id(dnsId).
+		AddNameserversToDomain(addNs)
+
+	if err != nil || nsAdd_res != true {
+		return fmt.Errorf("Error Adding name servers to record: %s", err)
+	}
+
+	// current NS to delete, if not found in original list
+	var delNs []string
+	for _, currentNs := range currentNameServers {
+		found := false
+		for _, originalNs := range originalNameServers {
+			log.Printf("current %s, original %s", currentNs, originalNs)
+			if currentNs == originalNs {
+				found = true
+				break
+			}
+		}
+		if found == false {
+			delNs = append(delNs, currentNs.(string))
+		}
+	}
+
+	log.Printf("Name servers to delete %v\n", delNs)
+
+	nsDel_res := false
+	nsDel_res, err = nService.Id(dnsId).
+		RemoveNameserversFromDomain(delNs)
+
+	if err != nil || nsDel_res != true {
+		return fmt.Errorf("Error Deleting name servers from record: %s", err)
+	}
+
+	_, _ = nService.Id(dnsId).LockDomain()
+	// Ignore lock errors as does not impact operation
+
 	d.SetId("")
 	return nil
 }
