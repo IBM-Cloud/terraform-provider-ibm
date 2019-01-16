9e4ae9e7e59669239e1f3856d63fd46fb7ff7577
Sagar <sagakale@in.ibm.com>
2019-01-16 14:08:30 +0530

    modified UT

--- ibm/resource_ibm_ssl_certificate_test.go
+++ ibm/resource_ibm_ssl_certificate_test.go
@@ -16,15 +16,15 @@ func TestAccIBMSSLCertificate_Basic(t *testing.T) {
 
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(
-						"ibm_ssl_certificate.myssllll", "orderApproverEmailAddress", "admin@pune.in"),
+						"ibm_ssl_certificate.myssllll", "order_approver_email_address", "admin@pune.in"),
 					resource.TestCheckResourceAttr(
-						"ibm_ssl_certificate.myssllll", "serverType", "apache2"),
+						"ibm_ssl_certificate.myssllll", "server_type", "apache2"),
 					resource.TestCheckResourceAttr(
-						"ibm_ssl_certificate.myssllll", "serverCount", "1"),
+						"ibm_ssl_certificate.myssllll", "server_count", "1"),
 					resource.TestCheckResourceAttr(
-						"ibm_ssl_certificate.myssllll", "validityMonths", "24"),
+						"ibm_ssl_certificate.myssllll", "validity_months", "24"),
 					resource.TestCheckResourceAttr(
-						"ibm_ssl_certificate.myssllll", "sslType", "SSL_CERTIFICATE_QUICKSSL_PREMIUM_2_YEAR"),
+						"ibm_ssl_certificate.myssllll", "ssl_type", "SSL_CERTIFICATE_QUICKSSL_PREMIUM_2_YEAR"),
 				),
 			},
 		},
@@ -33,75 +33,81 @@ func TestAccIBMSSLCertificate_Basic(t *testing.T) {
 
 var testAccIBMSSLCertificateConfig_basic = `
 resource "ibm_ssl_certificate" "my_ssllllll" {
-	certificateSigningRequest= "-----BEGIN CERTIFICATE REQUEST-----\nMIIC2jCCAcICAQAwgYAxCzAJBgNVBAYTAklOMRMwEQYDVQQIDApNYWhhcmFzaHRh\nMQ0wCwYDVQQHDARQdW5lMRAwDgYDVQQKDAdJQk1QdW5lMQwwCgYDVQQLDANJQk0x\nFDASBgNVBAMMC2libS5wdW5lLmluMRcwFQYJKoZIhvcNAQkBFghyYkBncy5pbjCC\nASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBALZTfLDkU/q8ory+a+aBIAB7\nezyMbF4iqEpO3mVmlIfENBRq/MomhTiYID2jVWhPS4KytXa9bANgAG+NN5zMxqgH\nwUVa4fO2Th//o9RdRPZQcC3IGR+b3tpGSV+KxjsW9XV82lQp6l8HxfSBo12PZcwU\nlu53BXMiVKU6ZHSjz6eDF4MGTuPM1QWs/oexb9y6Yqqj2IicrkviJMuKhh0FN6H7\nXT2MHIF8OHT1xhs8w7U1SdUwhBa5dHvj7vqi33DU0p3s8JdXlzE9PUXKuAzXav1D\nngwE1iT5KFHkEFUr7plH5wGsy/y4tnu2xVzfGMyECS9EYmUtOA5M5RpqF5DdXGEC\nAwEAAaAUMBIGCSqGSIb3DQEJAjEFDANnc2wwDQYJKoZIhvcNAQELBQADggEBAGB7\n3lv/6fSn9rgTiHszLL9pOU9ytjOVrhNjFjDzQL73VQ0+Isb7aPHnWrLz4kT9m/60\nmgy/dHsOIF8KRP1LpOs5BYwlstD3Ss57XR8GatnrLMN4lZCjacL6A8RPhwr3x29W\nMyFntvu2caAL4ZpZpWMKHtoemXijCiFXa9Z4pZFBk4V7k0/DIEXEeyYazsSaXeTw\nXr4IFPmk7VS/NkLAht2hRhllN5NHGf/gzTsmgrgKclXtf1Z7EotnDTTIt0dFVtk1\nVX2Z7kvx9/QWbDVhPEz2uOrJnCoAm+0OpQfFc4THcP0uv0Y49B3WUG89mAjlWQKa\nU7hhc8gZ77+eaBQKD6k=\n-----END CERTIFICATE REQUEST-----"
-  organizationInformation = {
+	certificate_signing_request= "-----BEGIN CERTIFICATE REQUEST-----\nMIIC2jCCAcICAQAwgYAxCzAJBgNVBAYTAklOMRMwEQYDVQQIDApNYWhhcmFzaHRh\nMQ0wCwYDVQQHDARQdW5lMRAwDgYDVQQKDAdJQk1QdW5lMQwwCgYDVQQLDANJQk0x\nFDASBgNVBAMMC2libS5wdW5lLmluMRcwFQYJKoZIhvcNAQkBFghyYkBncy5pbjCC\nASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBALZTfLDkU/q8ory+a+aBIAB7\nezyMbF4iqEpO3mVmlIfENBRq/MomhTiYID2jVWhPS4KytXa9bANgAG+NN5zMxqgH\nwUVa4fO2Th//o9RdRPZQcC3IGR+b3tpGSV+KxjsW9XV82lQp6l8HxfSBo12PZcwU\nlu53BXMiVKU6ZHSjz6eDF4MGTuPM1QWs/oexb9y6Yqqj2IicrkviJMuKhh0FN6H7\nXT2MHIF8OHT1xhs8w7U1SdUwhBa5dHvj7vqi33DU0p3s8JdXlzE9PUXKuAzXav1D\nngwE1iT5KFHkEFUr7plH5wGsy/y4tnu2xVzfGMyECS9EYmUtOA5M5RpqF5DdXGEC\nAwEAAaAUMBIGCSqGSIb3DQEJAjEFDANnc2wwDQYJKoZIhvcNAQELBQADggEBAGB7\n3lv/6fSn9rgTiHszLL9pOU9ytjOVrhNjFjDzQL73VQ0+Isb7aPHnWrLz4kT9m/60\nmgy/dHsOIF8KRP1LpOs5BYwlstD3Ss57XR8GatnrLMN4lZCjacL6A8RPhwr3x29W\nMyFntvu2caAL4ZpZpWMKHtoemXijCiFXa9Z4pZFBk4V7k0/DIEXEeyYazsSaXeTw\nXr4IFPmk7VS/NkLAht2hRhllN5NHGf/gzTsmgrgKclXtf1Z7EotnDTTIt0dFVtk1\nVX2Z7kvx9/QWbDVhPEz2uOrJnCoAm+0OpQfFc4THcP0uv0Y49B3WUG89mAjlWQKa\nU7hhc8gZ77+eaBQKD6k=\n-----END CERTIFICATE REQUEST-----"
+  organization_information = {
 	  org_address = {
-		  org_addressLine1= "abc"
-		  org_addressLine2= "cms"
-		  org_city="xyz"
-		  org_countryCode= "AF"
-		  org_state="OT"
-		  org_postalCode= "411119"
+		  org_address_line1= "abc"
+		  org_address_line2= "xyz"
+		  org_city="pune"
+		  org_country_code= "IN"
+		  org_state="MH"
+		  org_postal_code= "411045"
 	  }
-	  org_organizationName= "IBMPune"
-	  org_phoneNumber= "9876543210"
+	  org_organization_name= "GSLAB"
+	  org_phone_number= "8657072955"
+	  org_fax_number = ""
   }	
-  technicalContactSameAsOrgAddressFlag = "false"
-  technicalContact = {
+  technical_contact_same_as_org_address_flag = "false"
+  technical_contact = {
 	  tech_address = {
-		  tech_addressLine1= "Baner"
-		  tech_addressLine2= "Mahalunge"
-		  tech_city="Pune"
-		  tech_countryCode= "AF"
-		  tech_state="OT"
-		  tech_postalCode= "411007"
+		  tech_address_line1= "fcb"
+		  tech_address_line2= "pqr"
+		  tech_city="pune"
+		  tech_country_code= "IN"
+		  tech_state="MH"
+		  tech_postal_code= "411045"
 	  }
-	  tech_organizationName= "IBMPune"
-	  tech_phoneNumber= "952741928"
-	  tech_firstName = "poy"
-	  tech_lastName = "joy"
-	  tech_emailAddress = "email.prefix@yahoo.com"
-	  tech_title= "something"
+	  tech_organization_name= "IBM"
+	  tech_phone_number= "8657072955"
+	  tech_fax_number = ""
+	  tech_first_name = "qwerty"
+	  tech_last_name = "ytrewq"
+	  tech_email_address = "abc@gmail.com"
+	  tech_title= "SSL CERT"
   }
-  billingContact = {
+  billing_contact = {
 	  billing_address = {
-		  billing_addressLine1= "Baner"
-		  billing_addressLine2= "Mahalunge"
-		  billing_city="Pune"
-		  billing_countryCode= "AF"
-		  billing_state="OT"
-		  billing_postalCode= "411007"
+		  billing_address_line1= "plk"
+		  billing_address_line2= "PLO"
+		  billing_city="PUNE"
+		  billing_country_code= "IN"
+		  billing_state="MH"
+		  billing_postal_code= "411045"
 	  }
-	  billing_organizationName= "IBMPune"
-	  billing_phoneNumber= "9876542102"
-	  billing_firstName = "pqr"
-	  billing_lastName = "qwe"
-	  billing_emailAddress = "email@domain.com"
-	  billing_title= "plzeru"
+	  billing_organization_name= "IBM"
+	  billing_phone_number= "8657072955"
+	  billing_fax_number = ""
+	  billing_first_name = "ERTYU"
+	  billing_last_name = "SDFGHJK"
+	  billing_email_address = "kjjj@gsd.com"
+	  billing_title= "PFGHJK"
   }
-  administrativeContact = {
+  administrative_contact = {
 	  admin_address = {
-		  admin_addressLine1= "Baner"
-		  admin_addressLine2= "Mahalunge"
-		  admin_city="Pune"
-		  admin_countryCode= "AF"
-		  admin_state="OT"
-		  admin_postalCode= "411007"
+		  admin_address_line1= "fghds"
+		  admin_address_line2= "twyu"
+		  admin_city="pune"
+		  admin_country_code= "IN"
+		  admin_state="MH"
+		  admin_postal_code= "411045"
 	  }
-	  admin_organizationName= "IBMPune"
-	  admin_phoneNumber= "952741928"
-	  admin_firstName = "lkj"
-	  admin_lastName = "ply"
-	  admin_emailAddress = "email.prefix@domain.com"
-	  admin_title= "see"
-  }
-  administrativeContactSameAsTechnicalFlag = "false"
-  billingContactSameAsTechnicalFlag = "false"	
-  sslType="SSL_CERTIFICATE_QUICKSSL_PREMIUM_2_YEAR"
-  renewalFlag= true
-  serverCount= 1
-  serverType= "apache2"
-  validityMonths= 24
-  orderApproverEmailAddress= "admin@pune.in"	
+	  admin_organization_name = "GSLAB"
+	  admin_phone_number = "8657072955"
+	  admin_fax_number = ""
+	  admin_first_name = "DFGHJ"
+	  admin_last_name = "dfghjkl"
+	  admin_email_address = "fghjk@gshhds.com"
+	  admin_title= "POIUYGHJK"
+  }	
+  administrative_contact_same_as_technical_flag = "false"
+  billing_contact_same_as_technical_flag = "false"	
+  billing_address_same_as_organization_flag = "false"
+  administrative_address_same_as_organization_flag = "false"
+  ssl_type="SSL_CERTIFICATE_QUICKSSL_PREMIUM_2_YEAR"
+  renewal_flag= true
+  server_count= 1
+  server_type= "apache2"
+  validity_months= 24
+  order_approver_email_address= "admin@pune.in"	
 }
 `
