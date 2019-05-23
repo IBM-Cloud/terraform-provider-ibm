39014884d69db9425c92363e89383b38bba01fbe
sagarkale1 <36848920+sagarkale1@users.noreply.github.com>
2019-05-23 10:58:08 +0530

    added resource for ssl certificate order (#478)
    * added resource for ssl certificate order
    
    * added documentation for ordering ssl certificate
    
    * modified doc for ssl certificate
    
    * added ssl certificate resource entry in provider.go
    
    * modified doc
    
    * modified UT
    
    * updated documentation
    
    * added suggested changes

--- ibm/provider.go
+++ ibm/provider.go
@@ -201,6 +201,7 @@ func Provider() terraform.ResourceProvider {
 			"ibm_storage_block":                         resourceIBMStorageBlock(),
 			"ibm_storage_file":                          resourceIBMStorageFile(),
 			"ibm_subnet":                                resourceIBMSubnet(),
+			"ibm_ssl_certificate":                       resourceIBMSSLCertificate(),
 			"ibm_cdn":                                   resourceIBMCDN(),
 			"ibm_hardware_firewall_shared":              resourceIBMFirewallShared(),
 		},
