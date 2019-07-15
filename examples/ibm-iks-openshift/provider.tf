d79c418b34bbc1fe05b2745864fcb6c324a883fe
hkantare@in.ibm.com <hkantare@in.ibm.com>
2019-07-15 18:00:30 +0530

    Add example for IKS openshift

---
+++ examples/ibm-iks-openshift/provider.tf
@@ -0,0 +1,8 @@
+variable "ibmcloud_api_key" {
+  description = "Enter your IBM Cloud API Key, you can get your IBM Cloud API key using: https://console.bluemix.net/iam#/apikeys"
+}
+
+provider "ibm" {
+  ibmcloud_api_key    = "${var.ibmcloud_api_key}"
+  region             = "us-east"
+}
---
+++ examples/ibm-iks-openshift/terraform.tfvars
@@ -0,0 +1,3 @@
+# Enter your IBM Cloud API Key, you can get your IBM Cloud API key using: https://console.bluemix.net/iam#/apikeys
+ibmcloud_api_key = ""
+
