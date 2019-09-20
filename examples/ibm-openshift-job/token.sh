a90d250e12432333d04b91b406eff62752abe831
Soumyajit  Das <soumyajitdas@soumyajitsmbp2.c4p-in.ibmmobiledemo.com>
2019-09-20 16:30:09 +0530

    api changes

--- examples/ibm-openshift-job/token.sh
+++ examples/ibm-openshift-job/token.sh
@@ -5,7 +5,7 @@ INSTALL_CMD="apk install -q"
 
   test -f $(which jq) || ${INSTALL_CMD} jq
   eval "$(jq -r '@sh "export SERVER_URL=\(.server_url)"')"
-  TOKEN=$(curl -v -u apikey:u-HgIg6wJkEI83GiOyozObIIZImLkUgv8Nr-TwlWY6nR "https://c100-e.us-east.containers.cloud.ibm.com:30129/oauth/authorize?client_id=openshift-challenging-client&response_type=token" \
+  TOKEN=$(curl -v -u apikey:<your api_key> "https://c100-e.us-east.containers.cloud.ibm.com:30129/oauth/authorize?client_id=openshift-challenging-client&response_type=token" \
     -skv -H "X-CSRF-Token: xxx" --stderr - |  grep Location: | sed -E 's/(.*)(access_token=)([^&]*)(&.*)/\3/')
   jq -n \
     --arg token "$TOKEN" \
