976ab23102dbd3eb1e70900c604c1042de142873
vaishnavi <ggunishetty9@gmail.com>
2019-10-18 15:09:52 +0530

    Added examples for vpc next gen resources

---
+++ examples/ibm-is-ng/variables.tf
@@ -0,0 +1,15 @@
+variable "zone1" {
+  default = "us-south-1"
+}
+
+variable "ssh_public_key" {
+  default = ""
+}
+
+variable "image" {
+  default = "99edcc54-c513-4d46-9f5b-36243a1e50e2"
+}
+
+variable "profile" {
+  default = "cx2-2x4"
+}
