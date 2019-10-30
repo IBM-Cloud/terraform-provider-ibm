3b3f48be01a318d0d73eeef6a894b3d51ffab889
n-marton <marton.natko1@ibm.com>
2019-10-30 21:25:31 +0100

    ibm cos bucket as data source

---
+++ ibm/data_source_ibm_cos_bucket.go
@@ -0,0 +1,190 @@
+package ibm
+
+import (
+	"fmt"
+	"log"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/IBM/ibm-cos-sdk-go/aws"
+	"github.com/IBM/ibm-cos-sdk-go/aws/credentials/ibmiam"
+	token "github.com/IBM/ibm-cos-sdk-go/aws/credentials/ibmiam/token"
+	"github.com/IBM/ibm-cos-sdk-go/aws/session"
+	"github.com/IBM/ibm-cos-sdk-go/service/s3"
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+var bucketTypes = []string{"single_site_location", "region_location", "cross_region_location"}
+
+func dataSourceIBMCosBucket() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceIBMCosBucketRead,
+
+		Schema: map[string]*schema.Schema{
+			"bucket_name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"bucket_type": &schema.Schema{
+				Type:         schema.TypeString,
+				ValidateFunc: validateAllowedStringValue(bucketTypes),
+				Required:     true,
+			},
+			"region": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"resource_instance_id": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"crn": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "CRN of resource instance",
+			},
+			"key_protect": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "CRN of the key you want to use data at rest encryption",
+			},
+			"single_site_location": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"region_location": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"cross_region_location": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"storage_class": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"s3_endpoint_public": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Public endpoint for the COS bucket",
+			},
+			"s3_endpoint_private": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Private endpoint for the COS bucket",
+			},
+		},
+	}
+}
+
+func dataSourceIBMCosBucketRead(d *schema.ResourceData, meta interface{}) error {
+	var s3Conf *aws.Config
+	rsConClient, err := meta.(ClientSession).BluemixSession()
+	if err != nil {
+		return err
+	}
+	bucketName := d.Get("bucket_name").(string)
+	serviceID := d.Get("resource_instance_id").(string)
+	bucketType := d.Get("bucket_type").(string)
+	bucketRegion := d.Get("region").(string)
+
+	apiEndpoint, apiEndpointPrivate := selectCosApi(bucketLocationConvert(bucketType), bucketRegion)
+	log.Println("!!!!!!!!!!!!!")
+	log.Println(apiEndpoint)
+	authEndpoint, err := rsConClient.Config.EndpointLocator.IAMEndpoint()
+	if err != nil {
+		return err
+	}
+	authEndpointPath := fmt.Sprintf("%s%s", authEndpoint, "/identity/token")
+	apiKey := rsConClient.Config.BluemixAPIKey
+	if apiKey != "" {
+		s3Conf = aws.NewConfig().WithEndpoint(apiEndpoint).WithCredentials(ibmiam.NewStaticCredentials(aws.NewConfig(), authEndpointPath, apiKey, serviceID)).WithS3ForcePathStyle(true)
+	}
+	iamAccessToken := rsConClient.Config.IAMAccessToken
+	if iamAccessToken != "" {
+		initFunc := func() (*token.Token, error) {
+			return &token.Token{
+				AccessToken:  rsConClient.Config.IAMAccessToken,
+				RefreshToken: rsConClient.Config.IAMRefreshToken,
+				TokenType:    "Bearer",
+				ExpiresIn:    int64((time.Hour * 248).Seconds()) * -1,
+				Expiration:   time.Now().Add(-1 * time.Hour).Unix(),
+			}, nil
+		}
+		s3Conf = aws.NewConfig().WithEndpoint(apiEndpoint).WithCredentials(ibmiam.NewCustomInitFuncCredentials(aws.NewConfig(), initFunc, authEndpointPath, serviceID)).WithS3ForcePathStyle(true)
+	}
+	s3Sess := session.Must(session.NewSession())
+	s3Client := s3.New(s3Sess, s3Conf)
+
+	headInput := &s3.HeadBucketInput{
+		Bucket: aws.String(bucketName),
+	}
+	err = s3Client.WaitUntilBucketExists(headInput)
+	if err != nil {
+		return fmt.Errorf("failed waiting for bucket %s to be created, %v",
+			bucketName, err)
+	}
+	bucketLocationInput := &s3.GetBucketLocationInput{
+		Bucket: aws.String(bucketName),
+	}
+	bucketLocationConstraint, err := s3Client.GetBucketLocation(bucketLocationInput)
+	if err != nil {
+		return err
+	}
+	bLocationConstraint := *bucketLocationConstraint.LocationConstraint
+
+	singleSiteLocationRegex, err := regexp.Compile("^[a-z]{3}[0-9][0-9]-[a-z]{4,8}$")
+	if err != nil {
+		return err
+	}
+	regionLocationRegex, err := regexp.Compile("^[a-z]{2}-[a-z]{2,5}-[a-z]{4,8}$")
+	if err != nil {
+		return err
+	}
+	crossRegionLocationRegex, err := regexp.Compile("^[a-z]{2}-[a-z]{4,8}$")
+	if err != nil {
+		return err
+	}
+
+	if singleSiteLocationRegex.MatchString(bLocationConstraint) {
+		d.Set("single_site_location", strings.Split(bLocationConstraint, "-")[0])
+		d.Set("storage_class", strings.Split(bLocationConstraint, "-")[1])
+	}
+	if regionLocationRegex.MatchString(bLocationConstraint) {
+		d.Set("region_location", fmt.Sprintf("%s-%s", strings.Split(bLocationConstraint, "-")[0], strings.Split(bLocationConstraint, "-")[1]))
+		d.Set("storage_class", strings.Split(bLocationConstraint, "-")[2])
+	}
+	if crossRegionLocationRegex.MatchString(bLocationConstraint) {
+		d.Set("cross_region_location", strings.Split(bLocationConstraint, "-")[0])
+		d.Set("storage_class", strings.Split(bLocationConstraint, "-")[1])
+	}
+
+	head, err := s3Client.HeadBucket(headInput)
+	if err != nil {
+		return err
+	}
+	bucketID := fmt.Sprintf("%s:%s:%s:meta:%s:%s", strings.Replace(serviceID, "::", "", -1), "bucket", bucketName, bucketLocationConvert(bucketType), bucketRegion)
+	d.SetId(bucketID)
+	d.Set("key_protect", head.IBMSSEKPCrkId)
+	bucketCRN := fmt.Sprintf("%s:%s:%s", strings.Replace(serviceID, "::", "", -1), "bucket", bucketName)
+	d.Set("crn", bucketCRN)
+	d.Set("resource_instance_id", serviceID)
+	d.Set("s3_endpoint_public", apiEndpoint)
+	d.Set("s3_endpoint_private", apiEndpointPrivate)
+	return nil
+}
+
+func bucketLocationConvert(locationtype string) string {
+	if locationtype == "cross_region_location" {
+		return "crl"
+	}
+	if locationtype == "region_location" {
+		return "rl"
+	}
+	if locationtype == "single_site_location" {
+		return "crl"
+	}
+	return ""
+}
---
+++ website/docs/d/cos_bucket.html.markdown
@@ -0,0 +1,58 @@
+---
+layout: "ibm"
+page_title: "IBM : Cloud Object Storage Bucket"
+sidebar_current: "docs-ibm-datasource-cos-bucket"
+description: |-
+  Get information about IBM CloudObject Storage Bucket.
+---
+
+# ibm\_cos_bucket
+
+Get information about already existing buckets.
+
+## Example Usage
+
+```hcl
+data "ibm_resource_group" "cos_group" {
+  name = "cos-resource-group"
+}
+
+data "ibm_resource_instance" "cos_instance" {
+  name              = "cos-instance"
+  resource_group_id = "${data.ibm_resource_group.cos_group.id}"
+  service           = "cloud-object-storage"
+}
+
+data "ibm_cos_bucket" "standard-ams03" {
+  bucket_name = "a-standard-bucket-at-ams"
+  resource_instance_id = "${data.ibm_resource_instance.cos_instance.id}"
+  bucket_type = "single_site_location"
+  region = "ams03"
+}
+
+output "bucket_private_endpoint" {
+  value = "${data.ibm_cos_bucket.standard-ams03.s3_endpoint_private}"
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `bucket_name` - (Required, string) The name of the bucket.
+* `bucket_type` - (Required, string) The type of the bucket. Accepted values: single_site_location region_location cross_region_location
+* `resource_instance_id` - (Required, string) The id of Cloud Object Storage instance.
+* `region` - (Required, string) The region of the bucket.
+
+## Attribute Reference
+
+The following attributes are exported:
+
+* `id` - The ID of the bucket.
+* `crn` - The CRN of the bucket.
+* `resource_instance_id` - The id of Cloud Object Storage instance.
+* `key_protect` - CRN of the Key Protect instance where there a root key is already provisioned.
+* `single_site_location` - Location if single site bucket was created.
+* `region_location` - Location if regional bucket was created.
+* `cross_region_location` - Location if cross regional bucket was created.
+* `storage_class` - Storage class of the bucket.
