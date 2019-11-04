ea14a404ac56bebafccac058307727233f075eac
hkantare@in.ibm.com <hkantare@in.ibm.com>
2019-11-04 14:19:46 +0530

    Remove otherexampleS

--- examples/data-ibm-pi-images/variables.tf
+++
@@ -1,8 +0,0 @@
-
-variable "powerinstanceid"
-{
-description="Power Instance associated with the account"
-#default="49fba6c9-23f8-40bc-9899-aca322ee7d5b"
-default="d16705bd-7f1a-48c9-9e0e-1c17b71e7331"
-#default="d7d4b40a-b06f-40a9-aec0-7ccd144cfa7b"
-}
--- examples/ibm-compute-public-ip/output.tf
+++
@@ -1,3 +0,0 @@
-output "global ip"{
-  value = "http://${ibm_network_public_ip.test-global-ip.ip_address}"
-}
