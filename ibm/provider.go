d819081919b6450828b37023327d4ad8cba192a7
Kavya.Handadi <kavya.handadi@gmail.com>
2019-11-12 18:22:07 +0530

    support for iam-auth-token data source

--- ibm/provider.go
+++ ibm/provider.go
@@ -174,6 +174,7 @@ func Provider() terraform.ResourceProvider {
 			"ibm_dns_domain_registration":      dataSourceIBMDNSDomainRegistration(),
 			"ibm_dns_domain":                   dataSourceIBMDNSDomain(),
 			"ibm_dns_secondary":                dataSourceIBMDNSSecondary(),
+			"ibm_iam_auth_token":               dataSourceIBMIAMAuthToken(),
 			"ibm_iam_user_policy":              dataSourceIBMIAMUserPolicy(),
 			"ibm_iam_service_id":               dataSourceIBMIAMServiceID(),
 			"ibm_iam_service_policy":           dataSourceIBMIAMServicePolicy(),
