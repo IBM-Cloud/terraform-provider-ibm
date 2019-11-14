e003c45f0f928737a09b5c268febc1655d39f3a9
umar ali <umarali@umars-mbp.c4p-in.ibmmobiledemo.com>
2019-11-14 12:09:44 +0530

    cluster examples adaptation to v0.12

--- examples/ibm-cluster/cluster-worker-pool-zone/variables.tf
+++ examples/ibm-cluster/cluster-worker-pool-zone/variables.tf
@@ -1,36 +1,45 @@
 variable "org" {
+  
 }
 
 variable "space" {
+  
 }
 
 variable "region" {
+  
 }
 
 variable "datacenter" {
+  
 }
 
 variable "machine_type" {
+  
 }
 
 variable "isolation" {
+  
 }
 
 variable "private_vlan_id" {
+  
 }
 
 variable "public_vlan_id" {
+  
 }
 
 variable "subnet_id" {
+  
 }
 
 variable "service_instance_name" {
-  default = "myservice"
+  default = "myservice4"
 }
 
 variable "service_key" {
-  default = "myservicekey"
+  default = "myservicekey4"
 }
 
 variable "service_offering" {
@@ -42,19 +51,22 @@ variable "plan" {
 }
 
 variable "cluster_name" {
-  default = "cluster"
+  default = "cluster4"
 }
 
 variable "worker_pool_name" {
-  default = "worker_pool"
+  default = "worker_pool4"
 }
 
 variable "zone" {
+  
 }
 
 variable "zone_private_vlan_id" {
+  
 }
 
 variable "zone_public_vlan_id" {
+  
 }
 
