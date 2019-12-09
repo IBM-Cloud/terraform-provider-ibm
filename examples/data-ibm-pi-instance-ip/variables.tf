034b5f4a9e6d61f706da490cbcb760ee3b79df80
suraj <suraj@us.ibm.com>
2019-12-09 16:10:46 -0600

    Another update to fix build issues

---
+++ examples/data-ibm-pi-instance-ip/variables.tf
@@ -0,0 +1,18 @@
+
+variable "networkname"
+{
+description="Name of the Network" 
+}
+
+variable "instancename"
+{
+description="PVM Instance Name"
+}
+
+
+variable "powerinstanceid"
+{
+description="Power Instance associated with the account"
+default="59b8fc31-1e13-4f5e-a5dc-c8a999f59647"
+#default="d7d4b40a-b06f-40a9-aec0-7ccd144cfa7b"
+}
--- examples/ibm-power-image/main.tf
+++
@@ -1,8 +0,0 @@
-data "ibm_power_image" "powerimages"{
-  name="${var.image_name}"
-}
-
-output "state"
-{
-value="${data.ibm_power_image.powerimages.state}"
-}
