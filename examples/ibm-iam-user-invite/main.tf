df3d3118104479a0d8cb3b07826a76a4b0e9340d
AnilCM <amallak1@in.ibm.com>
2019-12-09 15:16:59 +0530

    added support for acces groups in user invite resource

--- examples/ibm-iam-user-invite/main.tf
+++ examples/ibm-iam-user-invite/main.tf
@@ -1,6 +1,25 @@
 provider "ibm" {}
 
-resource "ibm_iam_user_invite" "invite_user" {
-    users = ["${var.user1}", "${var.user2}"]
+resource "ibm_iam_access_group" "accgrp" {
+  name        = "test"
+  description = "New access group"
 }
 
+data "ibm_resource_group" "group" {
+  name = "default"
+}
+
+resource "ibm_iam_access_group_policy" "policy" {
+  access_group_id = "${ibm_iam_access_group.accgrp.id}"
+  roles        = ["Operator", "Writer"]
+
+  resources = [{
+    resource_group_id = "${data.ibm_resource_group.group.id}"
+  }]
+}
+
+resource "ibm_iam_user_invite" "invite_user" {
+    users = ["${var.user1}", "${var.user2}"]
+    access_groups = ["${ibm_iam_access_group.accgrp.id}"]
+
+}
