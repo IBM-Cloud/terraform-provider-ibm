3803bceb79d109fdb559ec5cd373bca1c2126f83
Rei Odaira <rodaira@us.ibm.com>
2019-12-10 13:47:38 -0600

    Fix the error code of non existing ibm_is_vpn_gateway_connection

--- ibm/resource_ibm_is_vpn_gateway_connections.go
+++ ibm/resource_ibm_is_vpn_gateway_connections.go
@@ -402,7 +402,7 @@ func resourceIBMISVPNGatewayConnectionExists(d *schema.ResourceData, meta interf
 		iserror, ok := err.(iserrors.RiaasError)
 		if ok {
 			if len(iserror.Payload.Errors) == 1 &&
-				iserror.Payload.Errors[0].Code == "not_found" {
+				iserror.Payload.Errors[0].Code == "vpn_connection_not_found" {
 				return false, nil
 			}
 		}
