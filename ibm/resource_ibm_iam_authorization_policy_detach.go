aeb77a4e25c5fcb413473607a2de01a0bbe9316b
hkantare@in.ibm.com <hkantare@in.ibm.com>
2019-12-13 11:28:47 +0530

    Add resource support for iam authorization policy detach

---
+++ ibm/resource_ibm_iam_authorization_policy_detach.go
@@ -0,0 +1,59 @@
+package ibm
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func resourceIBMIAMAuthorizationPolicyDetach() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceIBMIAMAuthorizationPolicyDetachCreate,
+		Read:   resourceIBMIAMAuthorizationPolicyDetachRead,
+		Delete: resourceIBMIAMAuthorizationPolicyDetachDelete,
+		Exists: resourceIBMIAMAuthorizationPolicyDetachExists,
+
+		Schema: map[string]*schema.Schema{
+			"authorization_policy_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+		},
+	}
+}
+
+func resourceIBMIAMAuthorizationPolicyDetachCreate(d *schema.ResourceData, meta interface{}) error {
+	iampapClient, err := meta.(ClientSession).IAMPAPAPI()
+	if err != nil {
+		return err
+	}
+	policyID := d.Get("authorization_policy_id").(string)
+	err = iampapClient.V1Policy().Delete(policyID)
+	if err != nil {
+		return fmt.Errorf("Error detaching authorization policy: %s", err)
+	}
+
+	d.SetId(time.Now().UTC().String())
+
+	return resourceIBMIAMAuthorizationPolicyDetachRead(d, meta)
+}
+
+func resourceIBMIAMAuthorizationPolicyDetachRead(d *schema.ResourceData, meta interface{}) error {
+	return nil
+}
+
+func resourceIBMIAMAuthorizationPolicyDetachDelete(d *schema.ResourceData, meta interface{}) error {
+
+	d.SetId("")
+
+	return nil
+}
+
+func resourceIBMIAMAuthorizationPolicyDetachExists(d *schema.ResourceData, meta interface{}) (bool, error) {
+	if d.Id() == "" {
+		return false, nil
+	}
+	return true, nil
+}
