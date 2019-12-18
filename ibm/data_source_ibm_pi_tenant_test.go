5f55725010e2c25bd181dc238e1d4cacb828b8a7
umar ali <umarali@umars-mbp.c4p-in.ibmmobiledemo.com>
2019-12-18 14:34:09 +0530

    Power Virtual Server : v0.12 upgradation

--- ibm/data_source_ibm_pi_tenant_test.go
+++ ibm/data_source_ibm_pi_tenant_test.go
@@ -9,26 +9,26 @@ import (
 
 func TestAccIBMPITenantDataSource_basic(t *testing.T) {
 
-	name := "Trial Tenant"
+	//name := "Trial Tenant"
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			resource.TestStep{
-				Config: testAccCheckIBMPITenantDataSourceConfig(name),
+				Config: testAccCheckIBMPITenantDataSourceConfig(),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.ibm_pi_tenant.testacc_ds_network", "name", name),
+					resource.TestCheckResourceAttr("data.ibm_pi_tenant.testacc_ds_tenant", "pi_cloud_instance_id", pi_cloud_instance_id),
 				),
 			},
 		},
 	})
 }
 
-func testAccCheckIBMPITenantDataSourceConfig(name string) string {
+func testAccCheckIBMPITenantDataSourceConfig() string {
 	return fmt.Sprintf(`
 	
 data "ibm_pi_tenant" "testacc_ds_tenant" {
-    name = "%s"
-}`, name)
+    pi_cloud_instance_id = "%s"
+}`, pi_cloud_instance_id)
 
 }
