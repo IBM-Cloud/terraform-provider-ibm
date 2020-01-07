80b128f1aee770de63192f181b2afbf3544f9221
AnilCM <amallak1@in.ibm.com>
2020-01-07 09:43:20 +0530

    updated example for terraform version 12

--- examples/ibm-iam-user-invite/main.tf
+++ examples/ibm-iam-user-invite/main.tf
@@ -1,12 +1,13 @@
-provider "ibm" {}
+provider "ibm" {
+}
 
 data "ibm_org" "org" {
-  org = "${var.org}"
+  org = var.org
 }
 
 data "ibm_space" "space" {
-  org   = "${var.org}"
-  space = "${var.space}"
+  org   = var.org
+  space = var.space
 }
 
 resource "ibm_iam_access_group" "accgrp" {
@@ -19,42 +20,43 @@ data "ibm_resource_group" "group" {
 }
 
 resource "ibm_iam_access_group_policy" "policy" {
-  access_group_id = "${ibm_iam_access_group.accgrp.id}"
-  roles        = ["Operator", "Writer"]
+  access_group_id = ibm_iam_access_group.accgrp.id
+  roles           = ["Operator", "Writer"]
 
-  resources = [{
-    resource_group_id = "${data.ibm_resource_group.group.id}"
-  }]
+  resources {
+    resource_group_id = data.ibm_resource_group.group.id
+  }
 }
 
-"resource" "ibm_iam_user_invite" "20191205-122825" {
-    users = [
-        "${var.user1}",
-        "${var.user2}"
-    ]
-    access_groups = [
-        "${ibm_iam_access_group.accgrp.id}"
-    ]
-    iam_policy = [{
-         roles = ["Operator", "Writer", "Manager", "Viewer"]
-         resources = [{
-             service           = "containers-kubernetes"
-             resource_group_id = "${data.ibm_resource_group.group.id}"
-         }]
-    }]
-    classic_infra_roles = {
-      permissions = [
-        "PORT_CONTROL", 
-        "DATACENTER_ACCESS"
-      ]
-      permission_set = "basicuser"
+resource "ibm_iam_user_invite" "invite_user" {
+  users = [
+    var.user1,
+    var.user2,
+  ]
+  access_groups = [
+    ibm_iam_access_group.accgrp.id,
+  ]
+  iam_policy {
+    roles = ["Operator", "Writer", "Manager", "Viewer"]
+    resources {
+      service           = "containers-kubernetes"
+      resource_group_id = data.ibm_resource_group.group.id
     }
-    cloud_foundry_roles = [{
-      organization_guid = "${data.ibm_org.org.id}"
-      org_roles = ["Manager", "Auditor"]
-      spaces = [{
-          space_guid = "${data.ibm_space.space.id}"
-          space_roles = ["Manager", "Developer"]
-      }]
-   }]
+  }
+  classic_infra_roles {
+    permissions = [
+      "PORT_CONTROL",
+      "DATACENTER_ACCESS",
+    ]
+    permission_set = "basicuser"
+  }
+  cloud_foundry_roles {
+    organization_guid = data.ibm_org.org.id
+    org_roles         = ["Manager", "Auditor"]
+    spaces {
+      space_guid  = data.ibm_space.space.id
+      space_roles = ["Manager", "Developer"]
+    }
+  }
 }
+
