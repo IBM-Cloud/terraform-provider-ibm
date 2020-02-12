7629dc5c31763f6c48f9aa95a3f0d4175bef33e6
hkantare@in.ibm.com <hkantare@in.ibm.com>
2020-02-12 10:22:28 +0530

    Update PI example

--- examples/ibm-power/var.tf
+++ examples/ibm-power/var.tf
@@ -1,5 +1,6 @@
 variable "imagename" {
   description = "Name of the image key to be used"
+  default = "7200-04-01"
 }
 
 variable "powerinstanceid" {
@@ -7,10 +8,25 @@ variable "powerinstanceid" {
 }
 
 variable "instancename" {
+  default = "myinstance"
   description = "Name of the instance"
 }
 
 variable "sshkeyname" {
+  default = "mykey"
   description = "Name of the ssh key to be used"
+}
+
+variable "volname" {
+  default = "myvol"
+  description = "Name of the volume"
+}
+variable "networkname" {
+  default = "mypublicnw"
+  description = "Name of the network"
+}
+
+variable "sshkey" {
+  description = "Public ssh key"
 }
 
