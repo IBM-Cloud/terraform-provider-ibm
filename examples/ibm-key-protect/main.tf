55a3e0377199c0b7a87b39f04353e040c99f4569
hkantare@in.ibm.com <hkantare@in.ibm.com>
2020-02-25 13:14:47 +0530

    Update key protect to add authorization policy

--- examples/ibm-key-protect/main.tf
+++ examples/ibm-key-protect/main.tf
@@ -1,28 +1,32 @@
 resource "ibm_resource_instance" "cos_instance" {
-  name              = "${var.cos_name}"
-  service           = "cloud-object-storage"
-  plan              = "${var.pln}"
-  location          = "${var.location}"
+  name     = var.cos_name
+  service  = "cloud-object-storage"
+  plan     = var.pln
+  location = var.location
 }
 resource "ibm_resource_instance" "kp_instance" {
-  name              = "${var.kp_name}"
-  service           = "kms"
-  plan              = "tiered-pricing"
-  location          = "us-south"
+  name     = var.kp_name
+  service  = "kms"
+  plan     = "tiered-pricing"
+  location = "us-south"
 }
 resource "ibm_kp_key" "test" {
-  key_protect_id = "${ibm_resource_instance.kp_instance.guid}"
-  key_name = "${var.key_name}"
-  standard_key =  false
+  key_protect_id = ibm_resource_instance.kp_instance.guid
+  key_name       = var.key_name
+  standard_key   = false
 }
-data "ibm_kp_key" "test" {
-  depends_on  = ["ibm_kp_key.test"]
-  key_protect_id = "${ibm_resource_instance.kp_instance.guid}"
+
+resource "ibm_iam_authorization_policy" "policy" {
+  source_service_name = "cloud-object-storage"
+  target_service_name = "kms"
+  roles               = ["Reader"]
 }
+
 resource "ibm_cos_bucket" "flex-us-south" {
-  bucket_name = "abuck4"
-  resource_instance_id = "${ibm_resource_instance.cos_instance.id}"
-  region_location = "us-south"
-  storage_class = "flex"
-  key_protect = data.ibm_kp_key.test.keys.0.crn
+  depends_on           = [ibm_iam_authorization_policy.policy]
+  bucket_name          = "abuck4"
+  resource_instance_id = ibm_resource_instance.cos_instance.id
+  region_location      = "us-south"
+  storage_class        = "flex"
+  key_protect          = ibm_kp_key.test.id
 }
