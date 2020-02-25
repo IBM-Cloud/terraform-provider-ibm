42304801b0831cd2090cf1f4677bc87e63c03614
Kavya.Handadi <kavya.handadi@gmail.com>
2020-02-25 16:43:51 +0530

    v11 ibm_container_cluster_config datasource update

--- vendor/github.com/IBM-Cloud/bluemix-go/models/access_group.go
+++ vendor/github.com/IBM-Cloud/bluemix-go/models/access_group.go
@@ -6,3 +6,12 @@ type AccessGroup struct {
 	Name        string `json:"name,omitempty"`
 	Description string `json:"description,omitempty"`
 }
+
+type AccessGroupV2 struct {
+	AccessGroup
+	AccountID        string `json:"account_id,omitempty"`
+	CreatedAt        string `json:"created_at,omitempty"`
+	CreatedByID      string `json:"created_by_id,omitempty"`
+	LastModifiedAt   string `json:"last_modified_at,omitempty"`
+	LastModifiedByID string `json:"last_modified_by_id,omitempty"`
+}
