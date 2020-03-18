700f7b4308df9ffb8e651727318ff50588f6a2a6
hkantare@in.ibm.com <hkantare@in.ibm.com>
2020-03-18 12:47:53 +0530

    Fix the status string of service based on API change v11

--- ibm/resource_ibm_cis.go
+++ ibm/resource_ibm_cis.go
@@ -19,11 +19,12 @@ import (
 )
 
 const (
-	cisInstanceSuccessStatus  = "active"
-	cisInstanceProgressStatus = "in progress"
-	cisInstanceInactiveStatus = "inactive"
-	cisInstanceFailStatus     = "failed"
-	cisInstanceRemovedStatus  = "removed"
+	cisInstanceSuccessStatus      = "active"
+	cisInstanceProgressStatus     = "in progress"
+	cisInstanceProvisioningStatus = "provisioning"
+	cisInstanceInactiveStatus     = "inactive"
+	cisInstanceFailStatus         = "failed"
+	cisInstanceRemovedStatus      = "removed"
 )
 
 func resourceIBMCISInstance() *schema.Resource {
@@ -396,7 +397,7 @@ func waitForCISInstanceCreate(d *schema.ResourceData, meta interface{}, instance
 	//instanceID := d.Id()
 
 	stateConf := &resource.StateChangeConf{
-		Pending: []string{cisInstanceProgressStatus, cisInstanceInactiveStatus},
+		Pending: []string{cisInstanceProgressStatus, cisInstanceInactiveStatus, cisInstanceProvisioningStatus},
 		Target:  []string{cisInstanceSuccessStatus},
 		Refresh: func() (interface{}, string, error) {
 			instance, err := rsConClient.ResourceServiceInstance().GetInstance(instanceID)
