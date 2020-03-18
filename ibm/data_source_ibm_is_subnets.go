c9e9033a6c5d793361b08daf12826e848a7b4f94
Gaëtan Trellu (goldyfruit) <gaetan.trellu@ibm.com>
2020-03-18 18:51:44 -0400

    [ibm_is_subnet] Add supported fields
    This extend the amount of information returned.

--- ibm/data_source_ibm_is_subnets.go
+++ ibm/data_source_ibm_is_subnets.go
@@ -1,6 +1,7 @@
 package ibm
 
 import (
+	"strconv"
 	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
@@ -35,7 +36,43 @@ func dataSourceIBMISSubnets() *schema.Resource {
 							Type:     schema.TypeString,
 							Computed: true,
 						},
+						"crn": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
 						"ipv4_cidr_block": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"ipv6_cidr_block": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"available_ipv4_address_count": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"network_acl": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"public_gateway": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"resource_group": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"total_ipv4_address_count": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"vpc": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"zone": {
 							Type:     schema.TypeString,
 							Computed: true,
 						},
@@ -60,11 +97,23 @@ func dataSourceIBMISSubnetsRead(d *schema.ResourceData, meta interface{}) error 
 	subnets := make([]map[string]string, len(availableSubnets))
 	for i, subnet := range availableSubnets {
 
+		var aac string = strconv.FormatInt(subnet.AvailableIPV4AddressCount, 10)
+		var tac string = strconv.FormatInt(subnet.TotalIPV4AddressCount, 10)
+
 		subn := make(map[string]string)
 		subn["name"] = subnet.Name
 		subn["id"] = string(subnet.ID)
 		subn["status"] = subnet.Status
+		subn["crn"] = subnet.Crn
 		subn["ipv4_cidr_block"] = subnet.IPV4CidrBlock
+		subn["ipv6_cidr_block"] = subnet.IPV6CidrBlock
+		subn["available_ipv4_address_count"] = aac
+		subn["network_acl"] = subnet.NetworkACL.Name
+		subn["public_gateway"] = string(subnet.PublicGateway.ID)
+		subn["resource_group"] = string(subnet.ResourceGroup.ID)
+		subn["total_ipv4_address_count"] = tac
+		subn["vpc"] = string(subnet.Vpc.ID)
+		subn["zone"] = subnet.Zone.Name
 
 		subnets[i] = subn
 	}
