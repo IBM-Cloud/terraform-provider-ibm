d37aa0ffd0f9b712d47aafe294857f7f6660a465
Kavya.Handadi <kavya.handadi@gmail.com>
2020-03-19 22:08:49 +0530

    v11 updated certificate import example

--- examples/ibm-certificate-manager-import/main.tf
+++ examples/ibm-certificate-manager-import/main.tf
@@ -5,6 +5,7 @@ resource "ibm_resource_instance" "cm" {
   plan     = "free"
 }
 
+// template file to generate ssl certificate and key and import generated certificate...
 resource "ibm_certificate_manager_import" "cert" {
   certificate_manager_instance_id = "${ibm_resource_instance.cm.id}"
   name                            = "test"
@@ -14,3 +15,39 @@ resource "ibm_certificate_manager_import" "cert" {
   }
 }
 
+// template file to generate ssl certificate and key and import generated certificate...
+#null resource for generating ssl key and certificate...
+resource "null_resource" "import" {
+  provisioner "local-exec" {
+    command = <<EOT
+openssl req -x509  \
+          -newkey rsa:1024 \
+          -keyout "${var.key}" \
+          -out "${var.cert}" \
+          -days 1 -nodes \
+          -subj "/C=us/ST="${var.region}"/L=Dal-10/O=IBM/OU=CloudCerts/CN="${var.host}"" 
+      EOT
+  }
+}
+
+#datasource to read local certificate file...
+data "local_file" "cert" {
+  filename   = "${path.module}/${var.cert}"
+  depends_on = ["null_resource.import"]
+}
+
+#datasource to read local priv_key file...
+data "local_file" "key" {
+  filename   = "${path.module}/${var.key}"
+  depends_on = ["null_resource.import"]
+}
+
+resource "ibm_certificate_manager_import" "cert" {
+  certificate_manager_instance_id = "${ibm_resource_instance.cm.id}"
+  name                            = "test"
+
+  data = {
+    content  = "${data.local_file.cert.content}"
+    priv_key = "${data.local_file.key.content}"
+  }
+}
