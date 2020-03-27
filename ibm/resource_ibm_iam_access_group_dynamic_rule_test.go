48b44fb2437491a7995e6a6059d738fd4013a874
Soumyajit <soumyda1@in.ibm.com>
2020-03-27 14:45:04 +0530

    added dynamic_rule for access group

---
+++ ibm/resource_ibm_iam_access_group_dynamic_rule_test.go
@@ -0,0 +1,174 @@
+package ibm
+
+import (
+	"fmt"
+	"testing"
+
+	"strings"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func TestAccIBMIAMDynamicRule_Basic(t *testing.T) {
+	agname := fmt.Sprintf("terraform_%d", acctest.RandInt())
+	name := fmt.Sprintf("terraform_%d", acctest.RandInt())
+	name1 := fmt.Sprintf("terraform_%d", acctest.RandInt())
+	expiration := 10
+	identityProvider := "test-idp.com"
+	claim := "blueGroups"
+	operator := "CONTAINS"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckIBMIAMDynamicRuleDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccCheckIBMIAMDynamicRuleBasic(agname, name, identityProvider, claim, operator, expiration),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("ibm_iam_access_group_dynamic_rule.accgroup", "name", name),
+					resource.TestCheckResourceAttr("ibm_iam_access_group_dynamic_rule.accgroup", "identity_provider", identityProvider),
+				),
+			},
+			resource.TestStep{
+				Config: testAccCheckIBMIAMDynamicRuleupdate(agname, name1, identityProvider, claim, operator, expiration),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("ibm_iam_access_group_dynamic_rule.accgroup", "name", name),
+					resource.TestCheckResourceAttr("ibm_iam_access_group_dynamic_rule.accgroup", "identity_provider", identityProvider),
+				),
+			},
+		},
+	})
+}
+
+func TestAccIBMIAMDynamicRuleimport(t *testing.T) {
+	agname := fmt.Sprintf("terraform_%d", acctest.RandInt())
+	name := fmt.Sprintf("terraform_%d", acctest.RandInt())
+	expiration := 10
+	identityProvider := "test-idp.com"
+	claim := "blueGroups"
+	operator := "CONTAINS"
+	resourceName := "ibm_iam_access_group_dynamic_rule.accgroup"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckIBMIAMDynamicRuleDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccCheckIBMIAMDynamicRuleMultiple(agname, name, identityProvider, claim, operator, expiration),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "name", name),
+					resource.TestCheckResourceAttr(resourceName, "identity_provider", identityProvider),
+				),
+			},
+			resource.TestStep{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccCheckIBMIAMDynamicRuleDestroy(s *terraform.State) error {
+	accClient, err := testAccProvider.Meta().(ClientSession).IAMUUMAPIV2()
+	if err != nil {
+		return err
+	}
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "ibm_iam_access_group_dynamic_rule" {
+			continue
+		}
+
+		parts, err := idParts(rs.Primary.ID)
+		if err != nil {
+			return err
+		}
+
+		grpID := parts[0]
+		ruleID := parts[1]
+
+		// Try to find the key
+		_, _, err = accClient.DynamicRule().Get(grpID, ruleID)
+
+		if err != nil && !strings.Contains(err.Error(), "404") {
+			return fmt.Errorf("Error waiting for Dynamic rule (%s) to be destroyed: %s", ruleID, err)
+		}
+	}
+
+	return nil
+}
+
+func testAccCheckIBMIAMDynamicRuleBasic(agname, name, identityProvider, claim, operator string, expiration int) string {
+	return fmt.Sprintf(`
+	
+	resource "ibm_iam_access_group" "newgroup" {
+		name = "%s"
+		tags = ["tag1", "tag2"]
+	  }
+	  
+	  resource "ibm_iam_access_group_dynamic_rule" "accgroup" {
+		name              = "%s"
+		access_group_id   = ibm_iam_access_group.newgroup.id
+		expiration        = %d
+		identity_provider = "%s"
+		conditions {
+		  claim    = "%s"
+		  operator = "%s"
+		  value    = "\"test-bluegroup-saml\""
+		}
+	  
+	  }`, agname, name, expiration, identityProvider, claim, operator)
+}
+
+func testAccCheckIBMIAMDynamicRuleupdate(agname, name1, identityProvider, claim, operator string, expiration int) string {
+	return fmt.Sprintf(`
+	
+	resource "ibm_iam_access_group" "newgroup" {
+		name = "%s"
+		tags = ["tag1", "tag2"]
+	  }
+	  
+	  resource "ibm_iam_access_group_dynamic_rule" "accgroup" {
+		name              = "%s"
+		access_group_id   = ibm_iam_access_group.newgroup.id
+		expiration        = %d
+		identity_provider = "%s"
+		conditions {
+		  claim    = "%s"
+		  operator = "%s"
+		  value    = "\"test-bluegroup-saml\""
+		}
+	  
+	  }`, agname, name1, expiration, identityProvider, claim, operator)
+}
+
+func testAccCheckIBMIAMDynamicRuleMultiple(agname, name, identityProvider, claim, operator string, expiration int) string {
+	return fmt.Sprintf(`
+	resource "ibm_iam_access_group" "newgroup" {
+		name = "%s"
+		tags = ["tag1", "tag2"]
+	  }
+	  
+	  resource "ibm_iam_access_group_dynamic_rule" "accgroup" {
+		name              = "%s"
+		access_group_id   = ibm_iam_access_group.newgroup.id
+		expiration        = %d
+		identity_provider = "%s"
+		conditions {
+		  claim    = "%s"
+		  operator = "%s"
+		  value    = "\"test-bluegroup-saml\""
+		}
+		conditions {
+		  claim    = "%s"
+		  operator = "%s"
+		  value    = "\"test-bluegroup-saml\""
+		}
+	  
+	  }
+	`, agname, name, expiration, identityProvider, claim, operator, claim, operator)
+}
---
+++ website/docs/r/iam_access_group_dynamic_rule.html.markdown
@@ -0,0 +1,58 @@
+---
+layout: "ibm"
+page_title: "IBM : iam_access_group_dynamic_rule"
+sidebar_current: "docs-ibm-resource-iam-access-group-dynamic-rule"
+description: |-
+  Manages Dynamic Rule for IBM IAM Access Group.
+---
+
+# ibm\_iam_access_group
+
+Provides a resource for Dynamic Rule of an IAM access group. This allows rules to be created, updated and deleted.
+
+## Example Usage
+
+```hcl
+resource "ibm_iam_access_group_dynamic_rule" "rule1" {
+  name              = "newrule"
+  access_group_id   = "AccessGroupId-dsnd4bvsaf"
+  expiration        = 4
+  identity_provider = "test-idp.com"
+  conditions {
+    claim    = "blueGroups"
+    operator = "CONTAINS"
+    value    = "\"test-bluegroup-saml\""
+  }
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `name` - (Required, string) Name of the dynamic rule.
+* `access_group_id` - (Required, string) ID of the access group.
+
+* `expiration` - (Required, int) The number of hours that the rule lives for (Must be between 1 and 24).
+* `identity_provider` - (Required, string) The url of the identity provider.  
+* `conditions` - (Required, list) A list of conditions the rule must satisfy:
+  * `claim` - (Required, string) The claim to evaluate against. This will be found in the ext claims of a user's login request. 
+  * `operator` - (Required, string) The operation to perform on the claim. Valid operators are EQUALS, EQUALS_IGNORE_CASE, IN, NOT_EQUALS_IGNORE_CASE, NOT_EQUALS, and CONTAINS.
+  * `value` - (Required, string) The stringified JSON value that the claim is compared to using the operator.
+
+
+## Attribute Reference
+
+The following attributes are exported:
+
+* `id` - The unique identifier of the dynamic rule resource. The id is composed of \<access_group_id\>/\<rule_id\>.<br/>
+* `rule_id` - The id of the rule.
+
+
+## Import
+
+iam_access_group_dynamic_rule can be imported using access group ID and rule id, eg
+
+```
+$ terraform import iam_access_group_dynamic_rule.example AccessGroupId-5391772e-1207-45e8-b032-2a21941c11ab/ClaimRule-3c5cd5fd-5b95-45f3-a693-08047eee56b5
+```
