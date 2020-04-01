71254bd99a3ac4d768d4be4f38d0d371f796bf35
Soumyajit <soumyda1@in.ibm.com>
2020-04-01 16:13:45 +0530

    added access group data source

---
+++ ibm/data_source_ibm_iam_access_group_test.go
@@ -0,0 +1,42 @@
+package ibm
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccIBMIAMAccessGroupDataSource_Basic(t *testing.T) {
+	name := fmt.Sprintf("terraform_%d", acctest.RandInt())
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccCheckIBMIAMAccessGroupDataSourceConfig(name),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.ibm_iam_access_group.accgroupdata", "access_group_name", name),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckIBMIAMAccessGroupDataSourceConfig(name string) string {
+	return fmt.Sprintf(`
+
+	resource "ibm_iam_access_group" "accgroup" {
+		name = "%s"
+		tags = ["tag1", "tag2"]
+	  }
+	  
+	  
+	  data "ibm_iam_access_group" "accgroupdata" {
+		access_group_name = ibm_iam_access_group.accgroup.name
+	  }
+	  `, name)
+
+}
