821a14ac716f3f6a5cd543927d8fbe77331844a3
hkantare@in.ibm.com <hkantare@in.ibm.com>
2020-04-16 11:33:07 +0530

    [ibm_is_subnets] Collect VPC subnets

---
+++ ibm/data_source_ibm_is_subnets_test.go
@@ -0,0 +1,43 @@
+package ibm
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccIBMISSubnetsDataSource_basic(t *testing.T) {
+	resName := "data.ibm_is_subnets.test1"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccCheckIBMISSubnetsDataSourceConfig(),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(resName, "subnets.0.name"),
+					resource.TestCheckResourceAttrSet(resName, "subnets.0.status"),
+					resource.TestCheckResourceAttrSet(resName, "subnets.0.status"),
+					resource.TestCheckResourceAttrSet(resName, "subnets.0.zone"),
+					resource.TestCheckResourceAttrSet(resName, "subnets.0.crn"),
+					resource.TestCheckResourceAttrSet(resName, "subnets.0.ipv4_cidr_block"),
+					resource.TestCheckResourceAttrSet(resName, "subnets.0.available_ipv4_address_count"),
+					resource.TestCheckResourceAttrSet(resName, "subnets.0.network_acl"),
+					resource.TestCheckResourceAttrSet(resName, "subnets.0.public_gateway"),
+					resource.TestCheckResourceAttrSet(resName, "subnets.0.resource_group"),
+					resource.TestCheckResourceAttrSet(resName, "subnets.0.total_ipv4_address_count"),
+					resource.TestCheckResourceAttrSet(resName, "subnets.0.vpc"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckIBMISSubnetsDataSourceConfig() string {
+	// status filter defaults to empty
+	return fmt.Sprintf(`
+      data "ibm_is_subnets" "test1" {
+      }`)
+}
