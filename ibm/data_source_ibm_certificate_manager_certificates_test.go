c5a125cc85c93de0477b42d3e1e4dcc166ef89f4
Kavya.Handadi <kavya.handadi@gmail.com>
2020-04-24 00:16:26 +0530

    v11 CMS datasource support

---
+++ ibm/data_source_ibm_certificate_manager_certificates_test.go
@@ -0,0 +1,36 @@
+package ibm
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccIBMCertificateManagerCertificatesDataSource_Basic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccCheckIBMCertificateManagerCertificatesDataSourceConfig_basic(),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.ibm_certificate_manager_certificates.certs", "certificates.0.name"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckIBMCertificateManagerCertificatesDataSourceConfig_basic() string {
+	return fmt.Sprintf(`
+	data "ibm_resource_instance" "cm" {
+		name     = "testname"
+		location = "us-south"
+		service  = "cloudcerts"
+	}
+	data "ibm_certificate_manager_certificates" "certs"{
+		certificate_manager_instance_id="${data.ibm_resource_instance.cm.id}"
+	}
+	  
+	`)
+}
