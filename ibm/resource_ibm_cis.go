c3e0ddd86285bb282c9eea0222a78dc2a45bb588
hkantare@in.ibm.com <hkantare@in.ibm.com>
2020-04-24 14:24:42 +0530

    Add support for pending_reclamination for databse and CIS

--- ibm/resource_ibm_cis.go
+++ ibm/resource_ibm_cis.go
@@ -25,6 +25,7 @@ const (
 	cisInstanceInactiveStatus     = "inactive"
 	cisInstanceFailStatus         = "failed"
 	cisInstanceRemovedStatus      = "removed"
+	cisInstanceReclamation        = "pending_reclamation"
 )
 
 func resourceIBMCISInstance() *schema.Resource {
@@ -466,7 +467,7 @@ func waitForCISInstanceDelete(d *schema.ResourceData, meta interface{}) (interfa
 	instanceID := d.Id()
 	stateConf := &resource.StateChangeConf{
 		Pending: []string{cisInstanceProgressStatus, cisInstanceInactiveStatus, cisInstanceSuccessStatus},
-		Target:  []string{cisInstanceRemovedStatus},
+		Target:  []string{cisInstanceRemovedStatus, cisInstanceReclamation},
 		Refresh: func() (interface{}, string, error) {
 			instance, err := rsConClient.ResourceServiceInstance().GetInstance(instanceID)
 			if err != nil {
