e4a3793669026f2819b4d40e438ffec12eefc7de
AnilCM <amallak1@in.ibm.com>
2020-05-07 10:07:36 +0530

    updated ibm_container_cluster and workerpool resources for openshift clusters's entitlement feature

--- ibm/resource_ibm_container_worker_pool.go
+++ ibm/resource_ibm_container_worker_pool.go
@@ -54,6 +54,13 @@ func resourceIBMContainerWorkerPool() *schema.Resource {
 				Description:  "Number of nodes per zone",
 			},
 
+			"entitlement": {
+				Type:             schema.TypeString,
+				Optional:         true,
+				DiffSuppressFunc: applyOnce,
+				Description:      "Entitlement option reduces additional OCP Licence cost in Openshift Clusters",
+			},
+
 			"hardware": {
 				Type:         schema.TypeString,
 				Optional:     true,
@@ -106,12 +113,12 @@ func resourceIBMContainerWorkerPool() *schema.Resource {
 			},
 
 			"labels": {
-				Type:             schema.TypeSet,
+				Type:             schema.TypeMap,
 				Optional:         true,
 				Computed:         true,
 				ForceNew:         true,
 				DiffSuppressFunc: applyOnce,
-				Elem:             &schema.Schema{Type: schema.TypeString},
+				Elem:             schema.TypeString,
 				Description:      "list of labels to worker pool",
 			},
 
@@ -171,6 +178,12 @@ func resourceIBMContainerWorkerPoolCreate(d *schema.ResourceData, meta interface
 		}
 		workerPoolConfig.Labels = labels
 	}
+
+	// Update workerpoolConfig with Entitlement option if provided
+	if v, ok := d.GetOk("entitlement"); ok {
+		workerPoolConfig.Entitlement = v.(string)
+	}
+
 	params := v1.WorkerPoolRequest{
 		WorkerPoolConfig: workerPoolConfig,
 		DiskEncryption:   d.Get("disk_encryption").(bool),
