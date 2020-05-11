301eca89e19a28e64e5e1c30d083400dc162907a
Malar Kandasamy <k.malarvizhi@in.ibm.com>
2020-05-11 15:55:29 +0530

    changes to list pdns zones

--- ibm/provider.go
+++ ibm/provider.go
@@ -230,6 +230,10 @@ func Provider() terraform.ResourceProvider {
 			"ibm_pi_public_network":   dataSourceIBMPIPublicNetwork(),
 			"ibm_pi_images":           dataSourceIBMPIImages(),
 			"ibm_pi_instance_ip":      dataSourceIBMPIInstanceIP(),
+
+			// Added for private dns zones
+
+			"ibm_dns_zones": dataSourceIBMPrivateDNSZones(),
 		},
 
 		ResourcesMap: map[string]*schema.Resource{
