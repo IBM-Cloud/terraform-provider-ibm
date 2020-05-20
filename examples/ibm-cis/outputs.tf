2bcc928f17cdd8795455d4ab8070c38cbb473741
Kavya.Handadi <kavya.handadi@gmail.com>
2020-05-20 20:27:55 +0530

    CIS Rate Limit Example Update

--- examples/ibm-cis/outputs.tf
+++ examples/ibm-cis/outputs.tf
@@ -1,4 +1,15 @@
 output "web_dns_name" {
   value = "http://${var.dns_name}${var.domain}"
 }
-
+output "instance_id" {
+  value = ibm_cis.web_domain.id
+}
+output "domain_id" {
+  value = ibm_cis_domain.web_domain.id
+}
+output "monitor" {
+  value = ibm_cis_healthcheck.root.id
+}
+output "rate_limit_id" {
+  value = ibm_cis_rate_limit.ratelimit.id
+}
