a54a6a4c3a90f2526701627d556dd4f416d14b96
umarali-nagoor <umarali.nagoor@in.ibm.com>
2020-06-11 18:40:16 +0530

    Cluster : README Doc Update

--- examples/ibm-cluster/cluster-worker-pool-zone/variables.tf
+++ examples/ibm-cluster/cluster-worker-pool-zone/variables.tf
@@ -1,13 +1,5 @@
-variable "org" {
-  
-}
-
-variable "space" {
-  
-}
-
-variable "region" {
-  
+variable "name" {
+  default = "Default"
 }
 
 variable "datacenter" {
@@ -18,8 +10,8 @@ variable "machine_type" {
   
 }
 
-variable "isolation" {
-  
+variable "service_instance_name" {
+  default = "myservice4"
 }
 
 variable "private_vlan_id" {
@@ -34,39 +26,11 @@ variable "subnet_id" {
   
 }
 
-variable "service_instance_name" {
-  default = "myservice4"
-}
-
-variable "service_key" {
-  default = "myservicekey4"
-}
-
-variable "service_offering" {
-  default = "speech_to_text"
-}
-
-variable "plan" {
-  default = "lite"
-}
-
-variable "cluster_name" {
-  default = "cluster4"
-}
-
 variable "worker_pool_name" {
   default = "worker_pool4"
 }
 
 variable "zone" {
-  
-}
-
-variable "zone_private_vlan_id" {
-  
-}
-
-variable "zone_public_vlan_id" {
   
 }
 
