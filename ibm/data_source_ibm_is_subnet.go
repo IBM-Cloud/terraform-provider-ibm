1d4a9d3c051727a227c4e10e4dfe4e33715ba84f
vaishnavi <ggunishetty9@gmail.com>
2020-06-19 16:16:19 +0530

    Fix for subnet ds when id doesn't exist

--- ibm/data_source_ibm_is_subnet.go
+++ ibm/data_source_ibm_is_subnet.go
@@ -1,7 +1,11 @@
 package ibm
 
 import (
+	"fmt"
+
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.ibm.com/ibmcloud/vpc-go-sdk/vpcclassicv1"
+	"github.ibm.com/ibmcloud/vpc-go-sdk/vpcv1"
 )
 
 func dataSourceIBMISSubnet() *schema.Resource {
@@ -115,17 +119,99 @@ func dataSourceIBMISSubnetRead(d *schema.ResourceData, meta interface{}) error {
 	}
 	id := d.Get("identifier").(string)
 	if userDetails.generation == 1 {
-		err := classicSubnetGet(d, meta, id)
-		d.SetId(id)
+		err := classicSubnetGetByID(d, meta, id)
 		if err != nil {
 			return err
 		}
 	} else {
-		err := subnetGet(d, meta, id)
-		d.SetId(id)
+		err := subnetGetByID(d, meta, id)
 		if err != nil {
 			return err
 		}
+	}
+	return nil
+}
+
+func classicSubnetGetByID(d *schema.ResourceData, meta interface{}, id string) error {
+	sess, err := classicVpcClient(meta)
+	if err != nil {
+		return err
+	}
+	getSubnetOptions := &vpcclassicv1.GetSubnetOptions{
+		ID: &id,
+	}
+	subnet, response, err := sess.GetSubnet(getSubnetOptions)
+	if err != nil {
+		return fmt.Errorf("Error Getting Subnet (%s): %s\n%s", id, err, response)
+	}
+	d.Set("id", *subnet.ID)
+	d.Set(isSubnetName, *subnet.Name)
+	d.Set(isSubnetIpv4CidrBlock, *subnet.Ipv4CidrBlock)
+	d.Set(isSubnetAvailableIpv4AddressCount, *subnet.AvailableIpv4AddressCount)
+	d.Set(isSubnetTotalIpv4AddressCount, *subnet.TotalIpv4AddressCount)
+	if subnet.NetworkAcl != nil {
+		d.Set(isSubnetNetworkACL, *subnet.NetworkAcl.ID)
+	}
+	if subnet.PublicGateway != nil {
+		d.Set(isSubnetPublicGateway, *subnet.PublicGateway.ID)
+	} else {
+		d.Set(isSubnetPublicGateway, nil)
+	}
+	d.Set(isSubnetStatus, *subnet.Status)
+	d.Set(isSubnetZone, *subnet.Zone.Name)
+	d.Set(isSubnetVPC, *subnet.Vpc.ID)
+
+	controller, err := getBaseController(meta)
+	if err != nil {
+		return err
+	}
+	d.Set(ResourceControllerURL, controller+"/vpc/network/subnets")
+	d.Set(ResourceName, *subnet.Name)
+	d.Set(ResourceCRN, *subnet.Crn)
+	d.Set(ResourceStatus, *subnet.Status)
+	return nil
+}
+
+func subnetGetByID(d *schema.ResourceData, meta interface{}, id string) error {
+	sess, err := vpcClient(meta)
+	if err != nil {
+		return err
+	}
+	getSubnetOptions := &vpcv1.GetSubnetOptions{
+		ID: &id,
+	}
+	subnet, response, err := sess.GetSubnet(getSubnetOptions)
+	if err != nil {
+		return fmt.Errorf("Error Getting Subnet (%s): %s\n%s", id, err, response)
+	}
+	d.Set("id", *subnet.ID)
+	d.Set(isSubnetName, *subnet.Name)
+	d.Set(isSubnetIpv4CidrBlock, *subnet.Ipv4CidrBlock)
+	d.Set(isSubnetAvailableIpv4AddressCount, *subnet.AvailableIpv4AddressCount)
+	d.Set(isSubnetTotalIpv4AddressCount, *subnet.TotalIpv4AddressCount)
+	if subnet.NetworkAcl != nil {
+		d.Set(isSubnetNetworkACL, *subnet.NetworkAcl.ID)
+	}
+	if subnet.PublicGateway != nil {
+		d.Set(isSubnetPublicGateway, *subnet.PublicGateway.ID)
+	} else {
+		d.Set(isSubnetPublicGateway, nil)
+	}
+	d.Set(isSubnetStatus, *subnet.Status)
+	d.Set(isSubnetZone, *subnet.Zone.Name)
+	d.Set(isSubnetVPC, *subnet.Vpc.ID)
+
+	controller, err := getBaseController(meta)
+	if err != nil {
+		return err
+	}
+	d.Set(ResourceControllerURL, controller+"/vpc-ext/network/subnets")
+	d.Set(ResourceName, *subnet.Name)
+	d.Set(ResourceCRN, *subnet.Crn)
+	d.Set(ResourceStatus, *subnet.Status)
+	if subnet.ResourceGroup != nil {
+		d.Set(isSubnetResourceGroup, *subnet.ResourceGroup.ID)
+		d.Set(ResourceGroupName, *subnet.ResourceGroup.Name)
 	}
 	return nil
 }
