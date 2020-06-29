25e31681dbd6187ba5008ada5d9f138751a30d31
Pinky Bhargava <pinky.bharagava@in.ibm.com>
2020-06-29 18:42:35 +0530

    updated for virtual connection in directlink

--- ibm/provider.go
+++ ibm/provider.go
@@ -384,8 +384,8 @@ func Provider() terraform.ResourceProvider {
 			"ibm_dns_resource_record":   resourceIBMPrivateDNSResourceRecord(),
 
 			//Direct Link related resources
-			"ibm_dl_gateway": resourceIBMDLGateway(),
-
+			"ibm_dl_gateway":            resourceIBMDLGateway(),
+			"ibm_dl_virtual_connection": resourceIBMDLGatewayVC(),
 			//Added for Transit Gateway
 			"ibm_tg_gateway": resourceIBMTransitGateway(),
 		},
@@ -401,11 +401,12 @@ func Validator() ValidatorDict {
 	initOnce.Do(func() {
 		globalValidatorDict = ValidatorDict{
 			ResourceValidatorDictionary: map[string]*ResourceValidator{
-				"ibm_is_vpc":          resourceIBMISVPCValidator(),
-				"ibm_is_ike_policy":   resourceIBMISIKEValidator(),
-				"ibm_iam_custom_role": resourceIBMIAMCustomRoleValidator(),
-				"ibm_cis_rate_limit":  resourceIBMCISRateLimitValidator(),
-				"ibm_tg_gateway":      resourceIBMTGValidator(),
+				"ibm_is_vpc":                resourceIBMISVPCValidator(),
+				"ibm_is_ike_policy":         resourceIBMISIKEValidator(),
+				"ibm_iam_custom_role":       resourceIBMIAMCustomRoleValidator(),
+				"ibm_cis_rate_limit":        resourceIBMCISRateLimitValidator(),
+				"ibm_tg_gateway":            resourceIBMTGValidator(),
+				"ibm_dl_virtual_connection": resourceIBMdlGatewayVCValidator(),
 			},
 		}
 	})
