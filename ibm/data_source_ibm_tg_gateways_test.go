4b0840822446215d4fbb033228908320d28e8200
Prasad Krishnegowda <pkrishn1@in.ibm.com>
2020-07-03 11:23:01 +0530

    Datasource for Transit Gateway

---
+++ ibm/data_source_ibm_tg_gateways_test.go
@@ -0,0 +1,47 @@
+package ibm
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccIBMTransitGatewaysDataSource_basic(t *testing.T) {
+	resName := "data.ibm_tg_gateways.test1"
+	gatewayname := fmt.Sprintf("gateway-name-%d", acctest.RandIntRange(10, 100))
+	location := fmt.Sprintf("us-south")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+
+			{
+				Config: testAccCheckIBMTransitGatewayDataSourceConfig(gatewayname, location),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"data.ibm_tg_gateway.test_tg_gateway", "name", gatewayname),
+					resource.TestCheckResourceAttr(
+						"data.ibm_tg_gateway.test_tg_gateway", "location", location),
+				),
+			},
+			{
+				Config: testAccCheckIBMTransitGatewaysDataSourceConfig(),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(resName, "transit_gateways.0.name"),
+					resource.TestCheckResourceAttrSet(resName, "transit_gateways.0.location"),
+					resource.TestCheckResourceAttrSet(resName, "transit_gateways.0.global"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckIBMTransitGatewaysDataSourceConfig() string {
+	// status filter defaults to empty
+	return fmt.Sprintf(`
+      data "ibm_tg_gateways" "test1" {
+      }`)
+}
---
+++ website/docs/d/tg_gateways.html.markdown
@@ -0,0 +1,34 @@
+---
+layout: "ibm"
+page_title: "IBM : tg_gateways"
+sidebar_current: "docs-ibm-datasource-tg-gateways"
+description: |-
+  Manages IBM Cloud Infrastructure Transit Gateway.
+---
+
+# ibm\_tg_gateways
+
+Import the details of an existing IBM Cloud Infrastructure transit gateways as a read-only data source. You can then reference the fields of the data source in other resources within the same configuration using interpolation syntax.
+
+
+## Example Usage
+
+```hcl
+data "ibm_tg_gateways" "ds_tggateways" {
+}
+```
+
+## Attribute Reference
+
+The following attributes are exported:
+
+* `transit_gateways` - List of all Transit gateways in the IBM Cloud Infrastructure.
+  * `created_at` - The date and time resource was created.
+  * `updated_at` - The date and time resource was last updated.
+  * `crn` - The CRN (Cloud Resource Name) of this gateway.
+  * `global` - Gateways with global routing (true) can connect to networks outside their associated region.
+  * `id` - The unique identifier of this gateway.
+  * `location` - Gateway location.
+  * `name` - The unique user-defined name for this gateway.
+  * `status` - Gateway status.
+  * `resource_group` - Resource group identifier.
