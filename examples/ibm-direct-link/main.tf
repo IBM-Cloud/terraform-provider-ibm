635f7acd8e0b4eab3d44cf2d1544069bc8154703
Pinky Bhargava <pinky.bharagava@in.ibm.com>
2020-07-07 13:56:58 +0530

    Added datasource for dl locations

--- examples/ibm-direct-link/main.tf
+++ examples/ibm-direct-link/main.tf
@@ -52,3 +52,6 @@ data "ibm_dl_ports" "test_ds_dl_ports" {
 data "ibm_dl_port" "test_ds_dl_port" {
   port_id = "2f41cf65-e72a-4522-9526-e156e4ca02b5"
 }
+data "ibm_dl_locations" "test_dl_locations"{
+		offering_type = "dedicated"
+}
