dd1d3826aa63fb048f089ec282e596b8bed35ae6
Pinky Bhargava <pinky.bharagava@in.ibm.com>
2020-07-13 17:46:49 +0530

    updated for TG resource connection

--- examples/ibm-transit-gateway/main.tf
+++ examples/ibm-transit-gateway/main.tf
@@ -1,7 +1,9 @@
 data "ibm_resource_group" "rg" {
   name = var.resource_group
 }
-
+resource "ibm_is_vpc" "test_tg_vpc" {
+  name = "myvpc"
+}
 provider "ibm" {
   ibmcloud_api_key = var.ibmcloud_api_key
 }
@@ -24,3 +26,9 @@ data "ibm_tg_gateways" "all_tg_gws"{
 data "ibm_tg_locations" "tg_locations" {
 }
  
+resource "ibm_tg_connection" "test_ibm_tg_connection"{
+		gateway = "${ibm_tg_gateway.new_tg_gw.id}"
+		network_type = "vpc"
+		name= "myconnection"
+		network_id = ibm_is_vpc.test_tg_vpc.resource_crn
+}
