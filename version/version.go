6d8237a6c054610fbd5cbe713d889124ae8a5be1
umarali-nagoor <umarali.nagoor@in.ibm.com>
2020-07-22 11:14:11 +0530

    Bump version to v0.31.0

--- version/version.go
+++ version/version.go
@@ -5,7 +5,7 @@ import (
 )
 
 // Version is the current provider main version
-const Version = "0.30.1"
+const Version = "0.31.0"
 
 // GitCommit is the git commit that was compiled. This will be filled in by the compiler.
 var GitCommit string
