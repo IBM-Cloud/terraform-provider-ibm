4f0a217bca5240c460af51396ee4c52bc56c7f3f
Wilma Wang <wilmawang@ibm.com>
2020-07-27 17:12:16 -0700

    IBM Event Streams support

---
+++ examples/ibm-event-streams/main.tf
@@ -0,0 +1,60 @@
+
+data "ibm_resource_group" "group" {
+  name = "Default"
+}
+
+#### Scenario 1: Create Event Streams service instance and topic
+resource "ibm_resource_instance" "es_instance_1" {
+  name              = "terraform-integration-1"
+  service           = "messagehub"
+  plan              = "standard" # "lite", "enterprise-3nodes-2tb"
+  location          = "us-south" # "us-east", "eu-gb", "eu-de", "jp-tok", "au-syd"
+  resource_group_id = data.ibm_resource_group.group.id
+}
+
+resource "ibm_event_streams_topic" "es_topic_1" {
+  resource_instance_id = ibm_resource_instance.es_instance_1.id
+  name                 = "my-es-topic"
+  partitions           = 1
+  config = {
+    "cleanup.policy"  = "compact,delete"
+    "retention.ms"    = "86400000"
+    "retention.bytes" = "1073741824"
+    "segment.bytes"   = "536870912"
+  }
+}
+
+#### Scenario 2: Create topic on an existing Event Streams instance
+data "ibm_resource_instance" "es_instance_2" {
+  name              = "terraform-integration-2"
+  resource_group_id = data.ibm_resource_group.group.id
+}
+
+resource "ibm_event_streams_topic" "es_topic_2" {
+  resource_instance_id = data.ibm_resource_instance.es_instance_2.id
+  name                 = "my-es-topic"
+  partitions           = 1
+  config = {
+    "cleanup.policy"  = "compact,delete"
+    "retention.ms"    = "86400000"
+    "retention.bytes" = "1073741824"
+    "segment.bytes"   = "536870912"
+  }
+}
+
+#### Scenario 3: Create a kafka consumer application connecting to an existing Event Streams instance and its topics
+data "ibm_resource_instance" "es_instance_3" {
+  name              = "terraform-integration-3"
+  resource_group_id = data.ibm_resource_group.group.id
+}
+
+data "ibm_event_streams_topic" "es_topic_3" {
+  resource_instance_id = data.ibm_resource_instance.es_instance_3.id
+  name                 = "my-es-topic"
+}
+
+resource "kafka_consumer_app" "es_kafka_app" {
+  bootstrap_server = lookup(data.ibm_resource_instance.es_instance_3.extensions, "kafka_brokers_sasl", [])
+  topics           = [data.ibm_event_streams_topic.es_topic_3.name]
+  apikey           = var.es_reader_api_key
+}
---
+++ ibm/data_source_ibm_event_streams_topic_test.go
@@ -0,0 +1,71 @@
+package ibm
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+var (
+	MZREnterpriseInstanceName = "mh-preprod-customer-us-south-wp"
+	SZREnterpriseInstanceName = "mh-preprod-customer-us-south-szr"
+	standardInstanceName      = "hyperion-preprod-spp-a-service"
+	topicName                 = "kafka-java-console-sample-topic"
+)
+
+func TestAccIBMEventStreamsTopicDataSourceBasic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccCheckIBMEventStreamsTopicDataSourceConfigBasic(MZREnterpriseInstanceName, topicName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.ibm_resource_instance.es_instance", "extensions.kafka_brokers_sasl.0"),
+					resource.TestCheckResourceAttrSet("data.ibm_resource_instance.es_instance", "extensions.kafka_http_url"),
+					resource.TestCheckResourceAttrSet("data.ibm_event_streams_topic.es_topic", "id"),
+					resource.TestCheckResourceAttr("data.ibm_event_streams_topic.es_topic", "name", topicName),
+					resource.TestCheckResourceAttrSet("data.ibm_event_streams_topic.es_topic", "kafka_brokers_sasl.0"),
+					resource.TestCheckResourceAttrSet("data.ibm_event_streams_topic.es_topic", "kafka_http_url"),
+				),
+			},
+			resource.TestStep{
+				Config: testAccCheckIBMEventStreamsTopicDataSourceConfigBasic(SZREnterpriseInstanceName, topicName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.ibm_resource_instance.es_instance", "extensions.kafka_brokers_sasl.0"),
+					resource.TestCheckResourceAttrSet("data.ibm_resource_instance.es_instance", "extensions.kafka_http_url"),
+					resource.TestCheckResourceAttrSet("data.ibm_event_streams_topic.es_topic", "id"),
+					resource.TestCheckResourceAttr("data.ibm_event_streams_topic.es_topic", "name", topicName),
+					resource.TestCheckResourceAttrSet("data.ibm_event_streams_topic.es_topic", "kafka_brokers_sasl.0"),
+					resource.TestCheckResourceAttrSet("data.ibm_event_streams_topic.es_topic", "kafka_http_url"),
+				),
+			},
+			resource.TestStep{
+				Config: testAccCheckIBMEventStreamsTopicDataSourceConfigBasic(standardInstanceName, topicName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.ibm_resource_instance.es_instance", "extensions.kafka_brokers_sasl.0"),
+					resource.TestCheckResourceAttrSet("data.ibm_resource_instance.es_instance", "extensions.kafka_http_url"),
+					resource.TestCheckResourceAttrSet("data.ibm_event_streams_topic.es_topic", "id"),
+					resource.TestCheckResourceAttr("data.ibm_event_streams_topic.es_topic", "name", topicName),
+					resource.TestCheckResourceAttrSet("data.ibm_event_streams_topic.es_topic", "kafka_brokers_sasl.0"),
+					resource.TestCheckResourceAttrSet("data.ibm_event_streams_topic.es_topic", "kafka_http_url"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckIBMEventStreamsTopicDataSourceConfigBasic(instancecName, topicName string) string {
+	return fmt.Sprintf(`
+	data "ibm_resource_group" "my_group" {
+		name = "Default"
+	  }
+	data "ibm_resource_instance" "es_instance" {
+		resource_group_id = data.ibm_resource_group.my_group.id
+		name              = "%s"
+	}
+	data "ibm_event_streams_topic" "es_topic" {
+		resource_instance_id = data.ibm_resource_instance.es_instance.id
+		name                 = "%s"
+	}`, instancecName, topicName)
+}
