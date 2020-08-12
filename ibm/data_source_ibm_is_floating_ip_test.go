8f3856801deb2d8ea0b51de156d406dea7829f0a
umarali-nagoor <umarali.nagoor@in.ibm.com>
2020-08-12 16:31:44 +0530

    DS: VPC Floating IP

---
+++ ibm/data_source_ibm_is_floating_ip_test.go
@@ -0,0 +1,81 @@
+package ibm
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccIBMISFloatingIPDataSource_basic(t *testing.T) {
+	resName := "data.ibm_is_floating_ip.test"
+
+	vpcname := fmt.Sprintf("tfip-vpc-%d", acctest.RandIntRange(10, 100))
+	fipname := fmt.Sprintf("tfip-%d", acctest.RandIntRange(10, 100))
+	instancename := fmt.Sprintf("tfip-instance-%d", acctest.RandIntRange(10, 100))
+	subnetname := fmt.Sprintf("tfip-subnet-%d", acctest.RandIntRange(10, 100))
+	publicKey := strings.TrimSpace(`
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCKVmnMOlHKcZK8tpt3MP1lqOLAcqcJzhsvJcjscgVERRN7/9484SOBJ3HSKxxNG5JN8owAjy5f9yYwcUg+JaUVuytn5Pv3aeYROHGGg+5G346xaq3DAwX6Y5ykr2fvjObgncQBnuU5KHWCECO/4h8uWuwh/kfniXPVjFToc+gnkqA+3RKpAecZhFXwfalQ9mMuYGFxn+fwn8cYEApsJbsEmb0iJwPiZ5hjFC8wREuiTlhPHDgkBLOiycd20op2nXzDbHfCHInquEe/gYxEitALONxm0swBOwJZwlTDOB7C6y2dzlrtxr1L59m7pCkWI4EtTRLvleehBoj3u7jB4usR
+`)
+	sshname := fmt.Sprintf("tfip-sshname-%d", acctest.RandIntRange(10, 100))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccCheckIBMISFloatingIPDataSourceConfig(vpcname, subnetname, sshname, publicKey, instancename, fipname),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resName, "name", fipname),
+					resource.TestCheckResourceAttr(resName, "zone", ISZoneName),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckIBMISFloatingIPDataSourceConfig(vpcname, subnetname, sshname, publicKey, instancename, fipname string) string {
+	// status filter defaults to empty
+	return fmt.Sprintf(`
+
+	resource "ibm_is_vpc" "testacc_vpc" {
+		name = "%s"
+	  }
+	  
+	  resource "ibm_is_subnet" "testacc_subnet" {
+		name            = "%s"
+		vpc             = ibm_is_vpc.testacc_vpc.id
+		zone            = "%s"
+		ipv4_cidr_block = "%s"
+	  }
+	  
+	  resource "ibm_is_ssh_key" "testacc_sshkey" {
+		name       = "%s"
+		public_key = "%s"
+	  }
+	  
+	  resource "ibm_is_instance" "testacc_instance" {
+		name    = "%s"
+		image   = "%s"
+		profile = "%s"
+		primary_network_interface {
+		  port_speed = "100"
+		  subnet     = ibm_is_subnet.testacc_subnet.id
+		}
+		vpc  = ibm_is_vpc.testacc_vpc.id
+		zone = "%s"
+		keys = [ibm_is_ssh_key.testacc_sshkey.id]
+	  }
+	  
+	  resource "ibm_is_floating_ip" "testacc_floatingip" {
+		name   = "%s"
+		target = ibm_is_instance.testacc_instance.primary_network_interface[0].id
+	  }
+
+	  data "ibm_is_floating_ip" "test" {
+		name   = ibm_is_floating_ip.testacc_floatingip.name
+	  }
+	  `, vpcname, subnetname, ISZoneName, ISCIDR, sshname, publicKey, instancename, isImage, instanceProfileName, ISZoneName, fipname)
+}
---
+++ website/docs/d/is_floating_ip.html.markdown
@@ -0,0 +1,36 @@
+---
+layout: "ibm"
+page_title: "IBM : floating_ip"
+sidebar_current: "docs-ibm-datasource-floating-ip"
+description: |-
+  Fechtes floating ip information.
+---
+
+# ibm\_floating\_ip
+
+Import the details of vpc floating ip on IBM Cloud as a read-only data source. You can then reference the fields of the data source in other resources within the same configuration using interpolation syntax.
+
+## Example Usage
+
+```hcl
+
+    data "ibm_is_floating_ip" "test" {
+        name   = "test-fp"
+    }
+
+```
+## Argument Reference
+
+The following arguments are supported:
+
+* `name` - (Required, string) The name of the floating ip.
+
+## Attribute Reference
+
+The following attributes are exported:
+* `id` - An alphanumeric value identifying the floating ip.	
+* `address` - Floating IP address that was created.
+* `status` - Provisioning status of the floating IP address. 
+* `tags` - Tags associate with VPC.
+* `target` -  ID of the network interface use to allocate the IP address.
+* `zone` -   Name of the zone where to create the floating IP address. 
