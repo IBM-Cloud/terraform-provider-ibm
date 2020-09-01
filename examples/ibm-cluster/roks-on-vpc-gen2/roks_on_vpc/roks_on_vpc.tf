1fd738df5202056c07ca95c3768d12aa8f6d7756
umarali-nagoor <umarali.nagoor@in.ibm.com>
2020-09-01 18:23:25 +0530

    Updated cluster examples with crk key_id in kms_config

--- examples/ibm-cluster/roks-on-vpc-gen2/roks_on_vpc/roks_on_vpc.tf
+++ examples/ibm-cluster/roks-on-vpc-gen2/roks_on_vpc/roks_on_vpc.tf
@@ -87,7 +87,7 @@ resource "ibm_container_vpc_cluster" "cluster" {
 
   kms_config {
     instance_id = ibm_resource_instance.kms_instance1.guid
-    crk_id = ibm_kms_key.test.id
+    crk_id = ibm_kms_key.test.key_id
     private_endpoint = false
   }
 
