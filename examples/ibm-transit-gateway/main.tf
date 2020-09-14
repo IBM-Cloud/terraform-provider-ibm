a33f570c185f900192045551736dea0fc26c3734
Pinky Bhargava <pinky.bharagava@in.ibm.com>
2020-09-14 12:41:53 +0530

    updated for tg autodeploy

--- examples/ibm-transit-gateway/main.tf
+++ examples/ibm-transit-gateway/main.tf
@@ -1,47 +1,44 @@
-data "ibm_resource_group" "rg" {
-  name = var.resource_group
+# Create a transit gateway
+resource "ibm_tg_gateway" "new_tg_gw"{
+        name=var.name
+        location=var.location
+        global=true
+//      resource_group = data.ibm_resource_group.rg.id
 }
 resource "ibm_is_vpc" "test_tg_vpc" {
-  name = "myvpc"
+  name = var.vpc_name
 }
-provider "ibm" {
-  ibmcloud_api_key = var.ibmcloud_api_key
+# Add connection to a Transit Gateway
+resource "ibm_tg_connection" "test_ibm_tg_connection"{
+                gateway = "${ibm_tg_gateway.new_tg_gw.id}"
+                network_type = var.network_type
+                name = var.vc_name
+                network_id = ibm_is_vpc.test_tg_vpc.resource_crn
+}
+/*
+# Create a transit gateway cross account connection
+resource "ibm_tg_connection" "test_tg_cross_connection"{
+                gateway = "${ibm_tg_gateway.new_tg_gw.id}"
+                network_type = var.network_type
+                name= var.vc_name
+                # vpc crn from other account
+                network_id = var.network_id
+                network_account_id = var.network_account_id
 }
 
-resource "ibm_tg_gateway" "new_tg_gw"{
-name="tg-gateway-1"
-location="us-south"
-global=true
-resource_group = data.ibm_resource_group.rg.id
-} 
-
+# Retrieves specified Transit Gateway
 data "ibm_tg_gateway" "tg_gateway" {
-name="tg-gateway-1"
+        name= ibm_tg_gateway.new_tg_gw.name
 }
-
+# List all the Transit Gateways in the account.
 data "ibm_tg_gateways" "all_tg_gws"{
-
 }
-
+# List all locations that support Transit Gateways
 data "ibm_tg_locations" "tg_locations" {
 }
-
+# Get the details of a Transit Gateway Location.
 data "ibm_tg_location" "tg_location" {
-	name = "us-south"
-} 
- 
-resource "ibm_tg_connection" "test_ibm_tg_connection"{
-		gateway = "${ibm_tg_gateway.new_tg_gw.id}"
-		network_type = "vpc"
-		name= "myconnection"
-		network_id = ibm_is_vpc.test_tg_vpc.resource_crn
+        name = "us-south"
 }
-// cross account
-resource "ibm_tg_connection" "test_ibm_tg_connection"{
-		gateway = "${ibm_tg_gateway.new_tg_gw.id}"
-		network_type = "vpc"
-		name= "mycrossconnection"
-		// vpc crn from other account
-		network_id = var.network_id
-		network_account_id = var.network_account_id
-}
+
+*/
