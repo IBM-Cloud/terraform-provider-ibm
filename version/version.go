f2b3d7fa3ed6508906f81ba24cc31a488f59cea7
hkantare@in.ibm.com <hkantare@in.ibm.com>
2020-09-14 16:23:39 +0530

    Bump up version to 1.12.0

--- version/version.go
+++ version/version.go
@@ -5,7 +5,7 @@ import (
 )
 
 // Version is the current provider main version
-const Version = "1.11.2"
+const Version = "1.12.0"
 
 // GitCommit is the git commit that was compiled. This will be filled in by the compiler.
 var GitCommit string
