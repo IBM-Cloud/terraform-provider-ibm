195b17e53871f3b159dc7da7b195eec514a6df76
Kavya.Handadi <kavya.handadi@gmail.com>
2020-09-14 11:40:18 +0530

    Support: Cluster Addons

---
+++ ibm/resource_ibm_container_addons_test.go
@@ -0,0 +1,102 @@
+package ibm
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+
+	v1 "github.com/IBM-Cloud/bluemix-go/api/container/containerv1"
+)
+
+func TestAccIBMContainerAddOns_Basic(t *testing.T) {
+	clusterName := fmt.Sprintf("terraform-%d", acctest.RandIntRange(10, 100))
+	vpc := fmt.Sprintf("terraform-vpc-%d", acctest.RandIntRange(10, 100))
+	subnet := fmt.Sprintf("terraform-subnet-%d", acctest.RandIntRange(10, 100))
+	flavor := "c2.2x4"
+	zone := "us-south"
+	workerCount := "1"
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckIBMContainerAddOnsDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccCheckIBMContainerAddOnsBasic(clusterName, zone, vpc, subnet, flavor, workerCount),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"ibm_container_addons.addons", "addons.#", "3"),
+				),
+			},
+			resource.TestStep{
+				Config: testAccCheckIBMContainerAddOnsUpdate(zone, vpc, subnet, clusterName, flavor, workerCount),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"ibm_container_addons.addons", "addons.#", "3"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckIBMContainerAddOnsDestroy(s *terraform.State) error {
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "ibm_container_addons" {
+			continue
+		}
+		targetEnv := v1.ClusterTargetHeader{
+			Region: "us-south",
+		}
+		csClient, err := testAccProvider.Meta().(ClientSession).ContainerAPI()
+		if err != nil {
+			return err
+		}
+		cluster := rs.Primary.ID
+		addOnAPI := csClient.AddOns()
+		_, err = addOnAPI.GetAddons(cluster, targetEnv)
+		if err != nil && !strings.Contains(err.Error(), "404") {
+			return fmt.Errorf("Error checking if AddOns (%s) has been destroyed: %s", rs.Primary.ID, err)
+		}
+	}
+	return nil
+}
+
+func testAccCheckIBMContainerAddOnsBasic(zone, vpc, subnet, clusterName, flavor, workerCount string) string {
+	return testAccCheckIBMContainerVpcOcpClusterGen2basic(zone, vpc, subnet, clusterName, flavor, workerCount) + fmt.Sprintf(`
+	resource "ibm_container_addons" "addons" {
+	cluster = ibm_container_vpc_cluster.clustergen2.name
+	addons {
+		name    = "istio"
+		version = "1.6"
+	}
+	addons {
+		name    = "vpc-block-csi-driver"
+		version = "2.0.3"
+	}
+	addons {
+		name    = "static-route"
+		version = "1.0.0"
+	}
+}`)
+}
+func testAccCheckIBMContainerAddOnsUpdate(clusterName, zone, vpc, subnet, flavor, workerCount string) string {
+	return testAccCheckIBMContainerVpcOcpClusterGen2basic(zone, vpc, subnet, clusterName, flavor, workerCount) + fmt.Sprintf(`
+	resource "ibm_container_addons" "addons" {
+	cluster = ibm_container_vpc_cluster.clustergen2.name
+	addons {
+		name    = "istio"
+		version = "1.7"
+	}
+	addons {
+		name    = "vpc-block-csi-driver"
+		version = "2.0.3"
+	}
+	addons {
+		name    = "kube-terminal"
+		version = "1.0.0"
+	}
+}`)
+}
---
+++ website/docs/r/container_addons.html.markdown
@@ -0,0 +1,58 @@
+---
+layout: "ibm"
+page_title: "IBM: container_addons"
+sidebar_current: "docs-ibm-resource-container-addons"
+description: |-
+  Manages IBM container addons.
+---
+
+# ibm\_container_addons
+
+Enable, update or Disable a single AddOn or a Set of AddOns. 
+
+## Example Usage
+
+In the following example, you can configure a Addons:
+
+```hcl
+resource "ibm_container_addons" "addons" {
+  cluster = ibm_container_vpc_cluster.cluster.name
+  addons {
+    name    = "istio"
+    version = "1.6"
+  }
+}
+
+```
+
+## Timeouts
+
+ibm_container_addons provides the following [Timeouts](https://www.terraform.io/docs/configuration/resources.html#timeouts) configuration options:
+
+* `create` - (Default 20 minutes) Used for creating Instance.
+* `update` - (Default 20 minutes) Used for updating Instance.
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `cluster` - (Required, string) Cluster Name or ID
+* `addons` - (Required, set) Set of AddOns that has to be enabled
+    * `name` - (Optional, string) The addon name such as 'istio'.
+    * `version` - (Optional,string) The addon version, omit the version if you wish to use the default version. It is required when one wants to update the addon to specified version.
+
+## Attribute Reference
+
+The following attributes are exported:
+
+* `id` - The AddOns ID.
+* `addons` - Details of Enabled AddOns
+    * `allowed_upgrade_versions` - The versions that the addon can be upgraded to 
+    * `deprecated` - Determines if this addon version is deprecated
+    * `health_state` - The health state for this addon, a short indication (e.g. critical, pending)
+    * `health_status` - The health status for this addon, provides a description of the state (e.g. error message)
+    * `min_kube_version` - The minimum kubernetes version for this addon.
+    * `min_ocp_version` - The minimum OpenShift version for this addon.
+    * `supported_kube_range` - The supported kubernetes version range for this addon.
+    * `target_version` - The addon target version.
+    * `vlan_spanning_required` - VLAN spanning required for multi-zone clusters.
