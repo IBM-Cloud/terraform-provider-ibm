38e7fa9c0663573936ed9acdbbf383923dda12cf
Nimisha Thekkarath <NIMISHA.Thekkarath@ibm.com>
2020-09-22 15:27:56 +0530

    Data block for Load Balancer Profiles

---
+++ ibm/data_source_ibm_is_lb_profiles_test.go
@@ -0,0 +1,48 @@
+package ibm
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccIBMISLBProfilesDatasource_basic(t *testing.T) {
+	name := fmt.Sprintf("tflb-name-%d", acctest.RandIntRange(10, 100))
+	vpcname := fmt.Sprintf("tflb-vpc-%d", acctest.RandIntRange(10, 100))
+	subnetname := fmt.Sprintf("tflb-subnet-name-%d", acctest.RandIntRange(10, 100))
+	var lb string
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+
+				Config: testDSCheckIBMISLBProfilesConfig(vpcname, subnetname, ISZoneName, ISCIDR, name),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckIBMISLBExists("ibm_is_lb.testacc_lb", lb),
+					resource.TestCheckResourceAttrSet("data.ibm_is_lb_profiles.test_profiles", "lb_profiles.#"),
+				),
+			},
+		},
+	})
+}
+func testDSCheckIBMISLBProfilesConfig(vpcname, subnetname, zone, cidr, name string) string {
+	return fmt.Sprintf(`
+resource "ibm_is_vpc" "testacc_vpc" {
+  name = "%s"
+}
+resource "ibm_is_subnet" "testacc_subnet" {
+  name            = "%s"
+  vpc             = ibm_is_vpc.testacc_vpc.id
+  zone            = "%s"
+  ipv4_cidr_block = "%s"
+}
+resource "ibm_is_lb" "testacc_lb" {
+  name    = "%s"
+  subnets = [ibm_is_subnet.testacc_subnet.id]
+}
+data "ibm_is_lb_profiles" "test_profiles" {
+} `, vpcname, subnetname, zone, cidr, name)
+}
