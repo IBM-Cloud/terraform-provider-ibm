b20b92c575c2de41705b736a35ecba407e44824b
AnilCM <amallak1@in.ibm.com>
2020-09-28 18:10:57 +0530

    vendored bluemix-go changes

--- vendor/github.com/IBM-Cloud/bluemix-go/endpoints/endpoints.go
+++ vendor/github.com/IBM-Cloud/bluemix-go/endpoints/endpoints.go
@@ -92,6 +92,7 @@ var regionToEndpoint = map[string]map[string]string{
 		"jp-tok":   "https://api.jp-tok.databases.cloud.ibm.com",
 		"osl01":    "https://api.osl01.databases.cloud.ibm.com",
 		"seo01":    "https://api.seo01.databases.cloud.ibm.com",
+		"che01":    "https://api.che01.databases.cloud.ibm.com",
 	},
 	"mccp": {
 		"us-south": "https://mccp.us-south.cf.cloud.ibm.com",
