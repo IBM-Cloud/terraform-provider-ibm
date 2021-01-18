26b4be91f1bd5df1bbed1e0b554dc56abffb3f75
kavya498 <hkavya26@in.ibm.com>
2021-01-18 11:38:27 +0530

    Remove HPCS Init from Examples

--- examples/ibm-hpcs-crypto/variables.tf
+++ examples/ibm-hpcs-crypto/variables.tf
@@ -26,35 +26,7 @@ variable "units" {
   description = "No of crypto units that has to be attached to the instance."
 }
 
-# COS Credentials
-variable "api_key" {
-  type        = string
-  description = "api key of the COS bucket"
-}
-variable "cos_crn" {
-  type        = string
-  description = "COS instance CRN"
-}
-variable "endpoint" {
-  type        = string
-  description = "COS endpoint"
-}
-variable "bucket_name" {
-  type        = string
-  description = "COS bucket name"
-}
 
-# Input Json file
-variable "input_file_name" {
-  type        = string
-  description = "Input json file name that is present in the cos-bucket or in the local"
-}
-# Path to which CLOUDTKEFILES has to be exported
-variable "tke_files_path" {
-  default     = "/Users/Kavya"
-  type        = string
-  description = "Path to which tke files has to be exported"
-}
 # Key name that has to be created on the HPCS Instance
 variable "key_name" {
   type        = string
