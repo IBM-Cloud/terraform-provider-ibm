ffc416b8ae6b75c765a6cd47000f9cf861b708e4
Deepak Selvakumar <Deepak.Selvakumar@ibm.com>
2021-01-19 11:28:56 +0530

    issue 2058 Update data_source_ibm_is_subnet.go

--- ibm/data_source_ibm_is_subnet.go
+++ ibm/data_source_ibm_is_subnet.go
@@ -238,6 +238,9 @@ func subnetGetByNameOrID(d *schema.ResourceData, meta interface{}) error {
 				break
 			}
 		}
+		if subnet == nil {
+			return fmt.Errorf("No subnet found with name (%s)", name)
+		}
 	}
 
 	d.SetId(*subnet.ID)
