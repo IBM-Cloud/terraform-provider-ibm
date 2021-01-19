1831c92f8d231e38066f5d5bd290eec3703055d3
vaishnavi <ggunishetty9@gmail.com>
2021-01-19 15:59:46 +0530

    fixed keyname lookup issue #2158

--- ibm/resource_ibm_storage_file.go
+++ ibm/resource_ibm_storage_file.go
@@ -1230,7 +1230,7 @@ func getSaaSPerformSpacePrice(productItems []datatypes.Product_Item, size int) (
 		}
 
 		keyname := fmt.Sprintf("%d_%d_GBS", capacityMinimum, capacityMaximum)
-		if *item.KeyName != keyname {
+		if item.KeyName == nil || !strings.Contains(*item.KeyName, keyname) {
 			continue
 		}
 
@@ -1291,7 +1291,7 @@ func getSaaSEnduranceSpacePrice(productItems []datatypes.Product_Item, size int,
 
 	for _, item := range productItems {
 
-		if *item.KeyName != keyName {
+		if item.KeyName == nil || !strings.Contains(*item.KeyName, keyName) {
 			continue
 		}
 
