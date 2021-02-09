786fc26b2e43b09c566ad0d17d3fa67920806640
Anil Kumar Nagaraj <anilkumar@in.ibm.com>
2021-02-09 20:31:12 +0530

    Add copyright information to src code

--- ibm/data_source_ibm_pi_image.go
+++ ibm/data_source_ibm_pi_image.go
@@ -1,3 +1,11 @@
+/**
+ * © Copyright IBM Corporation 2020. All Rights Reserved.
+ *
+ * Licensed under the Mozilla Public License, version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at https://mozilla.org/MPL/2.0/
+ */
+
 package ibm
 
 import (
