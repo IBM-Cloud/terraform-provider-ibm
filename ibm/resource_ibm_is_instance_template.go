36dbcd40d9a804ad2ec2feb924e9067ad2c21330
Deepak Selvakumar <Deepak.Selvakumar@ibm.com>
2021-02-19 20:03:29 +0530

    issue 2237 changes in boot volume attachment in instance template

--- ibm/resource_ibm_is_instance_template.go
+++ ibm/resource_ibm_is_instance_template.go
@@ -224,17 +224,10 @@ func resourceIBMISInstanceTemplate() *schema.Resource {
 						},
 						isInstanceTemplateBootSize: {
 							Type:     schema.TypeInt,
-							Optional: true,
-							Computed: true,
-						},
-						isInstanceTemplateBootIOPS: {
-							Type:     schema.TypeInt,
-							Optional: true,
 							Computed: true,
 						},
 						isInstanceTemplateBootProfile: {
 							Type:     schema.TypeString,
-							Optional: true,
 							Computed: true,
 						},
 						isInstanceTemplateVolumeDeleteOnInstanceDelete: {
@@ -341,24 +334,12 @@ func instanceTemplateCreate(d *schema.ResourceData, meta interface{}, profile, n
 			volTemplate.Name = &namestr
 		}
 
-		if volcap, ok := bootvol[isInstanceTemplateBootSize]; ok {
-			if volcapint64 := int64(volcap.(int)); volcapint64 != 0 {
-				volTemplate.Capacity = &volcapint64
-			}
-		}
-
-		if volprof, ok := bootvol[isInstanceTemplateBootProfile]; ok {
-			if volumeProfile := volprof.(string); volumeProfile != "" {
-				volTemplate.Profile = &vpcv1.VolumeProfileIdentity{
-					Name: &volumeProfile,
-				}
-			}
-		}
-
-		if iops, ok := bootvol[isInstanceTemplateBootIOPS]; ok {
-			if bootVolIOPS := int64(iops.(int)); bootVolIOPS != 0 {
-				volTemplate.Iops = &bootVolIOPS
-			}
+		volcap := 100
+		volcapint64 := int64(volcap)
+		volprof := "general-purpose"
+		volTemplate.Capacity = &volcapint64
+		volTemplate.Profile = &vpcv1.VolumeProfileIdentity{
+			Name: &volprof,
 		}
 
 		if encryption, ok := bootvol[isInstanceTemplateBootEncryption]; ok {
@@ -649,6 +630,12 @@ func instanceTemplateGet(d *schema.ResourceData, meta interface{}, ID string) er
 		bootVol[isInstanceTemplateBootName] = *instance.BootVolumeAttachment.Name
 		bootVol[isInstanceTemplateVolAttVolume] = *instance.BootVolumeAttachment.Volume
 		bootVol[isInstanceTemplateDeleteVolume] = *instance.BootVolumeAttachment.DeleteVolumeOnInstanceDelete
+		volumeIntf := instance.BootVolumeAttachment.Volume
+		bootVol[isInstanceTemplatesVol] = volumeIntf.Name
+		bootVol[isInstanceTemplateBootSize] = volumeIntf.Capacity
+		volProfIntf := instance.BootVolumeAttachment.Volume.Profile
+		volProfInst := volProfIntf.(*vpcv1.VolumeProfileIdentity)
+		bootVol[isInstanceTemplateBootProfile] = volProfInst.Name
 		bootVolList = append(bootVolList, bootVol)
 		d.Set(isInstanceTemplateBootVolume, bootVolList)
 	}
