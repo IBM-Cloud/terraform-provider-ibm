8525b0d3366e6f27a7e716705e3bff28523810e3
Ujjwal Kumar <Ujjwal.Kumar1@ibm.com>
2021-04-19 18:17:04 +0530

    Revert "Merge branch 'fix-sdk-snapshot' of github.com:ujjwal-ibm/terraform-provider-ibm into enhancemenet-instanceSupportForSnapshot"
    This reverts commit 886b822414c04cc1ec2de454dd65c492c594c66a, reversing
    changes made to 57a88fef1886fa8de59db6b8cf60ce90f7582dbe.

--- ibm/data_source_ibm_is_instance_templates.go
+++ ibm/data_source_ibm_is_instance_templates.go
@@ -354,7 +354,7 @@ func dataSourceIBMISInstanceTemplatesRead(d *schema.ResourceData, meta interface
 				volumeAttach[isInstanceTemplateVolAttName] = *volume.Name
 				volumeAttach[isInstanceTemplateDeleteVolume] = *volume.DeleteVolumeOnInstanceDelete
 				volumeIntf := volume.Volume
-				volumeInst := volumeIntf.(*vpcv1.VolumeAttachmentPrototypeVolume)
+				volumeInst := volumeIntf.(*vpcv1.VolumeAttachmentVolumePrototypeInstanceContext)
 				volumeAttach[isInstanceTemplateVolAttVolume] = volumeInst.Name
 				interfacesList = append(interfacesList, volumeAttach)
 			}
