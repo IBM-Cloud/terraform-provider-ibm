37584d5cf1b59a1d24473be750de1d0ce5940d47
poorani <poorani.senthilkumar.sankari@ibm.com>
2021-05-04 17:21:41 +0100

    updating example - main.tf file

--- examples/ibm-iam_identity-apikeys/main.tf
+++ examples/ibm-iam_identity-apikeys/main.tf
@@ -4,15 +4,13 @@ provider "ibm" {
 
 // Read iam_api_key data source
 data "ibm_iam_api_key" "iam_api_key" {
-  id= "ApiKey-toRead"
+  apikey_id= "ApiKey-toRead"
 }
 
 // Provision iam_api_key resource instance
 resource "ibm_iam_api_key" "iam_api_key_instance" {
   name = "apikey name"
-  iam_id = "IBMid"
   description = "apikey desc"
-  account_id = "unique account id"
 }
 
 
