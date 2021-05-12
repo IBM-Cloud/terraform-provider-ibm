c82178b92e251ee6098b59ea92801a031f9f2642
Jeff Sloyer <jsloyer@gmail.com>
2021-05-12 17:31:50 -0400

    update azure script to grow root volume group

--- ibm/data_source_ibm_satellite_host_script.go
+++ ibm/data_source_ibm_satellite_host_script.go
@@ -120,7 +120,25 @@ func dataSourceIBMSatelliteAttachHostScriptRead(d *schema.ResourceData, meta int
 			} else if strings.ToLower(hostProvider) == "ibm" {
 				lines[i] = "subscription-manager refresh\nsubscription-manager repos --enable=*\n"
 			} else if strings.ToLower(hostProvider) == "azure" {
-				lines[i] = "# Grow the TMP LV\nlvextend -L+10G /dev/rootvg/tmplv\nxfs_growfs /dev/rootvg/tmplv\n# Grow the var LV\nlvextend -L+20G /dev/rootvg/varlv\nxfs_growfs /dev/rootvg/varlv\nyum update -y\nyum-config-manager --enable '*'\nyum repolist all\nyum install container-selinux -y"
+				lines[i] = fmt.Sprintf(`
+					#Grow the base volume group first
+					echo -e "r\ne\ny\nw\ny\ny\n" | gdisk /dev/sda
+					echo -e "n\n\n\n\n\nw\n" | fdisk /dev/sda
+					partx -l /dev/sda
+					partx -v -a /dev/sda
+					pvcreate /dev/sda5
+					vgextend rootvg /dev/sda5
+					# Grow the TMP LV
+					lvextend -L+10G /dev/rootvg/tmplv
+					xfs_growfs /dev/rootvg/tmplv
+					# Grow the var LV
+					lvextend -L+20G /dev/rootvg/varlv
+					xfs_growfs /dev/rootvg/varlv
+					yum update -y
+					yum-config-manager --enable '*'
+					yum repolist all
+					yum install container-selinux -y
+				)`)
 			}
 		}
 	}
