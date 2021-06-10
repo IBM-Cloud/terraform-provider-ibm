e523e6ff1fce5c02bfbc11edc9853ac2801be92d
Sunitha Gudisagar <Sunitha.Gudisagar@ibm.com>
2021-06-10 16:47:37 +0530

    MigrationPR: VPC-GO-SDK version to v0.7.0

--- ibm/data_source_ibm_is_instance_group_manager_action.go
+++ ibm/data_source_ibm_is_instance_group_manager_action.go
@@ -220,7 +220,7 @@ func dataSourceIBMISInstanceGroupManagerActionRead(d *schema.ResourceData, meta 
 			}
 			instanceGroupManagerScheduledActionManagerManagerInt := instanceGroupManagerAction.Manager
 			if instanceGroupManagerScheduledActionManagerManagerInt != nil {
-				instanceGroupManagerScheduledActionManagerManager := instanceGroupManagerScheduledActionManagerManagerInt.(*vpcv1.InstanceGroupManagerScheduledActionManagerManager)
+				instanceGroupManagerScheduledActionManagerManager := instanceGroupManagerScheduledActionManagerManagerInt.(*vpcv1.InstanceGroupManagerScheduledActionManager)
 				if instanceGroupManagerScheduledActionManagerManager != nil && instanceGroupManagerScheduledActionManagerManager.ID != nil {
 
 					if instanceGroupManagerScheduledActionManagerManager.MaxMembershipCount != nil {
