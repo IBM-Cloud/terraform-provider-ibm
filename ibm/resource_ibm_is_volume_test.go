2491372ae1bf99a3151a262af56434053e426f96
Ujjwal Kumar <Ujjwal.Kumar1@ibm.com>
2021-07-14 23:33:15 +0530

    added attachments delete on volume deletion

--- ibm/resource_ibm_is_volume_test.go
+++ ibm/resource_ibm_is_volume_test.go
@@ -6,6 +6,7 @@ package ibm
 import (
 	"errors"
 	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/IBM/vpc-go-sdk/vpcv1"
@@ -39,6 +40,51 @@ func TestAccIBMISVolume_basic(t *testing.T) {
 					testAccCheckIBMISVolumeExists("ibm_is_volume.storage", vol),
 					resource.TestCheckResourceAttr(
 						"ibm_is_volume.storage", "name", name1),
+				),
+			},
+		},
+	})
+}
+
+func TestAccIBMISVolumeAttachmentDelete_basic(t *testing.T) {
+	var vol string
+	insname := fmt.Sprintf("tf-ins-%d", acctest.RandIntRange(10, 100))
+	initialVolumeCapacityArray := fmt.Sprintf("[%d, %d]", 10, 20)
+	finalVolumeCapacityArray := fmt.Sprintf("[%d]", 10)
+	vpcname := fmt.Sprintf("tf-vpc-%d", acctest.RandIntRange(10, 100))
+	subnetname := fmt.Sprintf("tf-subnet-%d", acctest.RandIntRange(10, 100))
+	publicKey := strings.TrimSpace(`
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCKVmnMOlHKcZK8tpt3MP1lqOLAcqcJzhsvJcjscgVERRN7/9484SOBJ3HSKxxNG5JN8owAjy5f9yYwcUg+JaUVuytn5Pv3aeYROHGGg+5G346xaq3DAwX6Y5ykr2fvjObgncQBnuU5KHWCECO/4h8uWuwh/kfniXPVjFToc+gnkqA+3RKpAecZhFXwfalQ9mMuYGFxn+fwn8cYEApsJbsEmb0iJwPiZ5hjFC8wREuiTlhPHDgkBLOiycd20op2nXzDbHfCHInquEe/gYxEitALONxm0swBOwJZwlTDOB7C6y2dzlrtxr1L59m7pCkWI4EtTRLvleehBoj3u7jB4usR
+`)
+	sshname := fmt.Sprintf("tf-ssh-%d", acctest.RandIntRange(10, 100))
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckIBMISVolumeDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccCheckIBMISVolumeAttachmentDeleteConfig(vpcname, subnetname, sshname, publicKey, insname, initialVolumeCapacityArray),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckIBMISVolumeExists("ibm_is_volume.storage.0", vol),
+					resource.TestCheckResourceAttr(
+						"ibm_is_instance.testacc_instance", "name", insname),
+					resource.TestCheckResourceAttr(
+						"ibm_is_instance.testacc_instance", "volume_attachments.#", "3"),
+					resource.TestCheckResourceAttr(
+						"ibm_is_instance.testacc_instance", "volumes.#", "2"),
+				),
+			},
+
+			resource.TestStep{
+				Config: testAccCheckIBMISVolumeAttachmentDeleteConfig(vpcname, subnetname, sshname, publicKey, insname, finalVolumeCapacityArray),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckIBMISVolumeExists("ibm_is_volume.storage.0", vol),
+					resource.TestCheckResourceAttr(
+						"ibm_is_instance.testacc_instance", "name", insname),
+					resource.TestCheckResourceAttr(
+						"ibm_is_instance.testacc_instance", "volume_attachments.#", "2"),
+					resource.TestCheckResourceAttr(
+						"ibm_is_instance.testacc_instance", "volumes.#", "1"),
 				),
 			},
 		},
@@ -93,11 +139,61 @@ func testAccCheckIBMISVolumeExists(n, volID string) resource.TestCheckFunc {
 
 func testAccCheckIBMISVolumeConfig(name string) string {
 	return fmt.Sprintf(
-		`resource "ibm_is_volume" "storage"{
-    name = "%s"
-    profile = "10iops-tier"
-    zone = "us-south-3"
-    # capacity= 200
-}`, name)
+		`
+	resource "ibm_is_volume" "storage"{
+		name = "%s"
+		profile = "10iops-tier"
+		zone = "us-south-3"
+		# capacity= 200
+	}
+`, name)
+
+}
+
+func testAccCheckIBMISVolumeAttachmentDeleteConfig(vpcname, subnetname, sshname, publicKey, insname, capacityArray string) string {
+	return fmt.Sprintf(
+		`
+		variable "vsi_vol_size" {
+			description = "capacity array"
+			default     =  %s
+		}
+
+		resource "ibm_is_volume" "storage"{
+			name 	 = "tf-vol-att-${count.index}"
+			count 	 = length(var.vsi_vol_size)
+			profile  = "general-purpose"
+			zone 	 = "%s"
+			capacity = var.vsi_vol_size[count.index]
+		}
+
+		resource "ibm_is_vpc" "testacc_vpc" {
+			name = "%s"
+		}
+		  
+		resource "ibm_is_subnet" "testacc_subnet" {
+			name            			= "%s"
+			vpc             			= ibm_is_vpc.testacc_vpc.id
+			zone            			= "%s"
+			total_ipv4_address_count 	= 16
+		}
+		  
+		resource "ibm_is_ssh_key" "testacc_sshkey" {
+			name       					= "%s"
+			public_key 					= "%s"
+		}
+		  
+		resource "ibm_is_instance" "testacc_instance" {
+			name    		= "%s"
+			image   		= "%s"
+			profile 		= "%s"
+			volumes = ibm_is_volume.storage[*].id
+			primary_network_interface {
+				subnet     = ibm_is_subnet.testacc_subnet.id
+			}
+			vpc  = ibm_is_vpc.testacc_vpc.id
+			zone = "%s"
+			keys = [ibm_is_ssh_key.testacc_sshkey.id]
+		}
+`, capacityArray, ISZoneName, vpcname, subnetname, ISZoneName, sshname, publicKey, insname, isImage, instanceProfileName, ISZoneName)
 
 }
