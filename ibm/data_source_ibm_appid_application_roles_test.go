5abea18402ad6d2ca236346c81aed72cc34d8fcd
Darius Bakunas-Milanowski <dbakuna@us.ibm.com>
2021-08-12 09:56:17 -0400

    test: add appid application roles acc test

--- ibm/data_source_ibm_appid_application_roles_test.go
+++ ibm/data_source_ibm_appid_application_roles_test.go
@@ -20,7 +20,7 @@ func TestAccIBMAppIDApplicationRolesDataSource_basic(t *testing.T) {
 				Config: testAccCheckIBMAppIDApplicationRolesDataSourceConfig(appIDTenantID, appName, roleName),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("data.ibm_appid_application_roles.roles", "roles.#", "1"),
-					resource.TestCheckResourceAttrSet("data.ibm_appid_application_roles.roles", "roles.0.id"),
+					resource.TestCheckResourceAttrPair("ibm_appid_role.role", "role_id", "data.ibm_appid_application_roles.roles", "roles.0.id"),
 					resource.TestCheckResourceAttr("data.ibm_appid_application_roles.roles", "roles.0.name", roleName),
 				),
 			},
