0367af941df605bb57f03117e3adb50727b9ef5b
Darius <dbakuna@us.ibm.com>
2021-08-13 01:54:11 -0400

    AppID role datasource and resource (#2975)
    * feat: add AppID role data source
    
    * feat: add AppID role resource
    
    * feat: add appid application data source
    
    * feat: add appid application resource
    
    * test: add appid application acc tests
    
    * docs: add application datasource markdown
    
    * docs: application resource markdown
    
    * refactor: handle 404 for application read
    
    * fix: we may not want to clear resource id on 404
    
    * feat: appid application scopes data source
    
    * fix: clear state on 404 for consistency
    
    * feat: add application scopes resource
    
    * fix: since this is scopes resource, scopes argument should probably be required
    
    * test: add scopes datasource acc test
    
    * test: add application scopes resource acc test
    
    * docs: add application scopes resource markdown
    
    * docs: add application scopes resource markdown
    
    * test: add appid role acc test
    
    * refactor: remove unnecessary diags
    
    * refactor: remove unnecessary diags
    
    * refactor: remove unnecessary diags
    
    * refactor: minor, missed appid prefix
    
    * refactor: minor, missed appid prefix
    
    * test: add appid role resource acc test
    
    * docs: add html markdown for AppID role data source
    
    * docs: add html markdown for AppID role resource
    
    * chore: go fmt
    
    * refactor: remove extra diags
    
    * refactor: remove extra diags
    
    * docs: fix tf
    
    * refactor: remove extra diags

--- ibm/provider.go
+++ ibm/provider.go
@@ -173,7 +173,10 @@ func Provider() *schema.Provider {
 			"ibm_app_route":          dataSourceIBMAppRoute(),
 
 			// AppID
-			"ibm_appid_token_config": dataSourceIBMAppIDTokenConfig(),
+			"ibm_appid_application":        dataSourceIBMAppIDApplication(),
+			"ibm_appid_application_scopes": dataSourceIBMAppIDApplicationScopes(),
+			"ibm_appid_token_config":       dataSourceIBMAppIDTokenConfig(),
+			"ibm_appid_role":               dataSourceIBMAppIDRole(),
 
 			"ibm_function_action":                    dataSourceIBMFunctionAction(),
 			"ibm_function_package":                   dataSourceIBMFunctionPackage(),
@@ -429,7 +432,11 @@ func Provider() *schema.Provider {
 			"ibm_app_route":                         resourceIBMAppRoute(),
 
 			// AppID
-			"ibm_appid_token_config": resourceIBMAppIDTokenConfig(),
+
+			"ibm_appid_application":        resourceIBMAppIDApplication(),
+			"ibm_appid_application_scopes": resourceIBMAppIDApplicationScopes(),
+			"ibm_appid_token_config":       resourceIBMAppIDTokenConfig(),
+			"ibm_appid_role":               resourceIBMAppIDRole(),
 
 			"ibm_function_action":                                resourceIBMFunctionAction(),
 			"ibm_function_package":                               resourceIBMFunctionPackage(),
