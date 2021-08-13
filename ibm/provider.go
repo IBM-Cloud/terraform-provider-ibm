62013c03d823c772075e37f170dd1f111e606340
Darius Bakunas-Milanowski <dbakuna@us.ibm.com>
2021-08-13 08:50:08 -0400

    chore: go fmt

--- ibm/provider.go
+++ ibm/provider.go
@@ -176,7 +176,7 @@ func Provider() *schema.Provider {
 			"ibm_appid_application":        dataSourceIBMAppIDApplication(),
 			"ibm_appid_application_scopes": dataSourceIBMAppIDApplicationScopes(),
 			"ibm_appid_token_config":       dataSourceIBMAppIDTokenConfig(),
-			"ibm_appid_redirect_urls": dataSourceIBMAppIDRedirectURLs(),
+			"ibm_appid_redirect_urls":      dataSourceIBMAppIDRedirectURLs(),
 			"ibm_appid_role":               dataSourceIBMAppIDRole(),
 
 			"ibm_function_action":                    dataSourceIBMFunctionAction(),
@@ -436,7 +436,7 @@ func Provider() *schema.Provider {
 			"ibm_appid_application":        resourceIBMAppIDApplication(),
 			"ibm_appid_application_scopes": resourceIBMAppIDApplicationScopes(),
 			"ibm_appid_token_config":       resourceIBMAppIDTokenConfig(),
-			"ibm_appid_redirect_urls": resourceIBMAppIDRedirectURLs(),
+			"ibm_appid_redirect_urls":      resourceIBMAppIDRedirectURLs(),
 			"ibm_appid_role":               resourceIBMAppIDRole(),
 
 			"ibm_function_action":                                resourceIBMFunctionAction(),
