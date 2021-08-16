cd3f752d8da7de0af5ce9f76ea4127119319bb48
Darius Bakunas-Milanowski <dbakuna@us.ibm.com>
2021-08-16 15:41:03 -0400

    feat: add appid cloud directory idp datasource and resource

--- ibm/provider.go
+++ ibm/provider.go
@@ -173,12 +173,13 @@ func Provider() *schema.Provider {
 			"ibm_app_route":          dataSourceIBMAppRoute(),
 
 			// AppID
-			"ibm_appid_application":        dataSourceIBMAppIDApplication(),
-			"ibm_appid_application_scopes": dataSourceIBMAppIDApplicationScopes(),
-			"ibm_appid_application_roles":  dataSourceIBMAppIDApplicationRoles(),
-			"ibm_appid_token_config":       dataSourceIBMAppIDTokenConfig(),
-			"ibm_appid_redirect_urls":      dataSourceIBMAppIDRedirectURLs(),
-			"ibm_appid_role":               dataSourceIBMAppIDRole(),
+			"ibm_appid_application":         dataSourceIBMAppIDApplication(),
+			"ibm_appid_application_scopes":  dataSourceIBMAppIDApplicationScopes(),
+			"ibm_appid_application_roles":   dataSourceIBMAppIDApplicationRoles(),
+			"ibm_appid_idp_cloud_directory": dataSourceIBMAppIDIDPCloudDirectory(),
+			"ibm_appid_token_config":        dataSourceIBMAppIDTokenConfig(),
+			"ibm_appid_redirect_urls":       dataSourceIBMAppIDRedirectURLs(),
+			"ibm_appid_role":                dataSourceIBMAppIDRole(),
 
 			"ibm_function_action":                    dataSourceIBMFunctionAction(),
 			"ibm_function_package":                   dataSourceIBMFunctionPackage(),
@@ -436,12 +437,13 @@ func Provider() *schema.Provider {
 
 			// AppID
 
-			"ibm_appid_application":        resourceIBMAppIDApplication(),
-			"ibm_appid_application_scopes": resourceIBMAppIDApplicationScopes(),
-			"ibm_appid_application_roles":  resourceIBMAppIDApplicationRoles(),
-			"ibm_appid_token_config":       resourceIBMAppIDTokenConfig(),
-			"ibm_appid_redirect_urls":      resourceIBMAppIDRedirectURLs(),
-			"ibm_appid_role":               resourceIBMAppIDRole(),
+			"ibm_appid_application":         resourceIBMAppIDApplication(),
+			"ibm_appid_application_scopes":  resourceIBMAppIDApplicationScopes(),
+			"ibm_appid_application_roles":   resourceIBMAppIDApplicationRoles(),
+			"ibm_appid_idp_cloud_directory": resourceIBMAppIDIDPCloudDirectory(),
+			"ibm_appid_token_config":        resourceIBMAppIDTokenConfig(),
+			"ibm_appid_redirect_urls":       resourceIBMAppIDRedirectURLs(),
+			"ibm_appid_role":                resourceIBMAppIDRole(),
 
 			"ibm_function_action":                                resourceIBMFunctionAction(),
 			"ibm_function_package":                               resourceIBMFunctionPackage(),
