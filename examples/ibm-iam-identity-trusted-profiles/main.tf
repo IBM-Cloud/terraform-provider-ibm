c8397b86826391be2652355e13da977c1f3e2994
poorani <poorani.senthilkumar.sankari@ibm.com>
2021-09-13 16:21:40 +0100

    adding trusted profiles: profiles items

--- examples/ibm-iam-identity-trusted-profiles/main.tf
+++ examples/ibm-iam-identity-trusted-profiles/main.tf
@@ -1,7 +1,7 @@
 
 // Provision iam_trusted_profiles resource instance
 resource "ibm_iam_trusted_profiles" "iam_trusted_profiles_instance" {
-  name = "profile123456789"
+  name = "profile12345678912"
   description = "my nice profile desc"
   account_id = "29671bdd39ef4a1e9c07c16b13466908"
 }
