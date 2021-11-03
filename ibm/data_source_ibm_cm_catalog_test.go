a19be285a68954396d426dffbd72718156e0ac8a
Jon Woodlief <jon13w@gmail.com>
2021-11-03 16:40:15 -0400

    add resource group to cm_catalog resource and datasource

--- ibm/data_source_ibm_cm_catalog_test.go
+++ ibm/data_source_ibm_cm_catalog_test.go
@@ -5,23 +5,26 @@ package ibm
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
 func TestAccIBMCmCatalogDataSource(t *testing.T) {
+	ResourceGroupID := os.Getenv("CATMGMT_RESOURCE_GROUP_ID")
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			resource.TestStep{
-				Config: testAccCheckIBMCmCatalogDataSourceConfig(),
+				Config: testAccCheckIBMCmCatalogDataSourceConfig(ResourceGroupID),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttrSet("data.ibm_cm_catalog.cm_catalog_data", "label"),
 					resource.TestCheckResourceAttrSet("data.ibm_cm_catalog.cm_catalog_data", "crn"),
 					resource.TestCheckResourceAttrSet("data.ibm_cm_catalog.cm_catalog_data", "kind"),
+					resource.TestCheckResourceAttrSet("ibm_cm_catalog.cm_catalog", "resource_group_id"),
 				),
 			},
 			resource.TestStep{
@@ -30,24 +33,26 @@ func TestAccIBMCmCatalogDataSource(t *testing.T) {
 					resource.TestCheckResourceAttrSet("data.ibm_cm_catalog.cm_catalog_data", "label"),
 					resource.TestCheckResourceAttrSet("data.ibm_cm_catalog.cm_catalog_data", "crn"),
 					resource.TestCheckResourceAttrSet("data.ibm_cm_catalog.cm_catalog_data", "kind"),
+					resource.TestCheckResourceAttrSet("ibm_cm_catalog.cm_catalog", "resource_group_id"),
 				),
 			},
 		},
 	})
 }
 
-func testAccCheckIBMCmCatalogDataSourceConfig() string {
+func testAccCheckIBMCmCatalogDataSourceConfig(resourceGroupID string) string {
 	return fmt.Sprintf(`
 
 		resource "ibm_cm_catalog" "cm_catalog" {
 			label = "tf_test_datasource_catalog"
 			short_description = "testing terraform provider with catalog"
+			resource_group_id = "%s"
 		}
 		
 		data "ibm_cm_catalog" "cm_catalog_data" {
 			catalog_identifier = ibm_cm_catalog.cm_catalog.id
 		}
-		`)
+		`, resourceGroupID)
 }
 
 func testAccCheckIBMCmCatalogDataSourceConfigDefault() string {
