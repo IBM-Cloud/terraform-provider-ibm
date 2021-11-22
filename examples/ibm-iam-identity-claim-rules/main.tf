0b4cb7c8c0219166446d1b0d409720ad9cb4b3a6
poorani <poorani.senthilkumar.sankari@ibm.com>
2021-11-22 14:32:53 +0000

    trusted profile: adding list operations

--- examples/ibm-iam-identity-claim-rules/main.tf
+++ examples/ibm-iam-identity-claim-rules/main.tf
@@ -21,3 +21,8 @@ data "ibm_iam_trusted_profile_claim_rule" "iam_trusted_profile_claim_rule_instan
   profile_id = var.iam_trusted_profile_claim_rule_profile_id
   rule_id = var.iam_trusted_profile_claim_rule_rule_id
 }
+
+// Create iam_trusted_profile_claim_rules data source
+data "ibm_iam_trusted_profile_claim_rules" "iam_trusted_profile_claim_rules_instance" {
+  profile_id = "profile_id"
+}
