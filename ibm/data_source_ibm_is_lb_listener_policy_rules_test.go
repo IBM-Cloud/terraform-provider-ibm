94749719347f90fc9af018266a3d15ddb1011a1d
Sunitha Gudisagar <Sunitha.Gudisagar@ibm.com>
2022-01-31 14:59:41 +0530

    Datasource for Loadbalancer listener and its sub resources

---
+++ ibm/data_source_ibm_is_lb_listener_policy_rules_test.go
@@ -0,0 +1,55 @@
+// Copyright IBM Corp. 2021 All Rights Reserved.
+// Licensed under the Mozilla Public License v2.0
+
+package ibm
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccIBMIsLbListenerPolicyRulesDataSourceBasic(t *testing.T) {
+	vpcname := fmt.Sprintf("tflblisuat-vpc-%d", acctest.RandIntRange(10, 100))
+	subnetname := fmt.Sprintf("tflblisuat-subnet-%d", acctest.RandIntRange(10, 100))
+	lbname := fmt.Sprintf("tflblisuat%d", acctest.RandIntRange(10, 100))
+	lblistenerpolicyname := fmt.Sprintf("tflblisuat-listener-policy-%d", acctest.RandIntRange(10, 100))
+	lblistenerpolicyRuleField1 := fmt.Sprintf("tflblipolicy-rule-field-%d", acctest.RandIntRange(10, 100))
+	lblistenerpolicyRuleValue1 := fmt.Sprintf("tflblipolicy-rule-value-%d", acctest.RandIntRange(10, 100))
+
+	priority := "1"
+	protocol := "http"
+	port := "8080"
+	action := "forward"
+	condition := "equals"
+	typeh := "header"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccCheckIBMIsLbListenerPolicyRulesDataSourceConfigBasic(vpcname, subnetname, ISZoneName, ISCIDR, lbname, port, protocol, lblistenerpolicyname, action, priority, condition, typeh, lblistenerpolicyRuleField1, lblistenerpolicyRuleValue1),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.ibm_is_lb_listener_policy_rules.is_lb_listener_policy_rules", "id"),
+					resource.TestCheckResourceAttrSet("data.ibm_is_lb_listener_policy_rules.is_lb_listener_policy_rules", "lb"),
+					resource.TestCheckResourceAttrSet("data.ibm_is_lb_listener_policy_rules.is_lb_listener_policy_rules", "listener"),
+					resource.TestCheckResourceAttrSet("data.ibm_is_lb_listener_policy_rules.is_lb_listener_policy_rules", "policy"),
+					resource.TestCheckResourceAttrSet("data.ibm_is_lb_listener_policy_rules.is_lb_listener_policy_rules", "rules.#"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckIBMIsLbListenerPolicyRulesDataSourceConfigBasic(vpcname, subnetname, zone, cidr, lbname, port, protocol, lblistenerpolicyname, action, priority, condition, types, field, value string) string {
+	return testAccCheckIBMISLBListenerPolicyRuleConfig(vpcname, subnetname, ISZoneName, ISCIDR, lbname, port, protocol, lblistenerpolicyname, action, priority, condition, types, field, value) + fmt.Sprintf(`
+	data "ibm_is_lb_listener_policy_rules" "is_lb_listener_policy_rules" {
+		lb = "${ibm_is_lb.testacc_LB.id}"
+		listener = ibm_is_lb_listener.testacc_lb_listener.listener_id
+		policy = ibm_is_lb_listener_policy.testacc_lb_listener_policy.policy_id
+	}
+	`)
+}
---
+++ ibm/data_source_ibm_is_lb_listeners_test.go
@@ -0,0 +1,43 @@
+// Copyright IBM Corp. 2021 All Rights Reserved.
+// Licensed under the Mozilla Public License v2.0
+
+package ibm
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccIBMIsLbListenersDataSourceBasic(t *testing.T) {
+	vpcname := fmt.Sprintf("tflblis-vpc-%d", acctest.RandIntRange(10, 100))
+	subnetname := fmt.Sprintf("tflblis-subnet-%d", acctest.RandIntRange(10, 100))
+	lbname := fmt.Sprintf("tflblis%d", acctest.RandIntRange(10, 100))
+	protocol1 := "http"
+	port1 := "8080"
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccCheckIBMIsLbListenersDataSourceConfigBasic(vpcname, subnetname, ISZoneName, ISCIDR, lbname, port1, protocol1),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.ibm_is_lb_listeners.is_lb_listeners", "id"),
+					resource.TestCheckResourceAttrSet("data.ibm_is_lb_listeners.is_lb_listeners", "lb"),
+					resource.TestCheckResourceAttrSet("data.ibm_is_lb_listeners.is_lb_listeners", "listeners.#"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckIBMIsLbListenersDataSourceConfigBasic(vpcname, subnetname, zone, cidr, lbname, port, protocol string) string {
+	return testAccCheckIBMISLBListenerConfig(vpcname, subnetname, ISZoneName, ISCIDR, lbname, port, protocol) + fmt.Sprintf(`
+
+	data "ibm_is_lb_listeners" "is_lb_listeners" {
+		lb = "${ibm_is_lb.testacc_LB.id}"
+	}
+	`)
+}
