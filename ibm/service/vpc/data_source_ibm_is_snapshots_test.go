78608f1356116c19625bc64194278021108ea0d2
Bhavesh Shrivastav <bhavesh.shrivastav@ibm.com>
2022-02-03 18:28:09 +0530

    snapshot added exception at captured_at
    snapshot added exception at captured_at

--- ibm/service/vpc/data_source_ibm_is_snapshots_test.go
+++ ibm/service/vpc/data_source_ibm_is_snapshots_test.go
@@ -20,8 +20,11 @@ func TestAccIBMISSnapshotsDatasource_basic(t *testing.T) {
 	vpcname := fmt.Sprintf("tf-vpc-%d", acctest.RandIntRange(10, 100))
 	name := fmt.Sprintf("tf-instnace-%d", acctest.RandIntRange(10, 100))
 	subnetname := fmt.Sprintf("tf-subnet-%d", acctest.RandIntRange(10, 100))
+	// 	publicKey := strings.TrimSpace(`
+	// ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCKVmnMOlHKcZK8tpt3MP1lqOLAcqcJzhsvJcjscgVERRN7/9484SOBJ3HSKxxNG5JN8owAjy5f9yYwcUg+JaUVuytn5Pv3aeYROHGGg+5G346xaq3DAwX6Y5ykr2fvjObgncQBnuU5KHWCECO/4h8uWuwh/kfniXPVjFToc+gnkqA+3RKpAecZhFXwfalQ9mMuYGFxn+fwn8cYEApsJbsEmb0iJwPiZ5hjFC8wREuiTlhPHDgkBLOiycd20op2nXzDbHfCHInquEe/gYxEitALONxm0swBOwJZwlTDOB7C6y2dzlrtxr1L59m7pCkWI4EtTRLvleehBoj3u7jB4usR
+	// `)
 	publicKey := strings.TrimSpace(`
-ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCKVmnMOlHKcZK8tpt3MP1lqOLAcqcJzhsvJcjscgVERRN7/9484SOBJ3HSKxxNG5JN8owAjy5f9yYwcUg+JaUVuytn5Pv3aeYROHGGg+5G346xaq3DAwX6Y5ykr2fvjObgncQBnuU5KHWCECO/4h8uWuwh/kfniXPVjFToc+gnkqA+3RKpAecZhFXwfalQ9mMuYGFxn+fwn8cYEApsJbsEmb0iJwPiZ5hjFC8wREuiTlhPHDgkBLOiycd20op2nXzDbHfCHInquEe/gYxEitALONxm0swBOwJZwlTDOB7C6y2dzlrtxr1L59m7pCkWI4EtTRLvleehBoj3u7jB4usR
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDCZNT0p7DKLKoXshjO0xHuPVpxAHRYhTkVI+yklxPoF/1UuIy+8cczEUnEoIZ81hHVnyY8Ltr75Xr2/yI1g+a6t+xwgdp74f5+R69AuIuywsoLdt8mda2iFryrPs5eebhSBo9c2dGya/uv1pR8o/ED9xrIB4QQ+vrT8uuss3GIP6lR5w9vJqWgeB4I4TqGu4giWLkeMyBKVVUn/UoAqoa7uD7deFXkmN73j227AyCV+lx8MBFQZKvnsglJxgSnjwlQP2iWCyLGvDV1j0lldXAvjkxYRl8H9ilrOk5KrCpqVtlsXcXqqBTeCEBpI0KOHIAl/JJ+YFc3+YTQq0waGB82CaqQCnJEy6bBpdPz4tge16/2BehY0fY5AG8sqb7Or91ilsE0nnuR3Tzk5l9YS8ftqyjYAoRbBy3P7AF433qbCgoclndp5Mcy5EsEJ//kme0u4n8ZF20ZS8oqKK1QIfOio8U2er/SSF3UAHhT2n/hKaeGeXv0kFwCsExjP1iZsHk= bhaveshshrivastav@Bhaveshs-MacBook-Pro.local
 `)
 	sshname := fmt.Sprintf("tf-ssh-%d", acctest.RandIntRange(10, 100))
 	volname := fmt.Sprintf("tf-vol-%d", acctest.RandIntRange(10, 100))
@@ -44,10 +47,12 @@ ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCKVmnMOlHKcZK8tpt3MP1lqOLAcqcJzhsvJcjscgVE
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(
 						snpName, "snapshots.0.name", name1),
+					// resource.TestCheckResourceAttrSet(snpName, "snapshots.0.delatable"), // Commented as it is deprecated.
 					resource.TestCheckResourceAttrSet(snpName, "snapshots.0.href"),
 					resource.TestCheckResourceAttrSet(snpName, "snapshots.0.crn"),
 					resource.TestCheckResourceAttrSet(snpName, "snapshots.0.lifecycle_state"),
 					resource.TestCheckResourceAttrSet(snpName, "snapshots.0.encryption"),
+					resource.TestCheckResourceAttrSet(snpName, "snapshots.0.captured_at"),
 				),
 			},
 		},
