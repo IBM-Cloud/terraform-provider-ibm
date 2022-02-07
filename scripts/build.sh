0ada74bbdf83f5d9def656a4bf3cdf15a8ac839f
hkavya26 <hkavya26@in.ibm.com>
2022-02-07 17:06:35 +0530

    Support arm binary

--- scripts/build.sh
+++ scripts/build.sh
@@ -18,9 +18,9 @@ GIT_COMMIT=$(git rev-parse HEAD)
 GIT_DIRTY=$(test -n "`git status --porcelain`" && echo "+CHANGES" || true)
 
 # Determine the arch/os combos we're building for
-XC_ARCH=${XC_ARCH:-"amd64"}
+XC_ARCH=${XC_ARCH:-"amd64" "arm64" "arm"}
 XC_OS=${XC_OS:-linux darwin windows}
-XC_EXCLUDE_OSARCH="!darwin/arm !darwin/386"
+XC_EXCLUDE_OSARCH="!darwin/386 !windows/arm64 !windows/arm !darwin/arm"
 
 # Delete the old dir
 echo "==> Removing old directory..."
