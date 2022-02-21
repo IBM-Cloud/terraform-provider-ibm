2d610bae083e39d35b3b10e856cab5ea9270477f
Rajeev Kumar Ranjan <rajeev10.1985@gmail.com>
2022-02-21 12:35:54 +0530

    Updated variables for bucket

--- examples/ibm-resource-instance/variables.tf
+++ examples/ibm-resource-instance/variables.tf
@@ -15,7 +15,23 @@ variable "location" {
 }
 
 variable "resource_group" {
-  default = "default"
+  default = "Default"
+}
+
+variable "bucket_name" {
+  default = "a-standard-bucket-at-ams-firewall"
+}
+
+variable "storage" {
+  default = "standard"
+}
+
+variable "region" {
+  default = "us"
+}
+
+variable "regional_loc" {
+  default = "us-south"
 }
 
 
