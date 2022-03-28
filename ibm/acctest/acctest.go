70a3ab4da8063de8e46640605e4d8221f830b0e5
Timothy-Yao <timothy.yao@ibm.com>
2022-03-28 13:30:30 -0500

    adding a warning if some of the env aren't set.

--- ibm/acctest/acctest.go
+++ ibm/acctest/acctest.go
@@ -141,6 +141,7 @@ var Account_to_be_imported string
 var Scc_si_account string
 
 //Security and Compliance Center, Posture Management
+var Scc_gov_account_id string
 var Scc_posture_scope_id string
 var Scc_posture_scan_id string
 var Scc_posture_profile_id string
@@ -767,6 +768,17 @@ func init() {
 	if HpcsToken2 == "" {
 		fmt.Println("[WARN] Set the environment variable IBM_HPCS_TOKEN2 with a VALID token for HPCS Admin Key2")
 	}
+
+	Scc_gov_account_id  = os.Getenv("SCC_GOVERNANCE_ACCOUNT_ID")
+	if Scc_gov_account_id == "" {
+		fmt.Println("[WARN] Set the environment variable SCC_GOVERNANCE_ACCOUNT_ID with a VALID account name")
+	}
+
+	Scc_resource_group_id = os.Getenv("IBM_SCC_RESOURCE_GROUP")
+	if Scc_resource_group_id == "" {
+		fmt.Println("[WARN] Set the environment variable IBM_SCC_RESOURCE_GROUP with a VALID resource group id")
+	}
+
 	Scc_si_account = os.Getenv("SCC_SI_ACCOUNT")
 	if Scc_si_account == "" {
 		fmt.Println("[INFO] Set the environment variable SCC_SI_ACCOUNT for testing SCC SI resources resource else  tests will fail if this is not set correctly")
