548a93ec88d4fcfd0d3592d3d236ad6dbbc19e93
Sugata Mazumdar <sugata.mazumdar@gmail.com>
2022-04-04 20:31:05 +0530

    SCC-PostureManagement Create scope issue
    Create scope issue

--- ibm/service/scc/resource_ibm_scc_posture_scope.go
+++ ibm/service/scc/resource_ibm_scc_posture_scope.go
@@ -114,7 +114,12 @@ func resourceIBMSccPostureScopesCreate(context context.Context, d *schema.Resour
 
 	createScopeOptions.SetName(d.Get("name").(string))
 	createScopeOptions.SetDescription(d.Get("description").(string))
-	createScopeOptions.SetCollectorIds([]string{"4188"}) //[]string{
+	collector_ids_int := d.Get("collector_ids").([]interface{})
+        collector_ids := make([]string, len(collector_ids_int))
+        for i, collector_id := range collector_ids_int {
+                collector_ids[i] = collector_id.(string)
+        }
+        createScopeOptions.SetCollectorIds(collector_ids) //[]string{
 	createScopeOptions.SetCredentialID(d.Get("credential_id").(string))
 	createScopeOptions.SetCredentialType(d.Get("credential_type").(string))
 
