b7e7996aefd7e5036ed3f3b61f4d8e4a1902e880
Ujjwal Kumar <Ujjwal.Kumar1@ibm.com>
2022-04-13 11:27:53 +0530

    enhancement(changed): changed list logic in datasource by name

--- ibm/service/vpc/data_source_ibm_is_flow_log.go
+++ ibm/service/vpc/data_source_ibm_is_flow_log.go
@@ -207,32 +207,24 @@ func dataSourceIBMIsFlowLogRead(context context.Context, d *schema.ResourceData,
 	var flowLogCollector *vpcv1.FlowLogCollector
 
 	if name != "" {
-		start := ""
-		allrecs := []vpcv1.FlowLogCollector{}
-		for {
-			listOptions := &vpcv1.ListFlowLogCollectorsOptions{}
-			if start != "" {
-				listOptions.Start = &start
-			}
-			flowlogCollectors, response, err := sess.ListFlowLogCollectors(listOptions)
-			if err != nil {
-				return diag.FromErr(fmt.Errorf("[ERROR] Error Fetching Flow Logs for VPC %s\n%s", err, response))
-			}
-			start = flex.GetNext(flowlogCollectors.Next)
-			allrecs = append(allrecs, flowlogCollectors.FlowLogCollectors...)
-			if start == "" {
-				break
-			}
+
+		listOptions := &vpcv1.ListFlowLogCollectorsOptions{
+			Name: &name,
 		}
-		for _, flowlogCollector := range allrecs {
-			if *flowlogCollector.Name == name {
-				flowLogCollector = &flowlogCollector
-				break
-			}
+
+		flowlogCollectors, response, err := sess.ListFlowLogCollectors(listOptions)
+		if err != nil {
+			return diag.FromErr(fmt.Errorf("Error Fetching Flow Logs for VPC %s\n%s", err, response))
 		}
-		if flowLogCollector == nil {
+
+		allrecs := flowlogCollectors.FlowLogCollectors
+
+		if len(allrecs) == 0 {
 			return diag.FromErr(fmt.Errorf("[ERROR] No flow log collector found with name (%s)", name))
 		}
+		flc := allrecs[0]
+		flowLogCollector = &flc
+
 	} else if identifier != "" {
 		getFlowLogCollectorOptions := &vpcv1.GetFlowLogCollectorOptions{}
 
