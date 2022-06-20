8eb232188a77a69fc377a0e0eb41a245099a4288
PIYUSH MUNDRA <PIYUSH.Mundra@ibm.com>
2022-06-20 18:45:55 +0530

    feat: IBM Cloud CD terraform resources examples - empty toolchain template

---
+++ examples/ibm-cd-toolchain-empty/versions.tf
@@ -0,0 +1,11 @@
+###############################
+# IBM Cloud Copyright 2020 IBM
+###############################
+
+terraform {
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+    }
+  }
+}
