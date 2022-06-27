874848b26b6212f65ffa3831e29de1ac5d8c57f5
arpit-srivastava-ibm <105713121+arpit-srivastava-ibm@users.noreply.github.com>
2022-06-27 17:43:45 +0530

    CIS MTLS terraform support - Data Source files - 2nd Review (#3855)
    * cis mtls data files
    
    * added go files
    
    * corrected hook in provider
    
    * corrected test files
    
    * added data source doc
    
    * removed resource file
    
    * added context
    
    * addressed review comments
    
    * corrected const name
    
    Co-authored-by: Arpit Srivastava <arpit-mac@Arpits-MacBook-Pro.local>

--- ibm/provider/provider.go
+++ ibm/provider/provider.go
@@ -273,6 +273,8 @@ func Provider() *schema.Provider {
 			"ibm_cis_ip_addresses":                  cis.DataSourceIBMCISIP(),
 			"ibm_cis_waf_groups":                    cis.DataSourceIBMCISWAFGroups(),
 			"ibm_cis_alerts":                        cis.DataSourceIBMCISAlert(),
+			"ibm_cis_mtlss":                         cis.DataSourceIBMCISMtls(),
+			"ibm_cis_mtls_apps":                     cis.DataSourceIBMCISMtlsApp(),
 			"ibm_cis_webhooks":                      cis.DataSourceIBMCISWebhooks(),
 			"ibm_cis_logpush_jobs":                  cis.DataSourceIBMCISLogPushJobs(),
 			"ibm_cis_edge_functions_actions":        cis.DataSourceIBMCISEdgeFunctionsActions(),
