56b2cabe8a8a6326f386d0dc11c9cba7fcc6a9ca
Zoltan Illes <zoltan.illes@ibm.com>
2022-06-27 16:33:12 +0200

    add vpc cluster and workerpool to the dhost example

--- examples/ibm-dedicated-host/main.tf
+++ examples/ibm-dedicated-host/main.tf
@@ -4,13 +4,49 @@ data "ibm_container_dedicated_host_flavor" "dhostflavor" {
 }
 
 resource "ibm_container_dedicated_host_pool" "dhostpool" {
-  name         = var.dhostpoolname
-  flavor_class = ibm_container_dedicated_host_flavor.dhostflavor.flavor_class
-  metro        = var.metro
+  name              = var.dhostpoolname
+  flavor_class      = data.ibm_container_dedicated_host_flavor.dhostflavor.flavor_class
+  metro             = var.metro
+  resource_group_id = var.resource_group_id
 }
 
 resource "ibm_container_dedicated_host" "dhost" {
-  flavor       = ibm_container_dedicated_host_flavor.dhostflavor.host_flavor_id
-  host_pool_id = ibm_container_dedicated_host_pool.dhostpool.host_id
+  flavor       = data.ibm_container_dedicated_host_flavor.dhostflavor.host_flavor_id
+  host_pool_id = ibm_container_dedicated_host_pool.dhostpool.id
   zone         = var.zone
 }
+
+resource "ibm_container_vpc_cluster" "dhost_vpc_cluster" {
+  name   = var.cluster_name
+  vpc_id = var.vpc_id
+  flavor = var.flavor
+  zones {
+    name      = var.zone
+    subnet_id = var.subnet_id
+  }
+  worker_count      = var.worker_count
+  resource_group_id = var.resource_group_id
+  wait_till         = "OneWorkerNodeReady"
+  host_pool_id      = ibm_container_dedicated_host_pool.dhostpool.id
+
+  depends_on        = [
+    ibm_container_dedicated_host.dhost
+    ]
+}
+
+resource "ibm_container_vpc_worker_pool" "dhost_vpc_worker_pool" {
+  cluster          = ibm_container_vpc_cluster.dhost_vpc_cluster.name
+  worker_pool_name = var.worker_pool_name
+  flavor           = ibm_container_vpc_cluster.dhost_vpc_cluster.flavor
+  vpc_id           = ibm_container_vpc_cluster.dhost_vpc_cluster.vpc_id
+  host_pool_id     = ibm_container_vpc_cluster.dhost_vpc_cluster.host_pool_id
+  worker_count     = var.worker_count
+  zones {
+    name      = var.zone
+    subnet_id = var.subnet_id
+  }
+  depends_on = [
+    ibm_container_dedicated_host.dhost
+  ]
+}
+
--- examples/ibm-dedicated-host/variables.tf
+++ examples/ibm-dedicated-host/variables.tf
@@ -2,12 +2,16 @@ variable "ibmcloud_api_key" {
   default = ""
 }
 
+variable "resource_group_id" {
+  default = ""
+}
+
 variable "metro" {
   default = "dal"
 }
 
 variable "zone" {
-  default = "us-south-1"
+  default = "us-south-2"
 }
 
 variable "dhostflavorid" {
@@ -16,4 +20,28 @@ variable "dhostflavorid" {
 
 variable "dhostpoolname" {
   default = "tf-dhostpool-1"
+}
+
+variable "cluster_name" {
+  default = "tf-dhost-vpc-cluster"
+}
+
+variable "worker_pool_name" {
+  default = "tf-dhost-vpc-worker-pool"
+}
+
+variable "vpc_id" {
+  default = "1234-a1b2c3d4-abcd-1234-abcd-123456789012"
+}
+
+variable "subnet_id" {
+  default = "1234-a1b2c3d4-abcd-1234-abcd-123456789012"
+}
+
+variable "flavor" {
+  default = "bx2d.4x16"
+}
+
+variable "worker_count" {
+  default = "1"
 }
