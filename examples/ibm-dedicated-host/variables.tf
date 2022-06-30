4e301be4c57fbd5d96d51146ba7097b166fc5d89
Zoltán Illés <zoltan.illes@live.com>
2022-06-30 09:39:37 +0200

    add vpc cluster and workerpool to the dhost example (#3878)
    * add vpc cluster and workerpool to the dhost example
    
    * use subnet,vpc datasource instead ids
    
    Co-authored-by: Zoltan Illes <zoltan.illes@ibm.com>

--- examples/ibm-dedicated-host/variables.tf
+++ examples/ibm-dedicated-host/variables.tf
@@ -2,12 +2,16 @@ variable "ibmcloud_api_key" {
   default = ""
 }
 
+variable "resource_group_id" {
+  default = ""
+}
+
 variable "metro" {
   default = "dal"
 }
 
 variable "zone" {
-  default = "us-south-1"
+  default = "us-south-2"
 }
 
 variable "dhostflavorid" {
@@ -16,4 +20,28 @@ variable "dhostflavorid" {
 
 variable "dhostpoolname" {
   default = "tf-dhostpool-1"
+}
+
+variable "cluster_name" {
+  default = "tf-dhost-vpc-cluster"
+}
+
+variable "worker_pool_name" {
+  default = "tf-dhost-vpc-worker-pool"
+}
+
+variable "vpc_name" {
+  default = "tf-vpc"
+}
+
+variable "subnet_name" {
+  default = "tf-subnet"
+}
+
+variable "flavor" {
+  default = "bx2d.4x16"
+}
+
+variable "worker_count" {
+  default = "1"
 }
