55c6cced212ac4da71632f478bd07b1bf409b88e
Brian Gleeson <brian.gleeson@ie.ibm.com>
2022-08-10 13:32:00 +0100

    chore(review): revert provider.go changes

--- ibm/provider/provider.go
+++ ibm/provider/provider.go
@@ -627,15 +627,10 @@ func Provider() *schema.Provider {
 			"ibm_atracker_endpoints": atracker.DataSourceIBMAtrackerEndpoints(),
 
 			//Security and Compliance Center
-			"ibm_scc_si_providers":                  scc.DataSourceIBMSccSiProviders(),
-			"ibm_scc_si_note":                       scc.DataSourceIBMSccSiNote(),
-			"ibm_scc_si_notes":                      scc.DataSourceIBMSccSiNotes(),
 			"ibm_scc_account_location":              scc.DataSourceIBMSccAccountLocation(),
 			"ibm_scc_account_locations":             scc.DataSourceIBMSccAccountLocations(),
 			"ibm_scc_account_location_settings":     scc.DataSourceIBMSccAccountLocationSettings(),
 			"ibm_scc_account_notification_settings": scc.DataSourceIBMSccNotificationSettings(),
-			"ibm_scc_si_occurrence":                 scc.DataSourceIBMSccSiOccurrence(),
-			"ibm_scc_si_occurrences":                scc.DataSourceIBMSccSiOccurrences(),
 
 			// Compliance Posture Management
 			"ibm_scc_posture_scopes":            scc.DataSourceIBMSccPostureScopes(),
@@ -1052,9 +1047,7 @@ func Provider() *schema.Provider {
 			"ibm_atracker_settings": atracker.ResourceIBMAtrackerSettings(),
 
 			// //Security and Compliance Center
-			"ibm_scc_si_note":             scc.ResourceIBMSccSiNote(),
 			"ibm_scc_account_settings":    scc.ResourceIBMSccAccountSettings(),
-			"ibm_scc_si_occurrence":       scc.ResourceIBMSccSiOccurrence(),
 			"ibm_scc_rule":                scc.ResourceIBMSccRule(),
 			"ibm_scc_rule_attachment":     scc.ResourceIBMSccRuleAttachment(),
 			"ibm_scc_template":            scc.ResourceIBMSccTemplate(),
@@ -1145,6 +1138,7 @@ func Validator() validate.ValidatorDict {
 				"ibm_cis_rate_limit":              cis.ResourceIBMCISRateLimitValidator(),
 				"ibm_cis":                         cis.ResourceIBMCISValidator(),
 				"ibm_cis_domain_settings":         cis.ResourceIBMCISDomainSettingValidator(),
+				"ibm_cis_domain":                  cis.ResourceIBMCISDomainValidator(),
 				"ibm_cis_tls_settings":            cis.ResourceIBMCISTLSSettingsValidator(),
 				"ibm_cis_routing":                 cis.ResourceIBMCISRoutingValidator(),
 				"ibm_cis_page_rule":               cis.ResourceIBMCISPageRuleValidator(),
@@ -1269,9 +1263,7 @@ func Validator() validate.ValidatorDict {
 				"ibm_atracker_route":                      atracker.ResourceIBMAtrackerRouteValidator(),
 				"ibm_atracker_settings":                   atracker.ResourceIBMAtrackerSettingsValidator(),
 				"ibm_satellite_endpoint":                  satellite.ResourceIBMSatelliteEndpointValidator(),
-				"ibm_scc_si_note":                         scc.ResourceIBMSccSiNoteValidator(),
 				"ibm_scc_account_settings":                scc.ResourceIBMSccAccountSettingsValidator(),
-				"ibm_scc_si_occurrence":                   scc.ResourceIBMSccSiOccurrenceValidator(),
 				"ibm_scc_posture_collector":               scc.ResourceIBMSccPostureCollectorsValidator(),
 				"ibm_scc_posture_scope":                   scc.ResourceIBMSccPostureScopesValidator(),
 				"ibm_scc_posture_credential":              scc.ResourceIBMSccPostureCredentialsValidator(),
