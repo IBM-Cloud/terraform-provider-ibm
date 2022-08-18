f8d0ad38eb6a1aa650bd3ae69b99536c8b3620d4
Omar Ibrahim <97538078+omaraibrahim@users.noreply.github.com>
2022-08-18 02:17:37 -0400

    Deprecate key_protect_key_id (#3939)
    * Modified platform options and deprecated key_protect_key_id
    
    * deprecated key_protect_key_id
    
    * added deprecation to docs
    
    * removed comment
    
    * removed another comment

--- ibm/flex/structures.go
+++ ibm/flex/structures.go
@@ -1664,6 +1664,16 @@ func FlattenWhitelist(whitelist icdv4.Whitelist) []map[string]interface{} {
 	return entries
 }
 
+func ExpandPlatformOptions(platformOptions icdv4.PlatformOptions) []map[string]interface{} {
+	pltOptions := make([]map[string]interface{}, 0, 1)
+	pltOption := make(map[string]interface{})
+	pltOption["key_protect_key_id"] = platformOptions.KeyProtectKey
+	pltOption["disk_encryption_key_crn"] = platformOptions.DiskENcryptionKeyCrn
+	pltOption["backup_encryption_key_crn"] = platformOptions.BackUpEncryptionKeyCrn
+	pltOptions = append(pltOptions, pltOption)
+	return pltOptions
+}
+
 func expandStringMap(inVal interface{}) map[string]string {
 	outVal := make(map[string]string)
 	if inVal == nil {
