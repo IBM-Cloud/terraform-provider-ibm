43aad33f0a7e319c000b6f5028b8445340743900
Arpit Srivastava <arpit-mac@Arpits-MacBook-Pro.local>
2022-09-02 11:11:43 +0530

    revert conflict changes

--- ibm/service/cis/resource_ibm_cis_firewall_rules.go
+++ ibm/service/cis/resource_ibm_cis_firewall_rules.go
@@ -39,6 +39,8 @@ func ResourceIBMCISFirewallrules() *schema.Resource {
 				Type:        schema.TypeString,
 				Description: "CIS instance crn",
 				Required:    true,
+				ValidateFunc: validate.InvokeValidator("ibm_cis_firewall_rules",
+					"cis_id"),
 			},
 			cisDomainID: {
 				Type:             schema.TypeString,
@@ -207,10 +209,6 @@ func ResourceIBMCISFirewallrulesUpdate(context context.Context, d *schema.Resour
 			filterUpdate, _ := cisClient.NewFirewallRulesUpdateInputItemFilter(filterid)
 			updatefirewallrules.Filter = filterUpdate
 		}
-		if priority, ok := d.GetOk(cisFirewallrulesPriority); ok {
-			rulePriority := int64(priority.(int))
-			updatefirewallrules.Priority = &rulePriority
-		}
 		opt := cisClient.NewUpdateFirewllRulesOptions(xAuthtoken, crn, zoneID)
 
 		opt.SetFirewallRulesUpdateInputItem([]firewallrulesv1.FirewallRulesUpdateInputItem{updatefirewallrules})
@@ -267,7 +265,14 @@ func ResourceIBMCISFirewallrulesDelete(context context.Context, d *schema.Resour
 }
 func ResourceIBMCISFirewallrulesValidator() *validate.ResourceValidator {
 	validateSchema := make([]validate.ValidateSchema, 0)
-
+	validateSchema = append(validateSchema,
+		validate.ValidateSchema{
+			Identifier:                 "cis_id",
+			ValidateFunctionIdentifier: validate.ValidateCloudData,
+			Type:                       validate.TypeString,
+			CloudDataType:              "ResourceInstance",
+			CloudDataRange:             []string{"service:internet-svcs"},
+			Required:                   true})
 	validateSchema = append(validateSchema,
 		validate.ValidateSchema{
 			Identifier:                 cisFirewallrulesAction,
