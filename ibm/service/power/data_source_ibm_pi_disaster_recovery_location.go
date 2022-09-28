859250f437ad64c77dd1786c7733e0b4c8a9bb44
dhirendersingh19 <dhirender.singh@ibm.com>
2022-09-28 11:02:43 +0530

    Add disaster recovery locations data-source

---
+++ ibm/service/power/data_source_ibm_pi_disaster_recovery_location.go
@@ -0,0 +1,93 @@
+// Copyright IBM Corp. 2017, 2021 All Rights Reserved.
+// Licensed under the Mozilla Public License v2.0
+
+package power
+
+import (
+	//"fmt"
+
+	"context"
+	"log"
+
+	"github.com/hashicorp/go-uuid"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+
+	"github.com/IBM-Cloud/power-go-client/clients/instance"
+	"github.com/IBM-Cloud/power-go-client/helpers"
+	"github.com/IBM-Cloud/power-go-client/power/models"
+	"github.com/IBM-Cloud/terraform-provider-ibm/ibm/conns"
+)
+
+func DataSourceIBMPIDisasterRecoveryLocation() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceIBMPIDisasterRecoveryLocation,
+		Schema: map[string]*schema.Schema{
+			helpers.PICloudInstanceId: {
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: validation.NoZeroValues,
+			},
+
+			// Computed Attributes
+			"location": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "RegionZone of a site",
+			},
+			"replication_sites": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"is_active": {
+							Type:     schema.TypeBool,
+							Computed: true,
+						},
+						"location": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func dataSourceIBMPIDisasterRecoveryLocation(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	sess, err := meta.(conns.ClientSession).IBMPISession()
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	cloudInstanceID := d.Get(helpers.PICloudInstanceId).(string)
+	drClient := instance.NewIBMPIDisasterRecoveryLocationClient(ctx, sess, cloudInstanceID)
+	drLocationSite, err := drClient.Get()
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	var clientgenU, _ = uuid.GenerateUUID()
+	d.SetId(clientgenU)
+	d.Set("location", drLocationSite.Location)
+	d.Set("replication_sites", flattenDisasterRecoveryLocation(drLocationSite.ReplicationSites))
+
+	return nil
+}
+
+func flattenDisasterRecoveryLocation(list []*models.Site) []map[string]interface{} {
+	log.Printf("Calling the flattenDisasterRecoveryLocations call with list %d", len(list))
+	result := make([]map[string]interface{}, 0, len(list))
+	for _, i := range list {
+		l := map[string]interface{}{
+			"is_active": i.IsActive,
+			"location":  i.Location,
+		}
+
+		result = append(result, l)
+	}
+
+	return result
+}
---
+++ ibm/service/power/data_source_ibm_pi_disaster_recovery_locations.go
@@ -0,0 +1,92 @@
+// Copyright IBM Corp. 2017, 2021 All Rights Reserved.
+// Licensed under the Mozilla Public License v2.0
+
+package power
+
+import (
+	//"fmt"
+
+	"context"
+	"log"
+
+	"github.com/hashicorp/go-uuid"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"github.com/IBM-Cloud/power-go-client/clients/instance"
+	"github.com/IBM-Cloud/power-go-client/power/models"
+	"github.com/IBM-Cloud/terraform-provider-ibm/ibm/conns"
+)
+
+func DataSourceIBMPIDisasterRecoveryLocations() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceIBMPIDisasterRecoveryLocations,
+		Schema: map[string]*schema.Schema{
+
+			// Computed Attributes
+			"disaster_recovery_locations": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"location": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "RegionZone of a site",
+						},
+						"replication_sites": {
+							Type:     schema.TypeList,
+							Computed: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"is_active": {
+										Type:     schema.TypeBool,
+										Computed: true,
+									},
+									"location": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func dataSourceIBMPIDisasterRecoveryLocations(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	sess, err := meta.(conns.ClientSession).IBMPISession()
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	drClient := instance.NewIBMPIDisasterRecoveryLocationClient(ctx, sess, "")
+	drLocationSites, err := drClient.GetAll()
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	var clientgenU, _ = uuid.GenerateUUID()
+	d.SetId(clientgenU)
+	d.Set("disaster_recovery_locations", flattenDisasterRecoveryLocations(drLocationSites.DisasterRecoveryLocations))
+
+	return nil
+}
+
+func flattenDisasterRecoveryLocations(list []*models.DisasterRecoveryLocation) []map[string]interface{} {
+	log.Printf("Calling the flattenDisasterRecoveryLocations call with list %d", len(list))
+	result := make([]map[string]interface{}, 0, len(list))
+	for _, i := range list {
+		l := map[string]interface{}{
+			"location":          i.Location,
+			"replication_sites": flattenDisasterRecoveryLocation(i.ReplicationSites),
+		}
+
+		result = append(result, l)
+	}
+
+	return result
+}
