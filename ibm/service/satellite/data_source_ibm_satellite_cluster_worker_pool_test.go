2060f1016c1dfeee65a8c6d74b8cba53acb2ff44
bhpratt <bhpratt@ncsu.edu>
2022-09-28 13:03:29 -0400

    update input for data source test

--- ibm/service/satellite/data_source_ibm_satellite_cluster_worker_pool_test.go
+++ ibm/service/satellite/data_source_ibm_satellite_cluster_worker_pool_test.go
@@ -24,7 +24,7 @@ func TestAccIBMSatelliteClusterWorkerPoolDataSourceBasic(t *testing.T) {
 	region := "us-east"
 	resource_prefix := "tf-satellite"
 	host_provider := "ibm"
-	operating_system := "RHEL_7_64"
+	operating_system := "REDHAT_7_64"
 	publicKey := strings.TrimSpace(`
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCKVmnMOlHKcZK8tpt3MP1lqOLAcqcJzhsvJcjscgVERRN7/9484SOBJ3HSKxxNG5JN8owAjy5f9yYwcUg+JaUVuytn5Pv3aeYROHGGg+5G346xaq3DAwX6Y5ykr2fvjObgncQBnuU5KHWCECO/4h8uWuwh/kfniXPVjFToc+gnkqA+3RKpAecZhFXwfalQ9mMuYGFxn+fwn8cYEApsJbsEmb0iJwPiZ5hjFC8wREuiTlhPHDgkBLOiycd20op2nXzDbHfCHInquEe/gYxEitALONxm0swBOwJZwlTDOB7C6y2dzlrtxr1L59m7pCkWI4EtTRLvleehBoj3u7jB4usR
 `)
