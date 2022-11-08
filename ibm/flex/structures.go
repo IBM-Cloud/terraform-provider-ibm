f06f46cb166e057d5b56756617a916b57a043991
hkantare <hkantare@in.ibm.com>
2022-11-08 14:58:38 +0530

    Fix the len of the array to fix len out of range

--- ibm/flex/structures.go
+++ ibm/flex/structures.go
@@ -3274,7 +3274,7 @@ func FlattenSatelliteHosts(hostList []kubernetesserviceapiv1.MultishiftQueueNode
 }
 
 func FlattenWorkerPoolHostLabels(hostLabels map[string]string) *schema.Set {
-	mapped := make([]string, len(hostLabels)-1)
+	mapped := make([]string, len(hostLabels))
 	idx := 0
 	for k, v := range hostLabels {
 		if strings.HasPrefix(k, "os") {
