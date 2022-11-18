adafa0e5e1aec603e417457695023a18ca59efe1
Omar Ibrahim <97538078+omaraibrahim@users.noreply.github.com>
2022-11-18 14:36:15 -0500

    Wait for scaling task to complete (#4188)

--- ibm/service/database/resource_ibm_database.go
+++ ibm/service/database/resource_ibm_database.go
@@ -2101,7 +2101,7 @@ func resourceIBMDatabaseInstanceUpdate(context context.Context, d *schema.Resour
 				}
 
 				// API may return HTTP 204 No Content if no change made
-				if response.StatusCode == 200 {
+				if response.StatusCode == 202 {
 					taskIDLink := *setDeploymentScalingGroupResponse.Task.ID
 
 					_, err = waitForDatabaseTaskComplete(taskIDLink, d, meta, d.Timeout(schema.TimeoutCreate))
