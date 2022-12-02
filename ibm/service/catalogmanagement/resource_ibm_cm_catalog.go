729bbf83bbb4725dac2ff3ff39786a4ff25abe9d
Ben Buchanan <buchanan@ibm.com>
2022-12-02 09:45:35 -0500

    fix: make resource_group_id optional for catalogs again

--- ibm/service/catalogmanagement/resource_ibm_cm_catalog.go
+++ ibm/service/catalogmanagement/resource_ibm_cm_catalog.go
@@ -98,6 +98,8 @@ func ResourceIBMCmCatalog() *schema.Resource {
 			"resource_group_id": &schema.Schema{
 				Type:        schema.TypeString,
 				Computed:    true,
+				Optional:    true,
+				ForceNew:    true,
 				Description: "Resource group id the catalog is owned by.",
 			},
 			"owning_account": &schema.Schema{
