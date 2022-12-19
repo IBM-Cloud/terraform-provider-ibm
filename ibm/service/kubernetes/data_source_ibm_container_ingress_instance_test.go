72d2a9f6286fc8bedd2122eddbe960b12518b621
Lucas Copi <copi.lucas3@gmail.com>
2022-12-19 11:44:21 -0500

    add tests

---
+++ ibm/service/kubernetes/data_source_ibm_container_ingress_instance_test.go
@@ -0,0 +1,4 @@
+// Copyright IBM Corp. 2022 All Rights Reserved.
+// Licensed under the Mozilla Public License v2.0
+
+package kubernetes
---
+++ ibm/service/kubernetes/resource_ibm_container_ingress_instance_test.go
@@ -0,0 +1,94 @@
+// Copyright IBM Corp. 2022 All Rights Reserved.
+// Licensed under the Mozilla Public License v2.0
+
+package kubernetes_test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	acc "github.com/IBM-Cloud/terraform-provider-ibm/ibm/acctest"
+	"github.com/IBM-Cloud/terraform-provider-ibm/ibm/conns"
+	"github.com/IBM-Cloud/terraform-provider-ibm/ibm/flex"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
+
+func TestAccIBMContainerIngressInstance_Basic(t *testing.T) {
+	clusterName := fmt.Sprintf("tf-container-ingress-instance-%d", acctest.RandIntRange(10, 100))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { acc.TestAccPreCheck(t) },
+		Providers:    acc.TestAccProviders,
+		CheckDestroy: testAccCheckIBMContainerIngressInstanceDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckIBMContainerIngressInstanceBasic(clusterName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"ibm_container_ingress_instance.instance", "secret_group_id", acc.SecretGroupID),
+					resource.TestCheckResourceAttr(
+						"ibm_container_ingress_instance.instance", "instance_crn", acc.InstanceCRN),
+					resource.TestCheckResourceAttr(
+						"ibm_container_ingress_instance.instance", "status", "created"),
+				),
+			},
+			{
+				ResourceName:      "ibm_container_ingress_instance.instance",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccCheckIBMContainerIngressInstanceDestroy(s *terraform.State) error {
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "ibm_container_ingress_instance" {
+			continue
+		}
+
+		parts, err := flex.IdParts(rs.Primary.ID)
+		if err != nil {
+			return err
+		}
+		clusterID := parts[0]
+		instanceName := parts[1]
+
+		ingressClient, err := acc.TestAccProvider.Meta().(conns.ClientSession).VpcContainerAPI()
+		if err != nil {
+			return err
+		}
+
+		ingressAPI := ingressClient.Ingresses()
+		resp, err := ingressAPI.GetIngressInstance(clusterID, instanceName)
+		if err == nil && &resp != nil && resp.Status == "deleted" {
+			return nil
+		} else if err == nil || !strings.Contains(err.Error(), "404") {
+			return fmt.Errorf("[ERROR] Error checking if instance (%s) has been destroyed: %s", rs.Primary.ID, err)
+		}
+	}
+	return nil
+}
+
+func testAccCheckIBMContainerIngressInstanceBasic(clusterName string) string {
+	return fmt.Sprintf(`
+resource "ibm_container_cluster" "testacc_cluster" {
+  name              = "%s"
+  datacenter        = "%s"
+  default_pool_size = 1
+  machine_type      = "%s"
+  hardware          = "shared"
+  public_vlan_id    = "%s"
+  private_vlan_id   = "%s"
+}
+
+resource "ibm_container_ingress_instance" "instance" {
+  instance_crn    = "%s"
+  secret_group_id = "%s"
+  is_default = "%t"
+  cluster_id  = ibm_container_cluster.testacc_cluster.id
+}`, clusterName, acc.Datacenter, acc.MachineType, acc.PublicVlanID, acc.PrivateVlanID, acc.InstanceCRN, acc.SecretGroupID, true)
+}
