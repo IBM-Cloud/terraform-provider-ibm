7f69ed897035634246bbfb83967468351da824b1
Deepak Selvakumar <Deepak.Selvakumar@ibm.com>
2023-01-09 08:48:45 +0530

    initial eni

--- ibm/service/vpc/resource_ibm_is_security_group_target.go
+++ ibm/service/vpc/resource_ibm_is_security_group_target.go
@@ -124,6 +124,12 @@ func resourceIBMISSecurityGroupTargetCreate(d *schema.ResourceData, meta interfa
 		if errsgt != nil {
 			return errsgt
 		}
+	} else if crn != nil && *crn != "" && strings.Contains(*crn, "virtual_network_interfaces") {
+		vniId := sgtarget.ID
+		_, errsgt := isWaitForVNISgTargetCreateAvailable(sess, *vniId, d.Timeout(schema.TimeoutCreate))
+		if errsgt != nil {
+			return errsgt
+		}
 	}
 
 	return resourceIBMISSecurityGroupTargetRead(d, meta)
@@ -312,6 +318,40 @@ func isLBSgTargetRefreshFunc(sess *vpcv1.VpcV1, lbId string) resource.StateRefre
 
 		getlboptions := &vpcv1.GetLoadBalancerOptions{
 			ID: &lbId,
+		}
+		lb, response, err := sess.GetLoadBalancer(getlboptions)
+		if err != nil {
+			return nil, "", fmt.Errorf("[ERROR] Error Getting Load Balancer : %s\n%s", err, response)
+		}
+
+		if *lb.ProvisioningStatus == "active" || *lb.ProvisioningStatus == "failed" {
+			return lb, isLBProvisioningDone, nil
+		}
+
+		return lb, isLBProvisioning, nil
+	}
+}
+
+func isWaitForVNISgTargetCreateAvailable(sess *vpcv1.VpcV1, vniId string, timeout time.Duration) (interface{}, error) {
+	log.Printf("Waiting for virtual network interface (%s) to be available.", vniId)
+
+	stateConf := &resource.StateChangeConf{
+		Pending:    []string{"pending", "updating", "waiting"},
+		Target:     []string{isLBProvisioningDone, ""},
+		Refresh:    isVNISgTargetRefreshFunc(sess, vniId),
+		Timeout:    timeout,
+		Delay:      10 * time.Second,
+		MinTimeout: 10 * time.Second,
+	}
+
+	return stateConf.WaitForState()
+}
+
+func isVNISgTargetRefreshFunc(sess *vpcv1.VpcV1, vniId string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+
+		getlboptions := &vpcv1.GetLoadBalancerOptions{
+			ID: &vniId,
 		}
 		lb, response, err := sess.GetLoadBalancer(getlboptions)
 		if err != nil {
