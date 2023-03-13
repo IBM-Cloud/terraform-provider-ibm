8c0a9e3385aeede481aeba88fa13e785484e9c06
Diksha Mali <diksha.mali@ibm.com>
2023-03-13 14:50:58 +0530

    Resolving the merge conflict

--- ibm/service/cos/resource_ibm_cos_bucket_object_test.go
+++ ibm/service/cos/resource_ibm_cos_bucket_object_test.go
@@ -70,6 +70,103 @@ func TestAccIBMCOSBucketObject_basic(t *testing.T) {
 	})
 }
 
+func TestAccIBMCOSBucketObject_VersioningEnabled(t *testing.T) {
+	name := fmt.Sprintf("tf-testacc-cos-%d", acctest.RandIntRange(10, 100))
+	key := "plaintext.txt"
+	instanceCRN := acc.CosCRN
+	objectBody1 := "Acceptance Testing"
+	objectBody2 := "Acceptance Testing object 2"
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { acc.TestAccPreCheckCOS(t) },
+		Providers: acc.TestAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccIBMCOSBucketBucketObject_Versioning_Enabled(name, key, instanceCRN, objectBody1, objectBody2),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("ibm_cos_bucket.testacc", "object_versioning.#", "1"),
+					resource.TestCheckResourceAttr("ibm_cos_bucket.testacc", "object_versioning.0.enable", "true"),
+					resource.TestCheckResourceAttrSet("ibm_cos_bucket_object.testacc_object", "id"),
+					resource.TestCheckResourceAttrSet("ibm_cos_bucket_object.testacc_object", "content"),
+					resource.TestCheckResourceAttrSet("ibm_cos_bucket_object.testacc_object", "content_length"),
+					resource.TestCheckResourceAttrSet("ibm_cos_bucket_object.testacc_object", "content_type"),
+					resource.TestCheckResourceAttr("ibm_cos_bucket_object.testacc_object", "content", objectBody1),
+					resource.TestCheckResourceAttrSet("ibm_cos_bucket_object.testacc_object2", "id"),
+					resource.TestCheckResourceAttrSet("ibm_cos_bucket_object.testacc_object2", "content"),
+					resource.TestCheckResourceAttrSet("ibm_cos_bucket_object.testacc_object2", "content_length"),
+					resource.TestCheckResourceAttrSet("ibm_cos_bucket_object.testacc_object2", "content_type"),
+					resource.TestCheckResourceAttr("ibm_cos_bucket_object.testacc_object2", "content", objectBody2),
+				),
+			},
+		},
+	})
+}
+
+func TestAccIBMCOSBucketObject_Versioning_Enabled_Sequential_Upload_on_Existing_Bucket(t *testing.T) {
+	key := "plaintext.txt"
+	bucketCRN := acc.BucketCRN
+	objectBody1 := "Acceptance Testing"
+	objectBody2 := "Acceptance Testing object 2"
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { acc.TestAccPreCheckCOS(t) },
+		Providers: acc.TestAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccIBMCOSBucketBucketObjectUpload(bucketCRN, key, objectBody1),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("ibm_cos_bucket_object.testacc_object", "id"),
+					resource.TestCheckResourceAttrSet("ibm_cos_bucket_object.testacc_object", "content"),
+					resource.TestCheckResourceAttrSet("ibm_cos_bucket_object.testacc_object", "content_length"),
+					resource.TestCheckResourceAttrSet("ibm_cos_bucket_object.testacc_object", "content_type"),
+					resource.TestCheckResourceAttr("ibm_cos_bucket_object.testacc_object", "content", objectBody1),
+				),
+			},
+			{
+				Config: testAccIBMCOSBucketBucketObjectUpload(bucketCRN, key, objectBody2),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("ibm_cos_bucket_object.testacc_object", "id"),
+					resource.TestCheckResourceAttrSet("ibm_cos_bucket_object.testacc_object", "content"),
+					resource.TestCheckResourceAttrSet("ibm_cos_bucket_object.testacc_object", "content_length"),
+					resource.TestCheckResourceAttrSet("ibm_cos_bucket_object.testacc_object", "content_type"),
+					resource.TestCheckResourceAttr("ibm_cos_bucket_object.testacc_object", "content", objectBody2),
+				),
+			},
+		},
+	})
+}
+
+func TestAccIBMCOSBucketObject_Uploading_Multile_Objects_on_Existing_Bucket_without_Versioning(t *testing.T) {
+	key := "plaintext.txt"
+	bucketCRN := acc.BucketCRN
+	objectBody1 := "Acceptance Testing"
+	objectBody2 := "Acceptance Testing object 2"
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { acc.TestAccPreCheckCOS(t) },
+		Providers: acc.TestAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccIBMCOSBucketBucketObjectUpload(bucketCRN, key, objectBody1),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("ibm_cos_bucket_object.testacc_object", "id"),
+					resource.TestCheckResourceAttrSet("ibm_cos_bucket_object.testacc_object", "content"),
+					resource.TestCheckResourceAttrSet("ibm_cos_bucket_object.testacc_object", "content_length"),
+					resource.TestCheckResourceAttrSet("ibm_cos_bucket_object.testacc_object", "content_type"),
+					resource.TestCheckResourceAttr("ibm_cos_bucket_object.testacc_object", "content", objectBody1),
+				),
+			},
+			{
+				Config: testAccIBMCOSBucketBucketObjectUpload(bucketCRN, key, objectBody2),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("ibm_cos_bucket_object.testacc_object", "id"),
+					resource.TestCheckResourceAttrSet("ibm_cos_bucket_object.testacc_object", "content"),
+					resource.TestCheckResourceAttrSet("ibm_cos_bucket_object.testacc_object", "content_length"),
+					resource.TestCheckResourceAttrSet("ibm_cos_bucket_object.testacc_object", "content_type"),
+					resource.TestCheckResourceAttr("ibm_cos_bucket_object.testacc_object", "content", objectBody2),
+				),
+			},
+		},
+	})
+}
+
 func TestAccIBMCOSBucketObjectlock_Retention_Without_Mode(t *testing.T) {
 	name := fmt.Sprintf("tf-testacc-cos-%d", acctest.RandIntRange(10, 100))
 	instanceCRN := acc.CosCRN
@@ -222,6 +319,41 @@ func testAccIBMCOSBucketObjectConfig_file(name string, instanceCRN string, objec
 			key 					  = "%[1]s.txt"
 			content_file	  = "%[3]s"
 		}`, name, instanceCRN, objectFile)
+}
+
+func testAccIBMCOSBucketBucketObject_Versioning_Enabled(name string, key string, instanceCRN string, objectBody1 string, objectBody2 string) string {
+	return fmt.Sprintf(`
+		resource "ibm_cos_bucket" "testacc" {
+			bucket_name          = "%[1]s"
+			resource_instance_id = "%[3]s"
+			region_location      = "us-south"
+			storage_class        = "standard"
+			object_versioning {
+				enable  = true
+			  }
+		}
+		resource "ibm_cos_bucket_object" "testacc_object" {
+			bucket_crn	    = ibm_cos_bucket.testacc.crn
+			bucket_location = ibm_cos_bucket.testacc.region_location
+			key 			= "%[2]s"
+			content			= "%[4]s"
+		}
+		resource "ibm_cos_bucket_object" "testacc_object2" {
+			bucket_crn	    = ibm_cos_bucket.testacc.crn
+			bucket_location = ibm_cos_bucket.testacc.region_location
+			key 					  = "%[2]s"
+			content			    = "%[5]s"
+		}`, name, key, instanceCRN, objectBody1, objectBody2)
+}
+
+func testAccIBMCOSBucketBucketObjectUpload(bucketCrn string, key string, objectBody1 string) string {
+	return fmt.Sprintf(`
+		resource "ibm_cos_bucket_object" "testacc_object" {
+			bucket_crn	    = "%[1]s"
+			bucket_location = "us-south"
+			key 			= "%[2]s"
+			content			= "%[3]s"
+		}`, bucketCrn, key, objectBody1)
 }
 
 func testAccIBMCOSBucketObjectlock_retention_without_objectlock_enabled(name string, instanceCRN string, objectBody string, mode string, retainUntilDate string) string {
