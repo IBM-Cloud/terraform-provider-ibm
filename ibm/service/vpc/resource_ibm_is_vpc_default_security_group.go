a7954d30344b8214358d539a755b9eedaed351af
Ujjwal Kumar <Ujjwal.Kumar1@ibm.com>
2023-03-21 14:31:17 +0530

    Update resource_ibm_is_vpc_default_security_group.go

--- ibm/service/vpc/resource_ibm_is_vpc_default_security_group.go
+++ ibm/service/vpc/resource_ibm_is_vpc_default_security_group.go
@@ -299,14 +299,16 @@ func addNewDefaultSecurityGroupRules(d *schema.ResourceData, sess *vpcv1.VpcV1, 
 			direction = strings.ToLower(direction)
 			remote := rulex[isSecurityGroupRuleRemote].(string)
 			remote = strings.ToLower(direction)
-			ruletype := rulex[isSecurityGroupRuleType].(string)
-			ruletype = strings.ToLower(direction)
-			code := rulex[isSecurityGroupRuleCode].(string)
-			code = strings.ToLower(direction)
-			portmin := rulex[isSecurityGroupRulePortMin].(string)
-			portmin = strings.ToLower(direction)
-			portmax := rulex[isSecurityGroupRulePortMax].(string)
-			portmax = strings.ToLower(direction)
+			ruletype := rulex[isSecurityGroupRuleType].(int)
+			ruletype64 := int64(ruletype)
+			code := rulex[isSecurityGroupRuleCode].(int)
+			code64 := int64(code)
+			portmin := rulex[isSecurityGroupRulePortMin].(int)
+			portmin64 := int64(portmin)
+
+			portmax := rulex[isSecurityGroupRulePortMax].(int)
+			portmax64 := int64(portmax)
+
 			protocol := rulex[isSecurityGroupRuleProtocol].(string)
 			protocol = strings.ToLower(direction)
 
@@ -314,158 +316,35 @@ func addNewDefaultSecurityGroupRules(d *schema.ResourceData, sess *vpcv1.VpcV1, 
 			conns.IbmMutexKV.Lock(isSecurityGroupRuleKey)
 			defer conns.IbmMutexKV.Unlock(isSecurityGroupRuleKey)
 
-			parsed := &parsedIBMISSecurityGroupRuleDictionary{}
 			sgTemplate := &vpcv1.SecurityGroupRulePrototype{}
-			sgTemplateUpdate := &vpcv1.UpdateSecurityGroupRuleOptions{}
-			var err error
-			parsed.icmpType = -1
-			parsed.icmpCode = -1
-			parsed.portMin = -1
-			parsed.portMax = -1
-
-			parsed.secgrpID, parsed.ruleID, err = parseISTerraformID(d.Id())
-			if err != nil {
-				parsed.secgrpID = d.Get(isSecurityGroupID).(string)
-			} else {
-				sgTemplateUpdate.SecurityGroupID = &parsed.secgrpID
-				sgTemplateUpdate.ID = &parsed.ruleID
+			if direction != "" {
+				sgTemplate.Direction = &direction
 			}
-
-			securityGroupRulePatchModel := &vpcv1.SecurityGroupRulePatch{}
-
-			parsed.direction = d.Get(isSecurityGroupRuleDirection).(string)
-			sgTemplate.Direction = &parsed.direction
-			securityGroupRulePatchModel.Direction = &parsed.direction
-
-			if version, ok := d.GetOk(isSecurityGroupRuleIPVersion); ok {
-				parsed.ipversion = version.(string)
-				sgTemplate.IPVersion = &parsed.ipversion
-				securityGroupRulePatchModel.IPVersion = &parsed.ipversion
-			} else {
-				parsed.ipversion = "IPv4"
-				sgTemplate.IPVersion = &parsed.ipversion
-				securityGroupRulePatchModel.IPVersion = &parsed.ipversion
-			}
-
-			parsed.remote = ""
-			if pr, ok := d.GetOk(isSecurityGroupRuleRemote); ok {
-				parsed.remote = pr.(string)
-			}
-			parsed.remoteAddress = ""
-			parsed.remoteCIDR = ""
-			parsed.remoteSecGrpID = ""
-			err = nil
-			if parsed.remote != "" {
-				parsed.remoteAddress, parsed.remoteCIDR, parsed.remoteSecGrpID, err = inferRemoteSecurityGroup(parsed.remote)
+			if remote != "" {
 				remoteTemplate := &vpcv1.SecurityGroupRuleRemotePrototype{}
-				remoteTemplateUpdate := &vpcv1.SecurityGroupRuleRemotePatch{}
-				if parsed.remoteAddress != "" {
-					remoteTemplate.Address = &parsed.remoteAddress
-					remoteTemplateUpdate.Address = &parsed.remoteAddress
-				} else if parsed.remoteCIDR != "" {
-					remoteTemplate.CIDRBlock = &parsed.remoteCIDR
-					remoteTemplateUpdate.CIDRBlock = &parsed.remoteCIDR
-				} else if parsed.remoteSecGrpID != "" {
-					remoteTemplate.ID = &parsed.remoteSecGrpID
-					remoteTemplateUpdate.ID = &parsed.remoteSecGrpID
-
-					// check if remote is actually a SG identifier
-					getSecurityGroupOptions := &vpcv1.GetSecurityGroupOptions{
-						ID: &parsed.remoteSecGrpID,
-					}
-					sg, res, err := sess.GetSecurityGroup(getSecurityGroupOptions)
-					if err != nil || sg == nil {
-						if res != nil && res.StatusCode == 404 {
-							return nil, nil, nil, err
-						}
-						return nil, nil, nil, fmt.Errorf("Error getting Security Group in remote (%s): %s\n%s", parsed.remoteSecGrpID, err, res)
-					}
-				}
-				sgTemplate.Remote = remoteTemplate
-				securityGroupRulePatchModel.Remote = remoteTemplateUpdate
-			}
-			if err != nil {
-				return nil, nil, nil, err
-			}
-			parsed.protocol = "all"
-
-			if icmpInterface, ok := d.GetOk("icmp"); ok {
-				if icmpInterface.([]interface{})[0] != nil {
-					haveType := false
-					icmp := icmpInterface.([]interface{})[0].(map[string]interface{})
-					if value, ok := icmp["type"]; ok {
-						parsed.icmpType = int64(value.(int))
-						haveType = true
-					}
-					if value, ok := icmp["code"]; ok {
-						if !haveType {
-							return nil, nil, nil, fmt.Errorf("icmp code requires icmp type")
-						}
-						parsed.icmpCode = int64(value.(int))
-					}
-				}
-				parsed.protocol = "icmp"
-				if icmpInterface.([]interface{})[0] == nil {
-					parsed.icmpType = 0
-					parsed.icmpCode = 0
+				if validate.IsSecurityGroupAddress(remote) {
+					remoteTemplate.Address = &remote
+				} else if validate.IsSecurityGroupCIDR(remote) {
+					remoteTemplate.CIDRBlock = &remote
 				} else {
-					sgTemplate.Type = &parsed.icmpType
-					sgTemplate.Code = &parsed.icmpCode
-				}
-				sgTemplate.Protocol = &parsed.protocol
-				securityGroupRulePatchModel.Type = &parsed.icmpType
-				securityGroupRulePatchModel.Code = &parsed.icmpCode
-			}
-			for _, prot := range []string{"tcp", "udp"} {
-				if tcpInterface, ok := d.GetOk(prot); ok {
-					if tcpInterface.([]interface{})[0] != nil {
-						haveMin := false
-						haveMax := false
-						ports := tcpInterface.([]interface{})[0].(map[string]interface{})
-						if value, ok := ports["port_min"]; ok {
-							parsed.portMin = int64(value.(int))
-							haveMin = true
-						}
-						if value, ok := ports["port_max"]; ok {
-							parsed.portMax = int64(value.(int))
-							haveMax = true
-						}
-
-						// If only min or max is set, ensure that both min and max are set to the same value
-						if haveMin && !haveMax {
-							parsed.portMax = parsed.portMin
-						}
-						if haveMax && !haveMin {
-							parsed.portMin = parsed.portMax
-						}
-					}
-					parsed.protocol = prot
-					sgTemplate.Protocol = &parsed.protocol
-					if tcpInterface.([]interface{})[0] == nil {
-						parsed.portMax = 65535
-						parsed.portMin = 1
-					}
-					sgTemplate.PortMax = &parsed.portMax
-					sgTemplate.PortMin = &parsed.portMin
-					securityGroupRulePatchModel.PortMax = &parsed.portMax
-					securityGroupRulePatchModel.PortMin = &parsed.portMin
+					remoteTemplate.ID = &remote
 				}
 			}
-			if parsed.protocol == "all" {
-				sgTemplate.Protocol = &parsed.protocol
+			sgTemplate.Type = &ruletype64
+			if direction != "" {
+				sgTemplate.Direction = &direction
 			}
-			securityGroupRulePatch, err := securityGroupRulePatchModel.AsPatch()
-			if err != nil {
-				return nil, nil, nil, fmt.Errorf("[ERROR] Error calling asPatch for SecurityGroupRulePatch: %s", err)
-			}
-			sgTemplateUpdate.SecurityGroupRulePatch = securityGroupRulePatch
+			sgTemplate.Code = &code64
+			sgTemplate.PortMin = &portmin64
+			sgTemplate.PortMax = &portmax64
+			sgTemplate.Protocol = &protocol
 
 			options := &vpcv1.CreateSecurityGroupRuleOptions{
 				SecurityGroupID:            &id,
 				SecurityGroupRulePrototype: sgTemplate,
 			}
 
-			rule, response, err := sess.CreateSecurityGroupRule(options)
+			_, response, err := sess.CreateSecurityGroupRule(options)
 			if err != nil {
 				return fmt.Errorf("[ERROR] Error while creating Security Group Rule %s\n%s", err, response)
 			}
@@ -485,33 +364,11 @@ func addNewDefaultSecurityGroupRules(d *schema.ResourceData, sess *vpcv1.VpcV1, 
 		SecurityGroupRulePrototype: sgTemplate,
 	}
 
-	rule, response, err := sess.CreateSecurityGroupRule(options)
+	_, response, err := sess.CreateSecurityGroupRule(options)
 	if err != nil {
 		return fmt.Errorf("[ERROR] Error while creating Security Group Rule %s\n%s", err, response)
 	}
-	switch reflect.TypeOf(rule).String() {
-	case "*vpcv1.SecurityGroupRuleSecurityGroupRuleProtocolIcmp":
-		{
-			sgrule := rule.(*vpcv1.SecurityGroupRuleSecurityGroupRuleProtocolIcmp)
-			d.Set(isSecurityGroupRuleID, *sgrule.ID)
-			tfID := makeTerraformRuleID(parsed.secgrpID, *sgrule.ID)
-			d.SetId(tfID)
-		}
-	case "*vpcv1.SecurityGroupRuleSecurityGroupRuleProtocolAll":
-		{
-			sgrule := rule.(*vpcv1.SecurityGroupRuleSecurityGroupRuleProtocolAll)
-			d.Set(isSecurityGroupRuleID, *sgrule.ID)
-			tfID := makeTerraformRuleID(parsed.secgrpID, *sgrule.ID)
-			d.SetId(tfID)
-		}
-	case "*vpcv1.SecurityGroupRuleSecurityGroupRuleProtocolTcpudp":
-		{
-			sgrule := rule.(*vpcv1.SecurityGroupRuleSecurityGroupRuleProtocolTcpudp)
-			d.Set(isSecurityGroupRuleID, *sgrule.ID)
-			tfID := makeTerraformRuleID(parsed.secgrpID, *sgrule.ID)
-			d.SetId(tfID)
-		}
-	}
+
 	return nil
 }
 func resourceIBMISVPCDefaultSecurityGroupRead(d *schema.ResourceData, meta interface{}) error {
@@ -704,94 +561,7 @@ func resourceIBMISVPCDefaultSecurityGroupUpdate(d *schema.ResourceData, meta int
 }
 
 func resourceIBMISVPCDefaultSecurityGroupDelete(d *schema.ResourceData, meta interface{}) error {
-	sess, err := vpcClient(meta)
-	if err != nil {
-		return err
-	}
-	id := d.Id()
 
-	getSecurityGroupOptions := &vpcv1.GetSecurityGroupOptions{
-		ID: &id,
-	}
-	_, response, err := sess.GetSecurityGroup(getSecurityGroupOptions)
-	if err != nil {
-		if response != nil && response.StatusCode == 404 {
-			d.SetId("")
-			return nil
-		}
-		return fmt.Errorf("[ERROR] Error Getting Security Group (%s): %s\n%s", id, err, response)
-	}
-
-	start := ""
-	allrecs := []vpcv1.SecurityGroupTargetReferenceIntf{}
-
-	for {
-		listSecurityGroupTargetsOptions := sess.NewListSecurityGroupTargetsOptions(id)
-
-		groups, response, err := sess.ListSecurityGroupTargets(listSecurityGroupTargetsOptions)
-		if err != nil || groups == nil {
-			return fmt.Errorf("[ERROR] Error Getting Security Group Targets %s\n%s", err, response)
-		}
-		if *groups.TotalCount == int64(0) {
-			break
-		}
-
-		start = flex.GetNext(groups.Next)
-		allrecs = append(allrecs, groups.Targets...)
-
-		if start == "" {
-			break
-		}
-
-	}
-
-	for _, securityGroupTargetReferenceIntf := range allrecs {
-		if securityGroupTargetReferenceIntf != nil {
-			securityGroupTargetReference := securityGroupTargetReferenceIntf.(*vpcv1.SecurityGroupTargetReference)
-			if securityGroupTargetReference != nil && securityGroupTargetReference.ID != nil {
-
-				deleteSecurityGroupTargetBindingOptions := sess.NewDeleteSecurityGroupTargetBindingOptions(id, *securityGroupTargetReference.ID)
-				response, err = sess.DeleteSecurityGroupTargetBinding(deleteSecurityGroupTargetBindingOptions)
-				if err != nil {
-					if response != nil {
-						if response.StatusCode == 404 {
-							log.Printf("[DEBUG] Security group target(%s) binding is already deleted", *securityGroupTargetReference.ID)
-						} else if response.StatusCode == 409 {
-							log.Printf("[DEBUG] Security group target(%s) binding is in deleting status, waiting till target is removed", *securityGroupTargetReference.ID)
-							_, err = isWaitForTargetDeleted(sess, id, *securityGroupTargetReference.ID, securityGroupTargetReferenceIntf, d.Timeout(schema.TimeoutDelete))
-							if err != nil {
-								return err
-							}
-						}
-					} else {
-						return fmt.Errorf("[ERROR] Error deleting security group target binding while deleting security group : %s\n%s", err, response)
-					}
-				}
-
-			}
-		}
-	}
-
-	deleteSecurityGroupOptions := &vpcv1.DeleteSecurityGroupOptions{
-		ID: &id,
-	}
-	response, err = sess.DeleteSecurityGroup(deleteSecurityGroupOptions)
-
-	if err != nil {
-		if response != nil {
-			if response.StatusCode == 404 {
-				log.Printf("[DEBUG] Security group(%s) target bindings are already deleted", id)
-			} else if response.StatusCode == 409 {
-				log.Printf("[DEBUG] Security group(%s) has target bindings is in deleting, will wait till target is removed", id)
-				_, err = isWaitForSgCleanup(sess, id, allrecs, d.Timeout(schema.TimeoutDelete))
-				if err != nil {
-					return err
-				}
-			}
-		} else {
-			return fmt.Errorf("[ERROR] Error Deleting Security Group : %s\n%s", err, response)
-		}
-	}
 	d.SetId("")
 	return nil
 }
@@ -814,54 +584,6 @@ func resourceIBMISVPCDefaultSecurityGroupExists(d *schema.ResourceData, meta int
 		return false, fmt.Errorf("[ERROR] Error getting Security Group: %s\n%s", err, response)
 	}
 	return true, nil
-}
-
-func makeIBMISVPCDefaultSecurityRuleSchema() map[string]*schema.Schema {
-	return map[string]*schema.Schema{
-
-		isSecurityGroupRuleDirection: {
-			Type:        schema.TypeString,
-			Computed:    true,
-			Description: "Direction of traffic to enforce, either inbound or outbound",
-		},
-
-		isSecurityGroupRuleIPVersion: {
-			Type:        schema.TypeString,
-			Computed:    true,
-			Description: "IP version: ipv4",
-		},
-
-		isSecurityGroupRuleRemote: {
-			Type:        schema.TypeString,
-			Computed:    true,
-			Description: "Security group id: an IP address, a CIDR block, or a single security group identifier",
-		},
-
-		isSecurityGroupRuleType: {
-			Type:     schema.TypeInt,
-			Computed: true,
-		},
-
-		isSecurityGroupRuleCode: {
-			Type:     schema.TypeInt,
-			Computed: true,
-		},
-
-		isSecurityGroupRulePortMin: {
-			Type:     schema.TypeInt,
-			Computed: true,
-		},
-
-		isSecurityGroupRulePortMax: {
-			Type:     schema.TypeInt,
-			Computed: true,
-		},
-
-		isSecurityGroupRuleProtocol: {
-			Type:     schema.TypeString,
-			Computed: true,
-		},
-	}
 }
 
 func isWaitForVPCDefaultTargetDeleted(client *vpcv1.VpcV1, sgId, targetId string, target vpcv1.SecurityGroupTargetReferenceIntf, timeout time.Duration) (interface{}, error) {
