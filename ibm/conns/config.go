9ebf329e372b7e5666e8a6efb5d37564402763a3
rangelil <riccardo.angelilli@it.ibm.com>
2023-03-31 13:59:59 +0200

    .

--- ibm/conns/config.go
+++ ibm/conns/config.go
@@ -1545,9 +1545,9 @@ func (c *Config) ClientSession() (interface{}, error) {
 	} else {
 		session.projectClientErr = fmt.Errorf("Error occurred while configuring Projects API Specification service: %q", err)
 	}
-    if c.Visibility == "private" || c.Visibility == "public-and-private" {
-        session.pushServiceClientErr = fmt.Errorf("Projects Service API does not support private endpoints")
-    }
+	if c.Visibility == "private" || c.Visibility == "public-and-private" {
+		session.projectClientErr = fmt.Errorf("Projects Service API does not support private endpoints")
+	}
 
 	// Construct an "options" struct for creating the service client.
 	ukoClientOptions := &ukov4.UkoV4Options{
