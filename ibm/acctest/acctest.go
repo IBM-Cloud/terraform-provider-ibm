81ba38ff62896527461c7e17122d886f50e48c5a
Ujjwal Kumar <Ujjwal.Kumar1@ibm.com>
2023-07-19 18:02:58 +0530

    backup: added suuport for cross region copy for vpc backup

--- ibm/acctest/acctest.go
+++ ibm/acctest/acctest.go
@@ -141,6 +141,7 @@ var IcdDbTaskId string
 var KmsInstanceID string
 var CrkID string
 var KmsAccountID string
+var BaasEncryptionkeyCRN string
 
 // for snapshot encryption
 var IsKMSInstanceId string
@@ -630,6 +631,12 @@ func init() {
 	BackupPolicyID = os.Getenv("IS_BACKUP_POLICY_ID")
 	if BackupPolicyID == "" {
 		fmt.Println("[INFO] Set the environment variable IS_BACKUP_POLICY_ID for testing ibm_is_backup_policy_jobs datasource")
+	}
+
+	BaasEncryptionkeyCRN = os.Getenv("IS_REMOTE_CP_BAAS_ENCRYPTION_KEY_CRN")
+	if BaasEncryptionkeyCRN == "" {
+		BaasEncryptionkeyCRN = "crn:v1:bluemix:public:kms:us-south:a/dffc98a0f1f0f95f6613b3b752286b87:e4a29d1a-2ef0-42a6-8fd2-350deb1c647e:key:5437653b-c4b1-447f-9646-b2a2a4cd6179"
+		fmt.Println("[INFO] Set the environment variable IS_REMOTE_CP_BAAS_ENCRYPTION_KEY_CRN for testing remote_copies_policy with Baas plans, else it is set to default value, 'crn:v1:bluemix:public:kms:us-south:a/dffc98a0f1f0f95f6613b3b752286b87:e4a29d1a-2ef0-42a6-8fd2-350deb1c647e:key:5437653b-c4b1-447f-9646-b2a2a4cd6179'")
 	}
 
 	InstanceProfileName = os.Getenv("SL_INSTANCE_PROFILE")
