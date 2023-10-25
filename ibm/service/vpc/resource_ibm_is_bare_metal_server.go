166d3d8122acc10964d037a5cb7a935c8eeea971
SunithaGudisagar <Sunitha.Gudisagar@ibm.com>
2023-10-25 16:54:36 +0530

    SDK Changes

--- ibm/service/vpc/resource_ibm_is_bare_metal_server.go
+++ ibm/service/vpc/resource_ibm_is_bare_metal_server.go
@@ -1296,7 +1296,7 @@ func resourceIBMISBareMetalServerCreate(context context.Context, d *schema.Resou
 
 	if v, ok := d.GetOk(isBareMetalServerVPC); ok {
 		vpc := v.(string)
-		options.VPC = &vpcv1.VPCIdentity{
+		options.VPC = &vpcv1.BareMetalServerPrototypeVPC{
 			ID: &vpc,
 		}
 	}
