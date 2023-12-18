6cec829ab6421ccc7325599d16a3a128f4d526db
rangelil <riccardo.angelilli@it.ibm.com>
2023-12-18 17:18:24 +0100

    fix samples

--- examples/ibm-project/main.tf
+++ examples/ibm-project/main.tf
@@ -14,7 +14,7 @@ resource "ibm_project_config" "project_config_instance" {
     }
     locator_id = "1082e7d2-5e2f-0a11-a3bc-f88a8e1931fc.145be7c1-9ec4-4719-b586-584ee52fbed0-global"
     inputs = {
-      name = "app_repo_name"
+      app_repo_name = "static-website-repo"
     }
   }
 }
