530d41bcb8d629c339798acd63491e290a2af4b7
Diptipowervs <dipti.kumari2@ibm.com>
2023-12-29 17:09:28 +0530

    Catalog refactor instance console languages-ip-volumes data source and documentation

--- ibm/service/power/data_source_ibm_pi_instance_console_languages.go
+++ ibm/service/power/data_source_ibm_pi_instance_console_languages.go
@@ -15,6 +15,10 @@ import (
 )
 
 // Datasource to list available console languages for an instance
+const (
+	ConsoleLanguageCode = "Code"
+)
+
 func DataSourceIBMPIInstanceConsoleLanguages() *schema.Resource {
 	return &schema.Resource{
 		ReadContext: dataSourceIBMPIInstanceConsoleLanguagesRead,
