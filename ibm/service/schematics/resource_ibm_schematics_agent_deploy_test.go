881cb4a348c90fabc1b1365bfc610bceb34c798b
VaishnaviGopal <ggunishetty9@gmail.com>
2024-01-02 20:46:49 +0530

    Changes wrt latest schematics sdk

--- ibm/service/schematics/resource_ibm_schematics_agent_deploy_test.go
+++ ibm/service/schematics/resource_ibm_schematics_agent_deploy_test.go
@@ -106,9 +106,13 @@ func testAccCheckIbmSchematicsAgentDeployExists(n string, obj *schematicsv1.Agen
 		}
 
 		getAgentDataOptions := &schematicsv1.GetAgentDataOptions{
-			XFeatureAgents: core.BoolPtr(true),
-			Profile:        core.StringPtr("detailed"),
+			// XFeatureAgents: core.BoolPtr(true),
+			Profile: core.StringPtr("detailed"),
 		}
+		ff := map[string]string{
+			"X-Feature-Agents": "true",
+		}
+		getAgentDataOptions.Headers = ff
 
 		parts, err := flex.SepIdParts(rs.Primary.ID, "/")
 		if err != nil {
@@ -138,8 +142,8 @@ func testAccCheckIbmSchematicsAgentDeployDestroy(s *terraform.State) error {
 		}
 
 		getAgentDataOptions := &schematicsv1.GetAgentDataOptions{
-			XFeatureAgents: core.BoolPtr(true),
-			Profile:        core.StringPtr("detailed"),
+			// XFeatureAgents: core.BoolPtr(true),
+			Profile: core.StringPtr("detailed"),
 		}
 
 		parts, err := flex.SepIdParts(rs.Primary.ID, "/")
