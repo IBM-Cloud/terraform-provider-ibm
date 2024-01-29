1ea2493c94969c5e929d83a632031cd079992274
Yonathan-Yellin <yonathan.yellin@ibm.com>
2024-01-29 13:09:19 +0200

    support for creating secret version for username password & version_custom_metadata for all

--- ibm/service/secretsmanager/data_source_ibm_secrets_manager_secrets_test.go
+++ ibm/service/secretsmanager/data_source_ibm_secrets_manager_secrets_test.go
@@ -3,34 +3,40 @@
 
 package secretsmanager_test
 
-//func TestAccIBMSecretsManagerSecretsDataSourceBasic(t *testing.T) {
-//	resource.Test(t, resource.TestCase{
-//		PreCheck:  func() { acc.TestAccPreCheck(t) },
-//		Providers: acc.TestAccProviders,
-//		Steps: []resource.TestStep{
-//			{
-//				Config: testAccCheckIBMSecretsManagerSecretsDataSourceConfigBasic(),
-//				Check: resource.ComposeTestCheckFunc(
-//					resource.TestCheckResourceAttr("data.ibm_secrets_manager_secrets.secrets_manager_secrets", "secret_type", acc.SecretsManagerSecretType),
-//					resource.TestCheckResourceAttrSet("data.ibm_secrets_manager_secrets.secrets_manager_secrets", "id"),
-//					resource.TestCheckResourceAttrSet("data.ibm_secrets_manager_secrets.secrets_manager_secrets", "secret_type"),
-//					resource.TestCheckResourceAttrSet("data.ibm_secrets_manager_secrets.secrets_manager_secrets", "metadata.#"),
-//					resource.TestCheckResourceAttrSet("data.ibm_secrets_manager_secrets.secrets_manager_secrets", "secrets.#"),
-//				),
-//			},
-//		},
-//	})
-//}
-//
-//func testAccCheckIBMSecretsManagerSecretsDataSourceConfigBasic() string {
-//	return fmt.Sprintf(`
-//		data "ibm_secrets_manager_secrets" "secrets_manager_secrets" {
-//			instance_id = "%s"
-//			secret_type = "%s"
-//		}
-//
-//		output "WorkSpaceValues" {
-//			value = data.ibm_secrets_manager_secrets.secrets_manager_secrets.secret_type
-//		}
-//	`, acc.SecretsManagerInstanceID, acc.SecretsManagerSecretType)
-//}
+import (
+	"fmt"
+	acc "github.com/IBM-Cloud/terraform-provider-ibm/ibm/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccIBMSecretsManagerSecretsDataSourceBasic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { acc.TestAccPreCheck(t) },
+		Providers: acc.TestAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckIBMSecretsManagerSecretsDataSourceConfigBasic(),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.ibm_secrets_manager_secrets.secrets_manager_secrets", "secret_type", acc.SecretsManagerSecretType),
+					resource.TestCheckResourceAttrSet("data.ibm_secrets_manager_secrets.secrets_manager_secrets", "id"),
+					resource.TestCheckResourceAttrSet("data.ibm_secrets_manager_secrets.secrets_manager_secrets", "secret_type"),
+					resource.TestCheckResourceAttrSet("data.ibm_secrets_manager_secrets.secrets_manager_secrets", "metadata.#"),
+					resource.TestCheckResourceAttrSet("data.ibm_secrets_manager_secrets.secrets_manager_secrets", "secrets.#"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckIBMSecretsManagerSecretsDataSourceConfigBasic() string {
+	return fmt.Sprintf(`
+		data "ibm_secrets_manager_secrets" "secrets_manager_secrets" {
+			instance_id = "%s"
+			secret_type = "%s"
+		}
+
+		output "WorkSpaceValues" {
+			value = data.ibm_secrets_manager_secrets.secrets_manager_secrets.secret_type
+		}
+	`, acc.SecretsManagerInstanceID, acc.SecretsManagerSecretType)
+}
