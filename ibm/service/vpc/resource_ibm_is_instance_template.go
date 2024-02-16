04baadefeecc4240f4bd7e536b1a21963b218a26
Ujjwal Kumar <78945437+ujjwal-ibm@users.noreply.github.com>
2024-02-16 15:52:05 +0530

    reordered reservation on instance to avoid update and length check on template (#5141)
    * reordered reservation on instance to avoid update and length check on template
    
    * Added length check for reservation affinity pool in instance template data

--- ibm/service/vpc/resource_ibm_is_instance_template.go
+++ ibm/service/vpc/resource_ibm_is_instance_template.go
@@ -1648,7 +1648,7 @@ func instanceTemplateGet(d *schema.ResourceData, meta interface{}, ID string) er
 		reservationAffinityMap := map[string]interface{}{}
 
 		reservationAffinityMap[isReservationAffinityPolicyResp] = instance.ReservationAffinity.Policy
-		if instance.ReservationAffinity.Pool != nil {
+		if instance.ReservationAffinity.Pool != nil && len(instance.ReservationAffinity.Pool) > 0 {
 			pool := instance.ReservationAffinity.Pool[0]
 			res := ""
 			if idPool, ok := pool.(*vpcv1.ReservationIdentityByID); ok {
