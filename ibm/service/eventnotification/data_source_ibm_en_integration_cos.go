7e18413ff3ebba5da50fdb7fc2e1c12bf212ba46
Zoltan Illes <zoltan.illes@ibm.com>
2024-02-23 20:37:41 +0100

    handle host_id in waitForHostAttachment

--- ibm/service/eventnotification/data_source_ibm_en_integration_cos.go
+++ ibm/service/eventnotification/data_source_ibm_en_integration_cos.go
@@ -1,4 +1,4 @@
-// Copyright IBM Corp. 2021 All Rights Reserved.
+// Copyright IBM Corp. 2021, 2024 All Rights Reserved.
 // Licensed under the Mozilla Public License v2.0
 
 package eventnotification
