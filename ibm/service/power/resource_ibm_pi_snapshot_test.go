f655a8624c7c54a764732fde405d9dbe376f3ed5
Axel Ismirlian <ismirlia@us.ibm.com>
2024-04-17 16:38:48 -0500

    Refactor ibm_pi_snapshot tests

--- ibm/service/power/resource_ibm_pi_snapshot_test.go
+++ ibm/service/power/resource_ibm_pi_snapshot_test.go
@@ -10,18 +10,16 @@ import (
 	"testing"
 
 	acc "github.com/IBM-Cloud/terraform-provider-ibm/ibm/acctest"
-	"github.com/IBM-Cloud/terraform-provider-ibm/ibm/conns"
 
+	"github.com/IBM-Cloud/power-go-client/clients/instance"
+	"github.com/IBM-Cloud/terraform-provider-ibm/ibm/conns"
+	"github.com/IBM-Cloud/terraform-provider-ibm/ibm/service/power"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-
-	st "github.com/IBM-Cloud/power-go-client/clients/instance"
-	"github.com/IBM-Cloud/power-go-client/helpers"
 )
 
 func TestAccIBMPIInstanceSnapshotbasic(t *testing.T) {
-
 	name := fmt.Sprintf("tf-pi-instance-snapshot-%d", acctest.RandIntRange(10, 100))
 	snapshotRes := "ibm_pi_snapshot.power_snapshot"
 	resource.Test(t, resource.TestCase{
@@ -30,19 +28,19 @@ func TestAccIBMPIInstanceSnapshotbasic(t *testing.T) {
 		CheckDestroy: testAccCheckIBMPIInstanceSnapshotDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCheckIBMPIInstanceSnapshotConfig(name, helpers.PIInstanceHealthOk),
+				Config: testAccCheckIBMPIInstanceSnapshotConfig(name, power.Health_OK),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckIBMPIInstanceSnapshotExists(snapshotRes),
 					resource.TestCheckResourceAttr(snapshotRes, "pi_snap_shot_name", name),
-					resource.TestCheckResourceAttr(snapshotRes, "status", "available"),
+					resource.TestCheckResourceAttr(snapshotRes, "status", power.State_Available),
 					resource.TestCheckResourceAttrSet(snapshotRes, "id"),
 				),
 			},
 		},
 	})
 }
-func testAccCheckIBMPIInstanceSnapshotDestroy(s *terraform.State) error {
 
+func testAccCheckIBMPIInstanceSnapshotDestroy(s *terraform.State) error {
 	sess, err := acc.TestAccProvider.Meta().(conns.ClientSession).IBMPISession()
 	if err != nil {
 		return err
@@ -55,20 +53,18 @@ func testAccCheckIBMPIInstanceSnapshotDestroy(s *terraform.State) error {
 		if err != nil {
 			return err
 		}
-		snapshotC := st.NewIBMPISnapshotClient(context.Background(), sess, cloudInstanceID)
+		snapshotC := instance.NewIBMPISnapshotClient(context.Background(), sess, cloudInstanceID)
 		_, err = snapshotC.Get(snapshotID)
 		if err == nil {
 			return fmt.Errorf("PI Instance Snapshot still exists: %s", rs.Primary.ID)
 		}
 	}
-
 	return nil
 }
+
 func testAccCheckIBMPIInstanceSnapshotExists(n string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
-
 		rs, ok := s.RootModule().Resources[n]
-
 		if !ok {
 			return fmt.Errorf("Not found: %s", n)
 		}
@@ -81,11 +77,13 @@ func testAccCheckIBMPIInstanceSnapshotExists(n string) resource.TestCheckFunc {
 		if err != nil {
 			return err
 		}
+
 		cloudInstanceID, snapshotID, err := splitID(rs.Primary.ID)
 		if err != nil {
 			return err
 		}
-		client := st.NewIBMPISnapshotClient(context.Background(), sess, cloudInstanceID)
+
+		client := instance.NewIBMPISnapshotClient(context.Background(), sess, cloudInstanceID)
 
 		_, err = client.Get(snapshotID)
 		if err != nil {
@@ -97,12 +95,11 @@ func testAccCheckIBMPIInstanceSnapshotExists(n string) resource.TestCheckFunc {
 
 func testAccCheckIBMPIInstanceSnapshotConfig(name, healthStatus string) string {
 	return testAccCheckIBMPIInstanceConfig(name, healthStatus) + fmt.Sprintf(`
-	resource "ibm_pi_snapshot" "power_snapshot"{
-		depends_on=[ibm_pi_instance.power_instance]
-		pi_instance_name       = ibm_pi_instance.power_instance.pi_instance_name
-		pi_cloud_instance_id = "%s"
-		pi_snap_shot_name       = "%s"
-		pi_volume_ids         = [ibm_pi_volume.power_volume.volume_id]
-	  }
-	`, acc.Pi_cloud_instance_id, name)
+		resource "ibm_pi_snapshot" "power_snapshot"{
+			depends_on=[ibm_pi_instance.power_instance]
+			pi_instance_name       = ibm_pi_instance.power_instance.pi_instance_name
+			pi_cloud_instance_id = "%s"
+			pi_snap_shot_name       = "%s"
+			pi_volume_ids         = [ibm_pi_volume.power_volume.volume_id]
+		}`, acc.Pi_cloud_instance_id, name)
 }
