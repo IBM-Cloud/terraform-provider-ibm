0bd379137425d96bf09b38c654123710ea38fa12
rangelil <riccardo.angelilli@it.ibm.com>
2024-07-04 10:40:11 +0200

    fix crash

--- ibm/service/project/resource_ibm_project.go
+++ ibm/service/project/resource_ibm_project.go
@@ -942,7 +942,9 @@ func ResourceIbmProjectProjectDefinitionPropertiesToMap(model *projectv1.Project
 	modelMap["name"] = *model.Name
 	modelMap["destroy_on_delete"] = *model.DestroyOnDelete
 	modelMap["description"] = *model.Description
-	modelMap["auto_deploy"] = *model.AutoDeploy
+	if model.AutoDeploy != nil {
+		modelMap["auto_deploy"] = *model.AutoDeploy
+	}
 	if model.MonitoringEnabled != nil {
 		modelMap["monitoring_enabled"] = *model.MonitoringEnabled
 	}
