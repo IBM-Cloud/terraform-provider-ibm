2f507a2467c5833f520598c1843495834d0c29db
Deepak Selvakumar <77007253+deepaksibm@users.noreply.github.com>
2024-07-18 17:24:24 +0530

    feat(bm-dynamic-bandwidth): Support for bandwidth in bare metal resource creation (#5493)
    * Update build.sh
    
    * sdk changes
    
    * Update resource_ibm_is_snapshot_consistency_group.go
    
    * vpn 113 changes
    
    * Update resource_ibm_is_vpn_gateway_connections.go
    
    * some changes
    
    * updated sdk
    
    * Added deprecation
    
    * made changes to sdk
    
    * Update resource_ibm_is_vpn_gateway_connections.go
    
    * Update resource_ibm_is_vpn_gateway_connections.go
    
    * Update resource_ibm_is_vpn_gateway_connections.go
    
    * Update is_vpn_gateway_connection.html.markdown
    
    * sdk update
    
    * sdk changes
    
    * set version to todays date in sdk
    
    * temporary fix for routing protocol in sdk.
    
    * error resolved
    
    * go mod changes reverted
    
    * removed sdk
    
    * Update go.sum
    
    * Update data_source_ibm_is_vpn_gateway_connection.go
    
    * fixed datasource errors
    
    * gateway fix
    
    * Update resource_ibm_is_vpn_gateway_connections.go
    
    * Update resource_ibm_is_vpn_gateway_connections.go
    
    * Update resource_ibm_is_vpn_gateway_connections.go
    
    * Update provider.go
    
    * Update resource_ibm_is_vpn_gateway_connections.go
    
    * fixed error on setting
    
    * fixed cidrs issue
    
    * Update resource_ibm_is_vpn_gateway_connections.go
    
    * Update resource_ibm_is_vpn_gateway_connection_test.go
    
    * Update resource_ibm_is_vpn_gateway_connection_test.go
    
    * feat(bm-dynamic-bandwidth): Support for bandwidth while creating bare metal server
    
    feat(bm-dynamic-bandwidth): Support for bandwidth while creating bare metal server
    
    * sdk fix
    
    * compile fix
    
    * tmp
    
    * sdk update
    
    * uncomment replica as spec is fixed
    
    * test case update
    
    * update test
    
    * update sdk
    
    * go mod changes
    
    * go mod update
    
    * go mod updates
    
    * test update
    
    * revert unintentional script changes
    
    ---------
    
    Co-authored-by: Ujjwal Kumar <Ujjwal.Kumar1@ibm.com>

--- ibm/service/vpc/resource_ibm_is_bare_metal_server_test.go
+++ ibm/service/vpc/resource_ibm_is_bare_metal_server_test.go
@@ -46,6 +46,48 @@ ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCKVmnMOlHKcZK8tpt3MP1lqOLAcqcJzhsvJcjscgVE
 		},
 	})
 }
+
+func TestAccIBMISBareMetalServer_bandwidth(t *testing.T) {
+	var server string
+	vpcname := fmt.Sprintf("tf-vpc-%d", acctest.RandIntRange(10, 100))
+	pipName := fmt.Sprintf("tf-vpc-pip-%d", acctest.RandIntRange(10, 100))
+	name := fmt.Sprintf("tf-server-%d", acctest.RandIntRange(10, 100))
+	subnetname := fmt.Sprintf("tfip-subnet-%d", acctest.RandIntRange(10, 100))
+	publicKey := strings.TrimSpace(`
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCKVmnMOlHKcZK8tpt3MP1lqOLAcqcJzhsvJcjscgVERRN7/9484SOBJ3HSKxxNG5JN8owAjy5f9yYwcUg+JaUVuytn5Pv3aeYROHGGg+5G346xaq3DAwX6Y5ykr2fvjObgncQBnuU5KHWCECO/4h8uWuwh/kfniXPVjFToc+gnkqA+3RKpAecZhFXwfalQ9mMuYGFxn+fwn8cYEApsJbsEmb0iJwPiZ5hjFC8wREuiTlhPHDgkBLOiycd20op2nXzDbHfCHInquEe/gYxEitALONxm0swBOwJZwlTDOB7C6y2dzlrtxr1L59m7pCkWI4EtTRLvleehBoj3u7jB4usR
+`)
+	sshname := fmt.Sprintf("tf-sshname-%d", acctest.RandIntRange(10, 100))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { acc.TestAccPreCheck(t) },
+		Providers:    acc.TestAccProviders,
+		CheckDestroy: testAccCheckIBMISBareMetalServerDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckIBMISBareMetalServerBandwidthConfig(vpcname, subnetname, sshname, publicKey, name, pipName, 10000),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckIBMISBareMetalServerExists("ibm_is_bare_metal_server.testacc_bms", server),
+					resource.TestCheckResourceAttr(
+						"ibm_is_bare_metal_server.testacc_bms", "name", name),
+					resource.TestCheckResourceAttr(
+						"ibm_is_bare_metal_server.testacc_bms", "zone", acc.ISZoneName),
+					resource.TestCheckResourceAttr(
+						"ibm_is_bare_metal_server.testacc_bms", "bandwidth", "10000"),
+				),
+			},
+			{
+				Config: testAccCheckIBMISBareMetalServerBandwidthConfig(vpcname, subnetname, sshname, publicKey, name, pipName, 25000),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckIBMISBareMetalServerExists("ibm_is_bare_metal_server.testacc_bms", server),
+					resource.TestCheckResourceAttr(
+						"ibm_is_bare_metal_server.testacc_bms", "name", name),
+					resource.TestCheckResourceAttr(
+						"ibm_is_bare_metal_server.testacc_bms", "bandwidth", "25000"),
+				),
+			},
+		},
+	})
+}
 func TestAccIBMISBareMetalServerVNI_basic(t *testing.T) {
 	var server string
 	vpcname := fmt.Sprintf("tf-vpc-%d", acctest.RandIntRange(10, 100))
@@ -486,6 +528,48 @@ func testAccCheckIBMISBareMetalServerConfig(vpcname, subnetname, sshname, public
 		}
 `, vpcname, subnetname, acc.ISZoneName, sshname, publicKey, acc.IsBareMetalServerProfileName, name, acc.IsBareMetalServerImage, acc.ISZoneName)
 }
+
+func testAccCheckIBMISBareMetalServerBandwidthConfig(vpcname, subnetname, sshname, publicKey, name, pipName string, bandwidth int) string {
+	return fmt.Sprintf(`
+		resource "ibm_is_vpc" "testacc_vpc" {
+			name = "%s"
+		}
+	  
+		resource "ibm_is_subnet" "testacc_subnet" {
+			name            			= "%s"
+			vpc             			= ibm_is_vpc.testacc_vpc.id
+			zone            			= "%s"
+			total_ipv4_address_count 	= 16
+		}
+	  
+		resource "ibm_is_ssh_key" "testacc_sshkey" {
+			name       			= "%s"
+			public_key 			= "%s"
+		}
+	  
+		resource "ibm_is_bare_metal_server" "testacc_bms" {
+			bandwidth = %d
+			profile 			= "%s"
+			name 				= "%s"
+			image 				= "%s"
+			zone 				= "%s"
+			keys 				= [ibm_is_ssh_key.testacc_sshkey.id]
+			primary_network_attachment {
+				virtual_network_interface { 
+						auto_delete = true
+						enable_infrastructure_nat = true
+						primary_ip {
+							name = "%s"
+						}
+						subnet = ibm_is_subnet.testacc_subnet.id
+				}
+				allowed_vlans = [100, 102]
+			}
+			vpc 				= ibm_is_vpc.testacc_vpc.id
+		}
+`, vpcname, subnetname, acc.ISZoneName, sshname, publicKey, bandwidth, acc.IsBareMetalServerProfileName, name, acc.IsBareMetalServerImage, acc.ISZoneName, pipName)
+}
+
 func testAccCheckIBMISBareMetalServerVNIConfig(vpcname, subnetname, sshname, publicKey, vniname, name string) string {
 	return fmt.Sprintf(`
 		resource "ibm_is_vpc" "testacc_vpc" {
