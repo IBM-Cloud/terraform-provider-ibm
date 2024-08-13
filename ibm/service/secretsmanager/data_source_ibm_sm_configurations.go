4670c9171733d081e8478b541e7b0043814fae86
Yonathan-Yellin <yonathan.yellin@ibm.com>
2024-08-13 17:59:54 +0300

    rejections fixes

--- ibm/service/secretsmanager/data_source_ibm_sm_configurations.go
+++ ibm/service/secretsmanager/data_source_ibm_sm_configurations.go
@@ -141,7 +141,6 @@ func DataSourceIbmSmConfigurations() *schema.Resource {
 						"crypto_key": &schema.Schema{
 							Type:        schema.TypeList,
 							MaxItems:    1,
-							Optional:    true,
 							Computed:    true,
 							Description: "The data that is associated with a cryptographic key.",
 							Elem: &schema.Resource{
