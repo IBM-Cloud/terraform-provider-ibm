205cf7a5ac752cf7bff854d6a8305a5e45c3c59e
IBM-Deeksha <deeksha.sharma8@ibm.com>
2024-09-17 16:40:18 +0530

    Add test bucket endpoints (#5636)
    * add changes for test bucket endpoints
    
    * update code
    
    ---------
    
    Co-authored-by: Deeksha Sharma <deekshasharma@Deekshas-MacBook-Pro.local>

--- ibm/service/cos/resource_ibm_cos_replication_configuration.go
+++ ibm/service/cos/resource_ibm_cos_replication_configuration.go
@@ -334,19 +334,25 @@ func parseBucketReplId(id string, info string) string {
 	return parseBucketId(bucketCRN, info)
 }
 
-func getCosEndpointType(bucketLocation string, endpointType string) string {
+func getCosEndpointType(bucketLocation string, endpointType string, test bool) string {
+
 	if bucketLocation != "" {
+		hostUrl := "cloud-object-storage.appdomain.cloud"
+		if test {
+			hostUrl = "cloud-object-storage.test.appdomain.cloud"
+		}
 		switch endpointType {
 		case "public":
-			return fmt.Sprintf("s3.%s.cloud-object-storage.appdomain.cloud", bucketLocation)
+			return fmt.Sprintf("s3.%s.%s", bucketLocation, hostUrl)
 		case "private":
-			return fmt.Sprintf("s3.private.%s.cloud-object-storage.appdomain.cloud", bucketLocation)
+			return fmt.Sprintf("s3.private.%s.%s", bucketLocation, hostUrl)
 		case "direct":
-			return fmt.Sprintf("s3.direct.%s.cloud-object-storage.appdomain.cloud", bucketLocation)
+			return fmt.Sprintf("s3.direct.%s.%s", bucketLocation, hostUrl)
 		default:
-			return fmt.Sprintf("s3.%s.cloud-object-storage.appdomain.cloud", bucketLocation)
+			return fmt.Sprintf("s3.%s.%s", bucketLocation, hostUrl)
 		}
 	}
+
 	return ""
 }
 
@@ -357,8 +363,7 @@ func getS3ClientSession(bxSession *bxsession.Session, bucketLocation string, end
 	if endpointType == "direct" {
 		visibility = "private"
 	}
-
-	apiEndpoint := getCosEndpointType(bucketLocation, endpointType)
+	apiEndpoint := getCosEndpointType(bucketLocation, endpointType, false)
 	apiEndpoint = conns.FileFallBack(bxSession.Config.EndpointsFile, visibility, "IBMCLOUD_COS_ENDPOINT", bucketLocation, apiEndpoint)
 	apiEndpoint = conns.EnvFallBack([]string{"IBMCLOUD_COS_ENDPOINT"}, apiEndpoint)
 	if apiEndpoint == "" {
