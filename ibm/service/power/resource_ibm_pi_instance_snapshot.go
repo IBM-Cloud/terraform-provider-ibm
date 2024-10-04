52a8928e1baf547f6ccd21e81cb91ed31ccb6d7a
Axel Ismirlian <ismirlia@us.ibm.com>
2024-10-04 11:19:18 -0500

    Remove extra length check in user tags field

--- ibm/service/power/resource_ibm_pi_instance_snapshot.go
+++ ibm/service/power/resource_ibm_pi_instance_snapshot.go
@@ -136,9 +136,7 @@ func resourceIBMPIInstanceSnapshotCreate(ctx context.Context, d *schema.Resource
 	}
 
 	if v, ok := d.GetOk(Arg_UserTags); ok {
-		if len(flex.FlattenSet(v.(*schema.Set))) > 0 {
-			snapshotBody.UserTags = flex.FlattenSet(v.(*schema.Set))
-		}
+		snapshotBody.UserTags = flex.FlattenSet(v.(*schema.Set))
 	}
 
 	snapshotResponse, err := client.CreatePvmSnapShot(instanceid, snapshotBody)
