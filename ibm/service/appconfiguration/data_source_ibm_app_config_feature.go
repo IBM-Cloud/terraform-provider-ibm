7fe9a6f3813f1acd7f81dcad0fb938b206c2f6db
Ramya C <Ramya.C4@ibm.com>
2024-10-29 16:08:55 +0530

    review

--- ibm/service/appconfiguration/data_source_ibm_app_config_feature.go
+++ ibm/service/appconfiguration/data_source_ibm_app_config_feature.go
@@ -126,11 +126,6 @@ func DataSourceIBMAppConfigFeature() *schema.Resource {
 							Computed:    true,
 							Description: "Rollout percentage for the segment rule.",
 						},
-						"format": {
-							Type:        schema.TypeString,
-							Computed:    true,
-							Description: "Format of the feature (TEXT, JSON, YAML) and it is a required attribute when `type` is `STRING`. It is not required for `BOOLEAN` and `NUMERIC` types. This property is populated in the response body of `POST, PUT and GET` calls if the type `STRING` is used and not populated for `BOOLEAN` and `NUMERIC` types.",
-						},
 					},
 				},
 			},
