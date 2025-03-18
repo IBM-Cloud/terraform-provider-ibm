7846da93e57e450959bd339dcf209adfc20b3c75
Arjun Chauhan <arjun.chauhan@ibm.com>
2025-03-18 18:37:28 +0530

    fix ruleset_rule test

--- ibm/service/cis/resource_ibm_cis_ruleset_rule_test.go
+++ ibm/service/cis/resource_ibm_cis_ruleset_rule_test.go
@@ -13,16 +13,17 @@ import (
 )
 
 func TestAccIBMCISRulesetRule_Basic(t *testing.T) {
-	name := "data.ibm_cis_ruleset_rule.test"
+	name := "ibm_cis_ruleset_rule.test"
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { acc.TestAccPreCheckCis(t) },
 		Providers: acc.TestAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCheckCisAlertsDataSource_basic("test", acc.CisDomainStatic),
+				Config: testAccCheckCisRulesetsRule_basic("test", acc.CisDomainStatic),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(name, "ruleset_id", "dcdec3fe0cbe41edac08619503da8de5"),
+					resource.TestCheckResourceAttr(name, "ruleset_id", "eb5efc50d5ec49d8b0b0f44b357b8d7b"),
+					resource.TestCheckResourceAttr(name, "rule.#", "1"),
 				),
 			},
 		},
@@ -31,17 +32,17 @@ func TestAccIBMCISRulesetRule_Basic(t *testing.T) {
 
 func testAccCheckCisRulesetsRule_basic(id, CisDomainStatic string) string {
 	return testAccCheckIBMCisDomainDataSourceConfigBasic1() + fmt.Sprintf(`
+
 	resource "ibm_cis_ruleset_rule" "%[1]s" {
 		cis_id = data.ibm_cis.cis.id
 		domain_id = data.ibm_cis_domain.cis_domain.domain_id
-		ruleset_id = "dcdec3fe0cbe41edac08619503da8de5"
-		rules {
-			{
+		ruleset_id = "eb5efc50d5ec49d8b0b0f44b357b8d7b"
+			rule {
 			  action =  "block"
-			  description = "Manually created rule"
+			  description = "Testing rule creation"
 			  enabled = true
+			  expression = "true"
 			}
-		}
 	  }
 `, id, acc.CisDomainStatic)
 }
