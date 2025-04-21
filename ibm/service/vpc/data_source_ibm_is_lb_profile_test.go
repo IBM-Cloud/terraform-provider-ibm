a69957a0a92a6055182a410e00fe5ac6dce9c7fc
keerthigp <keerthi.gp@ibm.com>
2025-04-21 13:08:46 +0530

    enhancement(is_lb_pool_member): Fix test

--- ibm/service/vpc/data_source_ibm_is_lb_profile_test.go
+++ ibm/service/vpc/data_source_ibm_is_lb_profile_test.go
@@ -27,7 +27,7 @@ func TestAccIBMISLBProfileDatasource_basic(t *testing.T) {
 					resource.TestCheckResourceAttrSet("data.ibm_is_lb_profile.test_profile", "href"),
 					resource.TestCheckResourceAttrSet("data.ibm_is_lb_profile.test_profile", "udp_supported"),
 					resource.TestCheckResourceAttrSet("data.ibm_is_lb_profile.test_profile", "access_modes.0.values.#"),
-					resource.TestCheckResourceAttrSet("data.ibm_is_lb_profiles.test_profiles", "lb_profiles.0.targetable_load_balancer_profiles.#"),
+					resource.TestCheckResourceAttrSet("data.ibm_is_lb_profile.test_profile", "targetable_load_balancer_profiles.#"),
 				),
 			},
 		},
