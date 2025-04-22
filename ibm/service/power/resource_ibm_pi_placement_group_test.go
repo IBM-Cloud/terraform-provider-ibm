c08bace700f4ac5d858766c345a1e5ab552630e2
ismirlia <90468712+ismirlia@users.noreply.github.com>
2025-04-22 08:26:41 -0500

    Fix warnings in pi data source and resources (#6159)
    * Fix warnings in pi data source and resources
    
    * Update placement group test to use PI_SAP_PROFILE_ID

--- ibm/service/power/resource_ibm_pi_placement_group_test.go
+++ ibm/service/power/resource_ibm_pi_placement_group_test.go
@@ -65,7 +65,7 @@ func TestAccIBMPIPlacementGroupBasic(t *testing.T) {
 				),
 			},
 			{
-				Config: testAccCheckIBMPICreateInstanceInPlacementGroup(name, policy, "tinytest-1x4"),
+				Config: testAccCheckIBMPICreateInstanceInPlacementGroup(name, policy),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttrSet(
 						"ibm_pi_instance.power_instance", "pi_placement_group_id"),
@@ -79,9 +79,9 @@ func TestAccIBMPIPlacementGroupBasic(t *testing.T) {
 				ExpectNonEmptyPlan: true,
 			},
 			{
-				Config: testAccCheckIBMPIDeletePlacementGroup(name, policy, "tinytest-1x4"),
+				Config: testAccCheckIBMPIDeletePlacementGroup(name, policy),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckIBMPIPlacementGroupDelete("ibm_pi_placement_group.power_placement_group", "ibm_pi_instance.power_instance", "ibm_pi_instance.power_instance_in_pg"),
+					testAccCheckIBMPIPlacementGroupDelete("ibm_pi_instance.power_instance", "ibm_pi_instance.power_instance_in_pg"),
 				),
 			},
 		},
@@ -304,7 +304,7 @@ func testAccCheckIBMPIPlacementGroupMemberExistsFromInstanceCreate(n string, ins
 	}
 }
 
-func testAccCheckIBMPIPlacementGroupDelete(n string, inst string, newInstance string) resource.TestCheckFunc {
+func testAccCheckIBMPIPlacementGroupDelete(inst string, newInstance string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		sess, err := acc.TestAccProvider.Meta().(conns.ClientSession).IBMPISession()
 		if err != nil {
@@ -484,7 +484,7 @@ func testAccCheckIBMPIPlacementGroupRemoveMemberConfig(name string, policy strin
 		}`, acc.Pi_cloud_instance_id, name, policy, acc.Pi_image, acc.Pi_network_name)
 }
 
-func testAccCheckIBMPICreateInstanceInPlacementGroup(name string, policy string, sapProfile string) string {
+func testAccCheckIBMPICreateInstanceInPlacementGroup(name string, policy string) string {
 	return fmt.Sprintf(`
 		resource "ibm_pi_key" "key" {
 			pi_cloud_instance_id = "%[1]s"
@@ -546,10 +546,10 @@ func testAccCheckIBMPICreateInstanceInPlacementGroup(name string, policy string,
 			pi_cloud_instance_id      = "%[1]s"
 			pi_placement_group_name   = "%[2]s-2"
 			pi_placement_group_policy = "%[3]s"
-		}`, acc.Pi_cloud_instance_id, name, policy, acc.Pi_image, sapProfile, acc.Pi_sap_image, acc.Pi_network_name)
+		}`, acc.Pi_cloud_instance_id, name, policy, acc.Pi_image, acc.Pi_sap_profile_id, acc.Pi_sap_image, acc.Pi_network_name)
 }
 
-func testAccCheckIBMPIDeletePlacementGroup(name string, policy string, sapProfile string) string {
+func testAccCheckIBMPIDeletePlacementGroup(name string, policy string) string {
 	return fmt.Sprintf(`
 		resource "ibm_pi_key" "key" {
 			pi_cloud_instance_id = "%[1]s"
@@ -608,5 +608,5 @@ func testAccCheckIBMPIDeletePlacementGroup(name string, policy string, sapProfil
 			pi_cloud_instance_id      = "%[1]s"
 			pi_placement_group_name   = "%[2]s-2"
 			pi_placement_group_policy = "%[3]s"
-		}`, acc.Pi_cloud_instance_id, name, policy, acc.Pi_image, sapProfile, acc.Pi_sap_image, acc.Pi_network_name)
+		}`, acc.Pi_cloud_instance_id, name, policy, acc.Pi_image, acc.Pi_sap_profile_id, acc.Pi_sap_image, acc.Pi_network_name)
 }
