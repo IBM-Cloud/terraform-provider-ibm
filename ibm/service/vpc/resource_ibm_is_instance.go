8a69bebe0c6fb1e96e218a4874596b2713d2fe42
Ujjwal Kumar <ujjwal.kumar1@ibm.com>
2025-06-16 11:37:26 +0530

    Added crn to virtual network interface for is_instance

--- ibm/service/vpc/resource_ibm_is_instance.go
+++ ibm/service/vpc/resource_ibm_is_instance.go
@@ -1098,6 +1098,11 @@ func ResourceIBMISInstance() *schema.Resource {
 										Computed:    true,
 										Description: "The resource type.",
 									},
+									"crn": &schema.Schema{
+										Type:        schema.TypeString,
+										Computed:    true,
+										Description: "The crn of the virtual network interface.",
+									},
 									"security_groups": {
 										Type:          schema.TypeSet,
 										Optional:      true,
@@ -1484,6 +1489,11 @@ func ResourceIBMISInstance() *schema.Resource {
 										Type:        schema.TypeString,
 										Computed:    true,
 										Description: "The resource type.",
+									},
+									"crn": &schema.Schema{
+										Type:        schema.TypeString,
+										Computed:    true,
+										Description: "The crn of the virtual network interface.",
 									},
 									"security_groups": {
 										Type:        schema.TypeSet,
@@ -7937,6 +7947,7 @@ func resourceIBMIsInstanceInstanceNetworkAttachmentReferenceToMap(model *vpcv1.I
 	vniMap := make(map[string]interface{})
 	if pna.VirtualNetworkInterface != nil {
 		vniMap["id"] = *pna.VirtualNetworkInterface.ID
+		vniMap["crn"] = *pna.VirtualNetworkInterface.CRN
 		vniMap["name"] = pna.VirtualNetworkInterface.Name
 		vniMap["resource_type"] = pna.VirtualNetworkInterface.ResourceType
 	}
