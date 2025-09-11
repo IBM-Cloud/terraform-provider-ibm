3fa254eef8c5bcfb6a82ea16cb627c975e0509d9
Jay Bhensdadia <jayvpatel29@gmail.com>
2025-09-11 18:10:20 +0530

    add schema in datasource and update the datasource related doc

--- ibm/service/cis/data_source_ibm_cis_rulesets.go
+++ ibm/service/cis/data_source_ibm_cis_rulesets.go
@@ -272,6 +272,28 @@ var CISResponseObject = &schema.Resource{
 										},
 									},
 								},
+								CISRulesToSkip: {
+									Type:        schema.TypeList,
+									Computed:    true,
+									Description: "A list of ruleset mappings, where each element is a map of ruleset_id and its associated rule_ids",
+									Elem: &schema.Resource{
+										Schema: map[string]*schema.Schema{
+											"ruleset_id": {
+												Type:        schema.TypeString,
+												Computed:    true,
+												Description: "The ruleset identifier",
+											},
+											"rule_ids": {
+												Type:        schema.TypeList,
+												Computed:    true,
+												Description: "A list of rule IDs to be skipped",
+												Elem: &schema.Schema{
+													Type: schema.TypeString,
+												},
+											},
+										},
+									},
+								},
 							},
 						},
 					},
