b2d21514df0331c1b3c4e9a911284dedf6f56368
Axel Ismirlian <ismirlia@us.ibm.com>
2025-10-21 17:02:27 -0500

    Update error handling on route data source

--- ibm/service/power/data_source_ibm_pi_route.go
+++ ibm/service/power/data_source_ibm_pi_route.go
@@ -5,6 +5,7 @@ package power
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"github.com/IBM-Cloud/power-go-client/clients/instance"
@@ -95,10 +96,12 @@ func DataSourceIBMPIRoute() *schema.Resource {
 	}
 }
 
-func dataSourceIBMPIRouteRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+func dataSourceIBMPIRouteRead(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
 	sess, err := meta.(conns.ClientSession).IBMPISession()
 	if err != nil {
-		return diag.FromErr(err)
+		tfErr := flex.TerraformErrorf(err, fmt.Sprintf("IBMPISession failed: %s", err.Error()), "(Data) ibm_pi_route", "read")
+		log.Printf("[DEBUG]\n%s", tfErr.GetDebugMessage())
+		return tfErr.GetDiag()
 	}
 
 	cloudInstanceID := d.Get(Arg_CloudInstanceID).(string)
@@ -107,7 +110,9 @@ func dataSourceIBMPIRouteRead(ctx context.Context, d *schema.ResourceData, meta 
 
 	route, err := client.Get(routeID)
 	if err != nil {
-		return diag.FromErr(err)
+		tfErr := flex.TerraformErrorf(err, fmt.Sprintf("Get failed: %s", err.Error()), "(Data) ibm_pi_route", "read")
+		log.Printf("[DEBUG]\n%s", tfErr.GetDebugMessage())
+		return tfErr.GetDiag()
 	}
 
 	d.Set(Attr_Action, route.Action)
