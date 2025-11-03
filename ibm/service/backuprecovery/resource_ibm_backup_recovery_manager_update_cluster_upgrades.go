c291fd653e2dc665bc8ab2f6142f4da38e5d50b0
Deeksha Sharma <deeksha.sharma8@ibm.com>
2025-11-03 21:57:26 +0530

    add resources

--- ibm/service/backuprecovery/resource_ibm_backup_recovery_manager_update_cluster_upgrades.go
+++ ibm/service/backuprecovery/resource_ibm_backup_recovery_manager_update_cluster_upgrades.go
@@ -22,11 +22,11 @@ import (
 	"github.com/IBM/ibm-backup-recovery-sdk-go/backuprecoveryv1"
 )
 
-func ResourceIbmBackupRecoveryManagerCreateClusterUpgrades() *schema.Resource {
+func ResourceIbmBackupRecoveryManagerUpdateClusterUpgrades() *schema.Resource {
 	return &schema.Resource{
-		CreateContext: resourceIbmBackupRecoveryManagerCreateClusterUpgradesCreate,
-		ReadContext:   resourceIbmBackupRecoveryManagerCreateClusterUpgradesRead,
-		DeleteContext: resourceIbmBackupRecoveryManagerCreateClusterUpgradesDelete,
+		CreateContext: resourceIbmBackupRecoveryManagerUpdateClusterUpgradesCreate,
+		ReadContext:   resourceIbmBackupRecoveryManagerUpdateClusterUpgradesRead,
+		DeleteContext: resourceIbmBackupRecoveryManagerUpdateClusterUpgradesDelete,
 		Importer:      &schema.ResourceImporter{},
 
 		Schema: map[string]*schema.Schema{
@@ -189,91 +189,91 @@ func ResourceIbmBackupRecoveryManagerCreateClusterUpgrades() *schema.Resource {
 	}
 }
 
-func resourceIbmBackupRecoveryManagerCreateClusterUpgradesCreate(context context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+func resourceIbmBackupRecoveryManagerUpdateClusterUpgradesCreate(context context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	managementSreApiClient, err := meta.(conns.ClientSession).BackupRecoveryManagerV1()
 	if err != nil {
-		tfErr := flex.DiscriminatedTerraformErrorf(err, err.Error(), "ibm_backup_recovery_manager_create_cluster_upgrades", "create", "initialize-client")
+		tfErr := flex.DiscriminatedTerraformErrorf(err, err.Error(), "ibm_backup_recovery_manager_update_cluster_upgrades", "create", "initialize-client")
 		log.Printf("[DEBUG]\n%s", tfErr.GetDebugMessage())
 		return tfErr.GetDiag()
 	}
 
-	createClustersUpgradesOptions := &backuprecoveryv1.CreateClustersUpgradesOptions{}
+	updateClustersUpgradesOptions := &backuprecoveryv1.UpdateClustersUpgradesOptions{}
 
 	if _, ok := d.GetOk("auth_headers"); ok {
 		var authHeaders []backuprecoveryv1.AuthHeaderForClusterUpgrade
 		for _, v := range d.Get("auth_headers").([]interface{}) {
 			value := v.(map[string]interface{})
-			authHeadersItem, err := ResourceIbmBackupRecoveryManagerCreateClusterUpgradesMapToAuthHeaderForClusterUpgrade(value)
+			authHeadersItem, err := ResourceIbmBackupRecoveryManagerUpdateClusterUpgradesMapToAuthHeaderForClusterUpgrade(value)
 			if err != nil {
-				return flex.DiscriminatedTerraformErrorf(err, err.Error(), "ibm_backup_recovery_manager_create_cluster_upgrades", "create", "parse-auth_headers").GetDiag()
+				return flex.DiscriminatedTerraformErrorf(err, err.Error(), "ibm_backup_recovery_manager_update_cluster_upgrades", "create", "parse-auth_headers").GetDiag()
 			}
 			authHeaders = append(authHeaders, *authHeadersItem)
 		}
-		createClustersUpgradesOptions.SetAuthHeaders(authHeaders)
+		updateClustersUpgradesOptions.SetAuthHeaders(authHeaders)
 	}
 	if _, ok := d.GetOk("clusters"); ok {
 		var clusters []backuprecoveryv1.Upgrade
 		for _, v := range d.Get("clusters").([]interface{}) {
 			value := v.(map[string]interface{})
-			clustersItem, err := ResourceIbmBackupRecoveryManagerCreateClusterUpgradesMapToUpgrade(value)
+			clustersItem, err := ResourceIbmBackupRecoveryManagerUpdateClusterUpgradesMapToUpgrade(value)
 			if err != nil {
-				return flex.DiscriminatedTerraformErrorf(err, err.Error(), "ibm_backup_recovery_manager_create_cluster_upgrades", "create", "parse-clusters").GetDiag()
+				return flex.DiscriminatedTerraformErrorf(err, err.Error(), "ibm_backup_recovery_manager_update_cluster_upgrades", "create", "parse-clusters").GetDiag()
 			}
 			clusters = append(clusters, *clustersItem)
 		}
-		createClustersUpgradesOptions.SetClusters(clusters)
+		updateClustersUpgradesOptions.SetClusters(clusters)
 	}
 	if _, ok := d.GetOk("interval_for_rolling_upgrade_in_hours"); ok {
-		createClustersUpgradesOptions.SetIntervalForRollingUpgradeInHours(int64(d.Get("interval_for_rolling_upgrade_in_hours").(int)))
+		updateClustersUpgradesOptions.SetIntervalForRollingUpgradeInHours(int64(d.Get("interval_for_rolling_upgrade_in_hours").(int)))
 	}
 	if _, ok := d.GetOk("package_url"); ok {
-		createClustersUpgradesOptions.SetPackageURL(d.Get("package_url").(string))
+		updateClustersUpgradesOptions.SetPackageURL(d.Get("package_url").(string))
 	}
 	if _, ok := d.GetOk("patch_upgrade_params"); ok {
-		patchUpgradeParamsModel, err := ResourceIbmBackupRecoveryManagerCreateClusterUpgradesMapToPatchUpgradeParams(d.Get("patch_upgrade_params.0").(map[string]interface{}))
+		patchUpgradeParamsModel, err := ResourceIbmBackupRecoveryManagerUpdateClusterUpgradesMapToPatchUpgradeParams(d.Get("patch_upgrade_params.0").(map[string]interface{}))
 		if err != nil {
-			return flex.DiscriminatedTerraformErrorf(err, err.Error(), "ibm_backup_recovery_manager_create_cluster_upgrades", "create", "parse-patch_upgrade_params").GetDiag()
+			return flex.DiscriminatedTerraformErrorf(err, err.Error(), "ibm_backup_recovery_manager_update_cluster_upgrades", "create", "parse-patch_upgrade_params").GetDiag()
 		}
-		createClustersUpgradesOptions.SetPatchUpgradeParams(patchUpgradeParamsModel)
+		updateClustersUpgradesOptions.SetPatchUpgradeParams(patchUpgradeParamsModel)
 	}
 	if _, ok := d.GetOk("target_version"); ok {
-		createClustersUpgradesOptions.SetTargetVersion(d.Get("target_version").(string))
+		updateClustersUpgradesOptions.SetTargetVersion(d.Get("target_version").(string))
 	}
 	if _, ok := d.GetOk("time_stamp_to_upgrade_at_msecs"); ok {
-		createClustersUpgradesOptions.SetTimeStampToUpgradeAtMsecs(int64(d.Get("time_stamp_to_upgrade_at_msecs").(int)))
+		updateClustersUpgradesOptions.SetTimeStampToUpgradeAtMsecs(int64(d.Get("time_stamp_to_upgrade_at_msecs").(int)))
 	}
 	if _, ok := d.GetOk("type"); ok {
-		createClustersUpgradesOptions.SetType(d.Get("type").(string))
+		updateClustersUpgradesOptions.SetType(d.Get("type").(string))
 	}
 
-	upgradesResponse, _, err := managementSreApiClient.CreateClustersUpgradesWithContext(context, createClustersUpgradesOptions)
+	upgradesResponse, _, err := managementSreApiClient.UpdateClustersUpgradesWithContext(context, updateClustersUpgradesOptions)
 	if err != nil {
-		tfErr := flex.TerraformErrorf(err, fmt.Sprintf("CreateClustersUpgradesWithContext failed: %s", err.Error()), "ibm_backup_recovery_manager_create_cluster_upgrades", "create")
+		tfErr := flex.TerraformErrorf(err, fmt.Sprintf("UpdateClustersUpgradesWithContext failed: %s", err.Error()), "ibm_backup_recovery_manager_update_cluster_upgrades", "create")
 		log.Printf("[DEBUG]\n%s", tfErr.GetDebugMessage())
 		return tfErr.GetDiag()
 	}
 
-	d.SetId(resourceIbmBackupRecoveryManagerCreateClusterUpgradesID(d))
+	d.SetId(resourceIbmBackupRecoveryManagerUpdateClusterUpgradesID(d))
 
 	if !core.IsNil(upgradesResponse) {
 		upgradeResponseListResult := []map[string]interface{}{}
 		for _, upgradeResponseListItem := range upgradesResponse {
-			upgradeResponseListItemMap, err := ResourceIbmBackupRecoveryManagerCreateClusterUpgradesUpgradeResponseToMap(&upgradeResponseListItem) // #nosec G601
+			upgradeResponseListItemMap, err := ResourceIbmBackupRecoveryManagerUpdateClusterUpgradesUpgradeResponseToMap(&upgradeResponseListItem) // #nosec G601
 			if err != nil {
-				return flex.DiscriminatedTerraformErrorf(err, err.Error(), "ibm_backup_recovery_manager_create_cluster_upgrades", "read", "upgrade_response_list-to-map").GetDiag()
+				return flex.DiscriminatedTerraformErrorf(err, err.Error(), "ibm_backup_recovery_manager_update_cluster_upgrades", "read", "upgrade_response_list-to-map").GetDiag()
 			}
 			upgradeResponseListResult = append(upgradeResponseListResult, upgradeResponseListItemMap)
 		}
 		if err = d.Set("upgrade_response_list", upgradeResponseListResult); err != nil {
 			err = fmt.Errorf("Error setting upgrade_response_list: %s", err)
-			return flex.DiscriminatedTerraformErrorf(err, err.Error(), "ibm_backup_recovery_manager_create_cluster_upgrades", "read", "set-upgrade_response_list").GetDiag()
+			return flex.DiscriminatedTerraformErrorf(err, err.Error(), "ibm_backup_recovery_manager_update_cluster_upgrades", "read", "set-upgrade_response_list").GetDiag()
 		}
 	}
 
-	return resourceIbmBackupRecoveryManagerCreateClusterUpgradesRead(context, d, meta)
+	return resourceIbmBackupRecoveryManagerUpdateClusterUpgradesRead(context, d, meta)
 }
 
-func ResourceIbmBackupRecoveryManagerCreateClusterUpgradesUpgradeResponseToMap(model *backuprecoveryv1.UpgradeResponse) (map[string]interface{}, error) {
+func ResourceIbmBackupRecoveryManagerUpdateClusterUpgradesUpgradeResponseToMap(model *backuprecoveryv1.UpgradeResponse) (map[string]interface{}, error) {
 	modelMap := make(map[string]interface{})
 	if model.ErrorMessage != nil {
 		modelMap["error_message"] = *model.ErrorMessage
@@ -290,15 +290,15 @@ func ResourceIbmBackupRecoveryManagerCreateClusterUpgradesUpgradeResponseToMap(m
 	return modelMap, nil
 }
 
-func resourceIbmBackupRecoveryManagerCreateClusterUpgradesID(d *schema.ResourceData) string {
+func resourceIbmBackupRecoveryManagerUpdateClusterUpgradesID(d *schema.ResourceData) string {
 	return time.Now().UTC().String()
 }
 
-func resourceIbmBackupRecoveryManagerCreateClusterUpgradesRead(context context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+func resourceIbmBackupRecoveryManagerUpdateClusterUpgradesRead(context context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	return nil
 }
 
-func resourceIbmBackupRecoveryManagerCreateClusterUpgradesDelete(context context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+func resourceIbmBackupRecoveryManagerUpdateClusterUpgradesDelete(context context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	// This resource does not support a "delete" operation.
 	var diags diag.Diagnostics
 	warning := diag.Diagnostic{
@@ -311,14 +311,14 @@ func resourceIbmBackupRecoveryManagerCreateClusterUpgradesDelete(context context
 	return diags
 }
 
-func ResourceIbmBackupRecoveryManagerCreateClusterUpgradesMapToAuthHeaderForClusterUpgrade(modelMap map[string]interface{}) (*backuprecoveryv1.AuthHeaderForClusterUpgrade, error) {
+func ResourceIbmBackupRecoveryManagerUpdateClusterUpgradesMapToAuthHeaderForClusterUpgrade(modelMap map[string]interface{}) (*backuprecoveryv1.AuthHeaderForClusterUpgrade, error) {
 	model := &backuprecoveryv1.AuthHeaderForClusterUpgrade{}
 	model.Key = core.StringPtr(modelMap["key"].(string))
 	model.Value = core.StringPtr(modelMap["value"].(string))
 	return model, nil
 }
 
-func ResourceIbmBackupRecoveryManagerCreateClusterUpgradesMapToUpgrade(modelMap map[string]interface{}) (*backuprecoveryv1.Upgrade, error) {
+func ResourceIbmBackupRecoveryManagerUpdateClusterUpgradesMapToUpgrade(modelMap map[string]interface{}) (*backuprecoveryv1.Upgrade, error) {
 	model := &backuprecoveryv1.Upgrade{}
 	if modelMap["cluster_id"] != nil {
 		model.ClusterID = core.Int64Ptr(int64(modelMap["cluster_id"].(int)))
@@ -332,12 +332,12 @@ func ResourceIbmBackupRecoveryManagerCreateClusterUpgradesMapToUpgrade(modelMap 
 	return model, nil
 }
 
-func ResourceIbmBackupRecoveryManagerCreateClusterUpgradesMapToPatchUpgradeParams(modelMap map[string]interface{}) (*backuprecoveryv1.PatchUpgradeParams, error) {
+func ResourceIbmBackupRecoveryManagerUpdateClusterUpgradesMapToPatchUpgradeParams(modelMap map[string]interface{}) (*backuprecoveryv1.PatchUpgradeParams, error) {
 	model := &backuprecoveryv1.PatchUpgradeParams{}
 	if modelMap["auth_headers"] != nil {
 		authHeaders := []backuprecoveryv1.AuthHeaderForClusterUpgrade{}
 		for _, authHeadersItem := range modelMap["auth_headers"].([]interface{}) {
-			authHeadersItemModel, err := ResourceIbmBackupRecoveryManagerCreateClusterUpgradesMapToAuthHeaderForClusterUpgrade(authHeadersItem.(map[string]interface{}))
+			authHeadersItemModel, err := ResourceIbmBackupRecoveryManagerUpdateClusterUpgradesMapToAuthHeaderForClusterUpgrade(authHeadersItem.(map[string]interface{}))
 			if err != nil {
 				return model, err
 			}
