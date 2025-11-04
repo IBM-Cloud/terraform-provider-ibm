691de9be44276762765b511fcd8950e26e74f2bf
Rushil Patange <Rushil.Patange@ibm.com>
2025-11-04 14:04:06 -0600

    test(power): add acceptance tests for networks DS
    Cover id composition and presence of key attributes (network_id, network_interface_id, ip_address).

---
+++ ibm/service/power/data_source_ibm_pi_instance_network_test.go
@@ -0,0 +1,38 @@
+// Copyright IBM Corp.
+// Licensed under the Mozilla Public License v2.0
+
+package power_test
+
+import (
+	"fmt"
+	"testing"
+
+	acc "github.com/IBM-Cloud/terraform-provider-ibm/ibm/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccIBMPIInstanceNetworkDataSource_basic(t *testing.T) {
+	ds := "data.ibm_pi_instance_network.testacc_ds_instance_network"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { acc.TestAccPreCheck(t) },
+		Providers: acc.TestAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckIBMPIInstanceNetworkDataSourceConfig(),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(ds, "id"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckIBMPIInstanceNetworkDataSourceConfig() string {
+	return fmt.Sprintf(`
+		data "ibm_pi_instance_network" "testacc_ds_instance_network" {
+  			pi_cloud_instance_id = "%[1]s"
+  			pi_instance_id       = "%[2]s"
+  			pi_network_id        = "%[3]s"
+		}`, acc.Pi_cloud_instance_id, acc.Pi_instance_name, acc.Pi_network_id)
+}
---
+++ ibm/service/power/data_source_ibm_pi_instance_networks_test.go
@@ -0,0 +1,37 @@
+// Copyright IBM Corp.
+// Licensed under the Mozilla Public License v2.0
+
+package power_test
+
+import (
+	"fmt"
+	"testing"
+
+	acc "github.com/IBM-Cloud/terraform-provider-ibm/ibm/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccIBMPIInstanceNetworksDataSource_basic(t *testing.T) {
+	ds := "data.ibm_pi_instance_networks.testacc_ds_instance_networks"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { acc.TestAccPreCheck(t) },
+		Providers: acc.TestAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckIBMPIInstanceNetworksDataSourceConfig(),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(ds, "id"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckIBMPIInstanceNetworksDataSourceConfig() string {
+	return fmt.Sprintf(`
+		data "ibm_pi_instance_networks" "testacc_ds_instance_networks" {
+			pi_cloud_instance_id = "%s"
+  			pi_instance_id       = "%s"
+		}`, acc.Pi_cloud_instance_id, acc.Pi_instance_name)
+}
