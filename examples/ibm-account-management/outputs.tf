c86e58bbf1798638e8647f4ef3c2e6b5e5a5d2d7
David <David.Lim0305@ibm.com>
2026-03-16 00:17:32 -0500

    feat: Add IBM Account Management API Support

---
+++ examples/ibm-account-management/outputs.tf
@@ -0,0 +1,27 @@
+// Uncomment the following outputs if you have uncommented the data source in main.tf
+/*
+output "account_name" {
+  description = "The name of the account"
+  value       = data.ibm_account_info.account_instance.name
+}
+
+output "account_owner" {
+  description = "The owner of the account"
+  value       = data.ibm_account_info.account_instance.owner
+}
+
+output "account_type" {
+  description = "The type of the account"
+  value       = data.ibm_account_info.account_instance.type
+}
+
+output "account_status" {
+  description = "The status of the account"
+  value       = data.ibm_account_info.account_instance.status
+}
+
+output "team_directory_enabled" {
+  description = "Whether team directory is enabled"
+  value       = data.ibm_account_info.account_instance.team_directory_enabled
+}
+*/
