b5f40574bcb95be6b8b1c789512768b699b96c41
BRIAN GLEESON <Brian.Gleeson@ie.ibm.com>
2026-06-08 09:15:59 +0100

    chore(iam-identity): improved API key example

--- examples/ibm-iam-identity-apikeys/main.tf
+++ examples/ibm-iam-identity-apikeys/main.tf
@@ -5,11 +5,13 @@ provider "ibm" {
 // Provision iam_api_key resource instance
 resource "ibm_iam_api_key" "iam_api_key_instance" {
   name = var.iam_api_key_name
-  description = "apikey desc"
-  file = var.iam_api_key_file_path
+  description = "apikey description"
+  entity_lock = var.iam_api_key_entity_lock
+  store_value = var.iam_api_key_store_value
+  file = var.iam_api_key_file
 }
 
 // Read iam_api_key data source
 data "ibm_iam_api_key" "iam_api_key_data" {
-  apikey_id = ibm_iam_api_key.iam_api_key_instance.apikey_id
+  apikey_id= ibm_iam_api_key.iam_api_key_instance.id
 }
