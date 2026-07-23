1e6b46c238be36d5c3c07856d3bdfe4b73118dd4
Ujjwal Kumar <ujjwal.kumar1@ibm.com>
2026-07-23 12:06:47 +0530

    Update data_source_ibm_is_lb_pools_test.go

--- ibm/service/vpc/data_source_ibm_is_lb_pools_test.go
+++ ibm/service/vpc/data_source_ibm_is_lb_pools_test.go
@@ -39,12 +39,60 @@ func TestAccIBMIsLbPoolsDataSourceBasic(t *testing.T) {
 	})
 }
 
+func TestAccIBMIsLbPoolsDataSource_mTLS(t *testing.T) {
+	vpcname := fmt.Sprintf("tflbp-vpc-%d", acctest.RandIntRange(10, 100))
+	subnetname := fmt.Sprintf("tflbpc-name-%d", acctest.RandIntRange(10, 100))
+	name := fmt.Sprintf("tfcreate%d", acctest.RandIntRange(10, 100))
+	poolName := fmt.Sprintf("tflbpoolc%d", acctest.RandIntRange(10, 100))
+	alg := "round_robin"
+	protocol := "https"
+	delay := "45"
+	retries := "5"
+	timeout := "15"
+	healthType := "https"
+
+	// Example CRNs - replace with actual values from your test environment
+	clientCertCRN := "crn:v1:bluemix:public:secrets-manager:us-south:a/7f75c7b025e54bc5635f754b2f888665:152af435-37ac-4b3e-83c3-828805bfc8e0:secret:1e5b9794-f576-de33-5e41-4a8c29d00132"
+	serverCACRN := "crn:v1:bluemix:public:secrets-manager:us-south:a/7f75c7b025e54bc5635f754b2f888665:152af435-37ac-4b3e-83c3-828805bfc8e0:secret:4a1bc2d6-ccd3-ad25-e6b7-8a0c522038f6"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { acc.TestAccPreCheck(t) },
+		Providers: acc.TestAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckIBMIsLbPoolsDataSourceConfigmTLS(vpcname, subnetname, acc.ISZoneName, acc.ISCIDR, name, poolName, alg, protocol, delay, retries, timeout, healthType, clientCertCRN, serverCACRN),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.ibm_is_lb_pools.is_lb_pools", "lb"),
+					resource.TestCheckResourceAttrSet("data.ibm_is_lb_pools.is_lb_pools", "pools.#"),
+					resource.TestCheckResourceAttrSet("data.ibm_is_lb_pools.is_lb_pools", "pools.0.client_authentication.#"),
+					resource.TestCheckResourceAttrSet("data.ibm_is_lb_pools.is_lb_pools", "pools.0.client_authentication.0.certificate_instance.#"),
+					resource.TestCheckResourceAttr("data.ibm_is_lb_pools.is_lb_pools", "pools.0.client_authentication.0.certificate_instance.0.crn", clientCertCRN),
+					resource.TestCheckResourceAttrSet("data.ibm_is_lb_pools.is_lb_pools", "pools.0.server_authentication.#"),
+					resource.TestCheckResourceAttr("data.ibm_is_lb_pools.is_lb_pools", "pools.0.server_authentication.0.verify_certificate", "true"),
+					resource.TestCheckResourceAttrSet("data.ibm_is_lb_pools.is_lb_pools", "pools.0.server_authentication.0.certificate_authority.#"),
+					resource.TestCheckResourceAttr("data.ibm_is_lb_pools.is_lb_pools", "pools.0.server_authentication.0.certificate_authority.0.crn", serverCACRN),
+				),
+			},
+		},
+	})
+}
+
 func testAccCheckIBMIsLbPoolsDataSourceConfigBasic(vpcname, subnetname, zone, cidr, name, poolName, algorithm, protocol, delay, retries, timeout, healthType string) string {
 	return testAccCheckIBMISLBPoolConfig(vpcname, subnetname, zone, cidr, name, poolName, algorithm, protocol, delay, retries, timeout, healthType) + fmt.Sprintf(`
         data "ibm_is_lb_pools" "is_lb_pools" {
             lb = "${ibm_is_lb.testacc_LB.id}"
         }
     `)
+}
+
+func testAccCheckIBMIsLbPoolsDataSourceConfigmTLS(vpcname, subnetname, zone, cidr, name, poolName, algorithm, protocol, delay, retries, timeout, healthType, clientCertCRN, serverCACRN string) string {
+	return testAccCheckIBMISLBPoolmTLSConfig(vpcname, subnetname, zone, cidr, name, poolName, algorithm, protocol, delay, retries, timeout, healthType, clientCertCRN, serverCACRN) + fmt.Sprintf(`
+
+	data "ibm_is_lb_pools" "is_lb_pools" {
+		lb = ibm_is_lb.testacc_LB.id
+		depends_on = [ibm_is_lb_pool.testacc_lb_pool_mtls]
+	}
+	`)
 }
 
 // TestAccIBMIsLbPoolsDataSourceHealthMonitor validates that the pools list data
