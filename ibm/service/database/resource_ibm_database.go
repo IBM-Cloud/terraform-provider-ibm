12e0a145795bd179a57efb76827167b733bf43f6
Puja Bharti <puja.bharti1@ibm.com>
2026-07-28 11:42:03 +0530

    Update allowed plans and rebase with master

--- ibm/service/database/resource_ibm_database.go
+++ ibm/service/database/resource_ibm_database.go
@@ -948,7 +948,7 @@ func ResourceIBMICDValidator() *validate.ResourceValidator {
 			Identifier:                 "plan",
 			ValidateFunctionIdentifier: validate.ValidateAllowedICDPlanValue,
 			Type:                       validate.TypeString,
-			AllowedValues:              "standard, standard-gen2, enterprise, enterprise-sharding, platinum, enterprise-gen2",
+			AllowedValues:              "standard, standard-gen2, enterprise, enterprise-gen2, enterprise-sharding, enterprise-sharding-gen2, platinum",
 			Required:                   true})
 	validateSchema = append(validateSchema,
 		validate.ValidateSchema{
