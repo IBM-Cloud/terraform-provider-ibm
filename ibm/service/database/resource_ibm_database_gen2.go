83fb7ec7017b408ce1db5536496dc3d3af384656
pushpalathaeg <pushpalathaeg@ibm.com>
2026-09-07 14:22:53 +0530

    updated the error

--- ibm/service/database/resource_ibm_database_gen2.go
+++ ibm/service/database/resource_ibm_database_gen2.go
@@ -417,7 +417,7 @@ func (g *resourceIBMDatabaseGen2Backend) buildDBConfig(d *schema.ResourceData, c
 	// member_zones is only valid when members == 1
 	if memberGroup != nil && len(memberGroup.MemberZones) > 0 {
 		if config.Members != 1 {
-			return nil, fmt.Errorf("[ERROR] member_zones can only be set when allocation_count is 1, got %d", config.Members)
+			return nil, fmt.Errorf("member_zones can only be set when allocation_count is 1, got %d", config.Members)
 		}
 		config.MemberZones = memberGroup.MemberZones
 	}
