da47df046c9f7d971a9768e02ecd88c4c8dd892f
pushpalathaeg <pushpalathaeg@ibm.com>
2026-09-07 14:28:46 +0530

    worked on go build

--- ibm/service/database/resource_ibm_database_gen2_test.go
+++ ibm/service/database/resource_ibm_database_gen2_test.go
@@ -2245,6 +2245,7 @@ func TestMemberZonesInDBConfig(t *testing.T) {
 		assert.Equal(t, []string{"us-east-1"}, zones)
 	})
 }
+
 // TestMemberCountFromGroups verifies the helper extracts the member allocation correctly.
 func TestMemberCountFromGroups(t *testing.T) {
 	cases := []struct {
